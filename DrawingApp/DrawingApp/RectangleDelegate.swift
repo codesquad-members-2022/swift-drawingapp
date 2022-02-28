a7613091c9d7d848ea351ab9a4d99776bd9fd0d0
sol <hskim6521@naver.com>
2022-02-28 23:38:34 +0900

    Feat: RectangleDelegate를 이용해, RectangleFactory 객체의 generateRectangle() 메서드가 실행될 때 로그를 출력하도록 설정

---
+++ DrawingApp/DrawingApp/RectangleDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  RectangleDelegate.swift
+//  DrawingApp
+//
+//  Created by 김한솔 on 2022/02/28.
+//
+
+import Foundation
+
+protocol RectangleDelegate {
+    func printLog(of rectangle: Rectangle)
+}
