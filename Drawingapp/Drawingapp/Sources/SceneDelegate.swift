203d437b3a8481549048bdef4b8e183aa3d03305
shingha <shingha1124@gmail.com>
2022-02-28 13:59:22 +0900

    [setting] 프로젝트 셋팅

---
+++ Drawingapp/Drawingapp/Sources/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Drawingapp
+//
+//  Created by seongha shin on 2022/02/28.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
