bc6a368bb2ddbeb3f55435cdd7c4e7d0caa438b0
BumgeunSong <isp1195@gmail.com>
2022-02-28 17:25:29 +0900

    Create Factory class

---
+++ DrawingApp/DrawingApp/Model/Model/Factory.swift
@@ -0,0 +1,24 @@
+//
+//  Factory.swift
+//  DrawingApp
+//
+//  Created by Bumgeun Song on 2022/02/28.
+//
+
+import Foundation
+
+class Factory {
+    func createRandomSquare() -> Square? {
+        let (width, height) = (Double(150), Double(120))
+        let X = Double.random(in: 10...500)
+        let Y = Double.random(in: 10...500)
+        guard let color = Color(r: Int.random(in: 0...255),
+                                g: Int.random(in: 0...255),
+                                b: Int.random(in: 0...255),
+                                a: Int.random(in: 0...10)) else {
+            return nil
+        }
+        
+        return Square(width: width, height: height, positionX: X, positionY: Y, color: color)
+    }
+}
