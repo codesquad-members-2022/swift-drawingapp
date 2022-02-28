d22e69b1a94c1717fa8c8c1bfb122306a84610e7
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-28 10:34:12 +0900

    [ADD]Create new project

---
+++ drawingApp/drawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  drawingApp
+//
+//  Created by Kai Kim on 2022/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
