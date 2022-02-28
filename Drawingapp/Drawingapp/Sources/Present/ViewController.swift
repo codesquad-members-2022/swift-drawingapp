20c847e6e78767ea2091be3d684400b77d6c2486
shingha <shingha1124@gmail.com>
2022-02-28 21:49:14 +0900

    [feature] viewController에서 4개의 사각형 만들고 출력하기

--- Drawingapp/Drawingapp/Sources/Present/ViewController.swift
+++ Drawingapp/Drawingapp/Sources/Present/ViewController.swift
@@ -9,8 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let factory = Factory()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        (0..<4).forEach { _ in
+            let newSquare = factory.makeSquare()
+            print("\(newSquare)")
+        }
+        
+        
 //        ViewController 클래스에 viewDidLoad() 함수에서 팩토리에서 모델 객체 4종류를 생성하고 print()가 아닌 시스템 로그 함수로 출력한다
     }
 
