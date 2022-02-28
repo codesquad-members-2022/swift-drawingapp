196af5f5fd178d6cc5aa6473c5c2a61ce4f6565a
kimdongjune <jun36817@naver.com>
2022-02-28 17:51:29 +0900

    사용자가 터치했을 경우, 프로토콜을 활용하여 사각형의 랜덤정보들을 생성 후, 이를 활용하여 사각형 뷰 추가.

---
+++ DrawingApp/DrawingApp/SquareViewProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  RandomValueProtocol.swift
+//  DrawingApp
+//
+//  Created by 김동준 on 2022/02/28.
+//
+
+import Foundation
+
+protocol SquareViewProtocol{
+    func makeSquareView(squareViewState: SquareViewState)
+    
+}
