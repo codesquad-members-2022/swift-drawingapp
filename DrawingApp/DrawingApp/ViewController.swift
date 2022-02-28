83f78f63147149dcf8a146327630d3fdad00093e
sungju-kim <wnwn0719@gmail.com>
2022-02-28 13:42:09 +0900

    Feat: RectangleFactory 구현

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let screenSize = (Double(self.view.safeAreaLayoutGuide.layoutFrame.size.width), Double(self.view.safeAreaLayoutGuide.layoutFrame.size.height))
+        let rect1 = RectangleFactory(screenSize: screenSize).make()
+        let rect2 = RectangleFactory(screenSize: screenSize).make()
+        let rect3 = RectangleFactory(screenSize: screenSize).make()
+        let rect4 = RectangleFactory(screenSize: screenSize).make()
+        
     }
 
 
