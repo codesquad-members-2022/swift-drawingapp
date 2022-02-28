b85635d9c2a8b1d81891734144a75e8576f5e7e1
sol <hskim6521@naver.com>
2022-02-28 22:40:45 +0900

    Feat: RootVC의 viewDidLoad() 메서드에 4개의 사각형 객체 생성되는 코드 추가

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let currentScreenSize = (width: Double(self.view.safeAreaLayoutGuide.layoutFrame.width),
+                                 height: Double(self.view.safeAreaLayoutGuide.layoutFrame.height))
+        let factory = RectangleFactory(screenSize: currentScreenSize)
+        let rect1 = factory.generateRectangle()
+        let rect2 = factory.generateRectangle()
+        let rect3 = factory.generateRectangle()
+        let rect4 = factory.generateRectangle()
         
     }
 
