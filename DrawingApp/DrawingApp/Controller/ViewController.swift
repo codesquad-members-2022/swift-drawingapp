a96009d57a88b610c80194776b56d32e66477737
BumgeunSong <isp1195@gmail.com>
2022-02-28 17:30:13 +0900

    Make viewController log Rect using Factory

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let factory = Factory()
+        if let rect = factory.createRandomSquare() {
+            print("rect1: \(rect)")
+            
+        }
     }
 
 
