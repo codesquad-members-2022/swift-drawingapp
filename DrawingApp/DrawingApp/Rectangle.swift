adce9b53f075653019262d731d24b4b52e8ef156
sungju-kim <wnwn0719@gmail.com>
2022-02-28 13:42:09 +0900

    Feat: RectangleFactory 구현

---
+++ DrawingApp/DrawingApp/Rectangle.swift
@@ -0,0 +1,33 @@
+//
+//  RectangleGenerator.swift
+//  DrawingApp
+//
+//  Created by dale on 2022/02/28.
+//
+
+import Foundation
+
+class Rectangle {
+    private var id : String
+    private var width : Double
+    private var height : Double
+    private var position : (Double,Double)
+    private var backGroundColor : (Int,Int,Int)
+    private var alpha : Int
+    
+    init(id: String, width : Double, height: Double, position : (Double,Double), color : (Int,Int,Int), alpha : Int) {
+        self.id = id
+        self.width = width
+        self.height = height
+        self.position = position
+        self.backGroundColor = color
+        self.alpha = alpha
+    }
+    
+}
+
+extension Rectangle : CustomStringConvertible {
+    var description: String {
+        return "(\(self.id)), x:\(self.position.0), y:\(self.position.1), W\(width), H\(height), R:\(self.backGroundColor.0), G:\(self.backGroundColor.1), B:\(self.backGroundColor.2), Alpha:\(self.alpha)"
+    }
+}
---
+++ DrawingApp/DrawingApp/RectangleFactory.swift
@@ -0,0 +1,45 @@
+//
+//  RectangleFactory.swift
+//  DrawingApp
+//
+//  Created by dale on 2022/02/28.
+//
+
+import Foundation
+
+class RectangleFactory {
+    private let screenSize : (Double,Double)
+    init(screenSize : (Double,Double)) {
+        self.screenSize = screenSize
+    }
+    func make() -> Rectangle {
+        return Rectangle(id: randomId(), width: 150, height: 120, position: randomPosition(), color: randomColor(), alpha: randomAlpha())
+    }
+    
+    private func randomId() -> String {
+        let string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var randomString : String {
+            return (0..<3).map{_ in String(string.randomElement() ?? Character(""))}.joined()
+        }
+        return "\(randomString)-\(randomString)-\(randomString)"
+    }
+    
+    private func randomPosition() -> (Double,Double) {
+        let screenWidth = screenSize.0
+        let screenHeight = screenSize.1
+        let position = (Double.random(in: 0...screenWidth), Double.random(in: 0...screenHeight))
+        return position
+    }
+    
+    private func randomColor() -> (Int,Int,Int){
+        var randomInt: Int {
+            return (0..<255).randomElement() ?? 0
+        }
+        return (randomInt,randomInt,randomInt)
+    }
+    
+    private func randomAlpha() -> Int {
+        return (1...10).randomElement() ?? 5
+    }
+    
+}
