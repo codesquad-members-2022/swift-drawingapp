740c873658185ca2c3fb278ce31597d9c076b37a
Song TaeHwan <axoghks@gmail.com>
2022-03-01 18:47:27 +0900

    refactor: Remove unnecessary annotations and life cycle methods

--- DrawingApp/DrawingAppTests/DrawingAppTests.swift
+++ DrawingApp/DrawingAppTests/DrawingAppTests.swift
@@ -8,14 +8,6 @@
 import XCTest
 
 class DrawingAppTests: XCTestCase {
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
     func testSize() {
         var size = Size(width: 30, height: 30)
         XCTAssertEqual(size.width, 30)
