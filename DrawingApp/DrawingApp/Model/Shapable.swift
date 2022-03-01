d92b65403df73dd44e2c25c8621d63caab5893f4
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-01 23:57:42 +0900

    [Feat] Shapable 프로토콜 생성 및 구현

---
+++ DrawingApp/DrawingApp/Model/Shapable.swift
@@ -0,0 +1,21 @@
+//
+//  Shapable.swift
+//  DrawingApp
+//
+//  Created by 김상혁 on 2022/03/01.
+//
+
+import Foundation
+
+protocol Shapable: CustomStringConvertible {
+    var id: String { get }
+    var size: Size { get }
+    var point: Point { get }
+    var backGroundColor: Color { get }
+}
+
+extension Shapable {
+    var description: String {
+        return "(\(self.id)), \(self.point), \(self.size), \(self.backGroundColor)"
+    }
+}
