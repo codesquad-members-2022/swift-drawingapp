97fbc3b253b6cca4e1ad152e4cd9c502048a0a5c
P_SeoB <akdlsjtm21@naver.com>
2022-03-01 20:09:02 +0900

    [ADD] os_log Test

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import OSLog
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let id = RandomFactory.makeRandomID()
+        let origin = RandomFactory.makeRandomPoint()
+        let size = Size(width: 150, height: 120)
+        let alpha = RandomFactory.makeRandomAlpha()
+        let rgb = RandomFactory.makeRandomRGB()
+        
+        let rectangle = RectangleFactory.makeRectangleAtRandomPoint(
+            id: id,
+            origin: origin,
+            size: size,
+            backGroundColor: rgb,
+            alpha:alpha
+        )
+        
+        os_log("\(rectangle)")
         
     }
-
-
 }
 
