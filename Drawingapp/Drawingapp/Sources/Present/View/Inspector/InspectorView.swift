94469ca3370347f9b187f37154fb52c338741827
shingha <shingha1124@gmail.com>
2022-03-01 16:39:18 +0900

    [feature] 인스펙터 UI, 영역 설정 및 오토레이아웃 작업

--- Drawingapp/Drawingapp/Sources/Present/View/Inspector/InspectorView.swift
+++ Drawingapp/Drawingapp/Sources/Present/View/Inspector/InspectorView.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class InspectorView: BaseView {
     
+    let itemStackView = UIStackView()
+    let backgroundColorInfo = InspectorItemLabelView()
+    let backgroundColorInfo1 = InspectorItemLabelView()
+    
     func bind(plane: Plane) {
     }
     
     override func attribute() {
         super.attribute()
         self.backgroundColor = UIColor(red: 200.0 / 255.0, green: 200.0 / 255.0, blue: 1, alpha: 1)
+        
+        itemStackView.axis = .vertical
+        
+        backgroundColorInfo.title.text = "배경색"
     }
     
     override func layout() {
         super.layout()
+        
+        let safeAreaGuide = self.safeAreaLayoutGuide
+        
+        self.addSubview(itemStackView)
+        itemStackView.addArrangedSubview(backgroundColorInfo)
+        itemStackView.addArrangedSubview(backgroundColorInfo1)
+        
+        itemStackView.translatesAutoresizingMaskIntoConstraints = false
+        itemStackView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor).isActive = true
+        itemStackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        itemStackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        itemStackView.bottomAnchor.constraint(equalTo: backgroundColorInfo1.bottomAnchor).isActive = true
     }
 }
--- Drawingapp/Drawingapp/Sources/Present/View/Inspector/ItemView/InspectorItemView.swift
+++ Drawingapp/Drawingapp/Sources/Present/View/Inspector/ItemView/InspectorItemView.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class InspectorItemView: BaseView {
     let title = UILabel()
+    let stackView = UIStackView()
+    
+    override func attribute() {
+        super.attribute()
+        title.backgroundColor = .red
+        title.text = "제목 출력 부분"
+        
+        stackView.backgroundColor = .black
+        stackView.axis = .vertical
+    }
+    
+    override func layout() {
+        super.layout()
+        self.addSubview(stackView)
+        stackView.addArrangedSubview(title)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5).isActive = true
+        stackView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 5).isActive = true
+        stackView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -5).isActive = true
+        
+        self.bottomAnchor.constraint(equalTo: stackView.bottomAnchor).isActive = true
+    }
 }
