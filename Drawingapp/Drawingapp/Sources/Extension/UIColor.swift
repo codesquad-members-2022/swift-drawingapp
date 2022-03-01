3212e96f96a0a6c25b5ea441ce12c2671852538e
shingha <shingha1124@gmail.com>
2022-03-01 10:18:33 +0900

    [add] color extension 추가

---
+++ Drawingapp/Drawingapp/Sources/Extension/UIColor.swift
@@ -0,0 +1,12 @@
+//
+//  Extension.swift
+//  Drawingapp
+//
+//  Created by seongha shin on 2022/03/01.
+//
+
+import Foundation
+
+extension Color {
+    static var black = Color(r: 0, g: 0, b: 0)
+}
