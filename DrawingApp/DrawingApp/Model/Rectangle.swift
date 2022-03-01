acdd723cf5aee7e35fed13d62baa5179f34c0952
Song TaeHwan <axoghks@gmail.com>
2022-03-01 16:31:37 +0900

    feat: Implement Rectangle class

--- DrawingApp/DrawingApp/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class Rectangle {
+    // MARK: - Properties
+    private let id: String
+    
+    var backgroundColor: Color = .white
+    var alpha: Alpha = .ten
+    var point: Point
+    var size: Size
+    
+    // MARK: - Initializers
+    init(id: String, point: Point, size: Size) {
+        self.id = id
+        self.point = point
+        self.size = size
+    }
+    
+    convenience init(id: String, x: Double, y: Double, width: Double, height: Double) {
+        let point = Point(x: x, y: y)
+        let size = Size(width: width, height: height)
+        self.init(id: id, point: point, size: size)
+    }
+}
+
+extension Rectangle: CustomStringConvertible {
+    var description: String {
+        return """
+        Rect (\(self.id)), \(self.point.x), \(self.size.height), \(self.backgroundColor), \(self.alpha)
+        """
+    }
+}
