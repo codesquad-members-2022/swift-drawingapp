29b013f1e3b8bc56756ffbf61136adf0cf3cd5b6
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-01 14:39:48 +0900

    Add: Rectangle.swift 생성 및 Rectangle 클래스 구현
    - Rectangle 클래스 구현

--- DrawingApp/DrawingApp/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -1,1 +1,75 @@
 import Foundation
+
+class Rectangle: CustomStringConvertible{
+    
+    struct Size: Equatable, CustomStringConvertible{
+        var width: Double
+        var height: Double
+        var description: String{
+            return "W\(width),H\(height)"
+        }
+        
+        init(width: Double, height: Double){
+            self.width = width
+            self.height = height
+        }
+    }
+    
+    struct Point: Equatable, CustomStringConvertible{
+        var x: Double
+        var y: Double
+        var description: String{
+            return "x:\(x),y:\(y)"
+        }
+        
+        init(x: Double, y: Double){
+            self.x = x
+            self.y = y
+        }
+    }
+    
+    struct Color:Equatable, CustomStringConvertible{
+        var r: Double
+        var g: Double
+        var b: Double
+        var description: String{
+            return "r:\(r),g:\(g),b:\(b)"
+        }
+    }
+    
+    enum Alpha:Int, CaseIterable, CustomStringConvertible{
+        
+        case one = 1
+        case two = 2
+        case three = 3
+        case four = 4
+        case five = 5
+        case six = 6
+        case seven = 7
+        case eight = 8
+        case nine = 9
+        case ten = 10
+        
+        var description: String{
+            return String(self.rawValue)
+        }
+    }
+    
+    private var id: String
+    private var size: Size
+    private var point: Point
+    private var backgroundColor: Color
+    private var alpha: Alpha
+    var description: String{
+        return "(\(id)), \(size), \(point), \(backgroundColor), \(alpha)"
+    }
+    
+    init(id: String, size: Size, point: Point, backgroundColor: Color, alpha: Alpha){
+        self.id = id
+        self.size = size
+        self.point = point
+        self.backgroundColor = backgroundColor
+        self.alpha = alpha
+    }
+    
+}
