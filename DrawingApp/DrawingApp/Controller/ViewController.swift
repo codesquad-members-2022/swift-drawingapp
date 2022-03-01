6917de9697b9ced3b1f2faa505a5ad8974e2eb73
Song TaeHwan <axoghks@gmail.com>
2022-03-01 13:10:47 +0900

    docs: Update README.md

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Properties
 
+    // MARK: - View Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Methods
 }
 
