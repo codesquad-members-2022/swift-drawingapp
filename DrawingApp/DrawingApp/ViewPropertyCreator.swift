a31bee25bdd7623822f3617b5d50f2f868b701c4
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-03-01 22:04:34 +0900

    Refactor Before Pull Request
    풀 리퀘스트 이전에 접근제한자, 객체 역할 변경, 테스트 수정 등을 진행하였습니다.

--- DrawingApp/DrawingApp/ViewPropertyCreator.swift
+++ DrawingApp/DrawingApp/ViewPropertyCreator.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// 위치, 크기, RGB 값을 생성하는 팩토리 메소드를 포함하는 클래스입니다.
+// 랜덤 값을 추출하는 RandomizeValue 를 서브클래싱 합니다.
 class ViewPropertyCreator: RandomizeValue {
     
     func rectPointGenerator(maxPointX: Double, maxPointY: Double) -> RectPoint {
