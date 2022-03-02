638e9cc611e33badefe2936f681210be79516a74
sanghee <anshee97@naver.com>
2022-03-02 15:41:34 +0900

    Initial commit

---
+++ DrawingApp/DrawingApp/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  DrawingApp
+//
+//  Created by Selina on 2022/03/02.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+}
+
