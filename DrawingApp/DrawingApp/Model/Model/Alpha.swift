de01c2d8af8b99bb419ec9b53419a10f8b84f1c0
BumgeunSong <isp1195@gmail.com>
2022-03-02 15:09:38 +0900

    Make Color & Alpha distinct struct

---
+++ DrawingApp/DrawingApp/Model/Model/Alpha.swift
@@ -0,0 +1,18 @@
+//
+//  Alpha.swift
+//  DrawingApp
+//
+//  Created by Bumgeun Song on 2022/03/02.
+//
+
+import Foundation
+
+struct Alpha {
+    var value: Float
+    
+    init?(value: Float) {
+        guard 0 <= value, value <= 10 else { return nil }
+        
+        self.value = value / 10
+    }
+}
--- DrawingApp/DrawingApp/Model/Model/Color.swift
+++ DrawingApp/DrawingApp/Model/Model/Color.swift
@@ -8,16 +8,15 @@
 import Foundation
 
 struct Color {
-    var red: Int
-    var green: Int
-    var blue: Int
-    var alpha: Int
+    var red: Float
+    var green: Float
+    var blue: Float
     
-    init?(r red: Int, g green: Int, b blue: Int, a alpha: Int) {
-        guard red >= 0, red <= 255, green >= 0, green <= 255, blue >= 0, blue <= 255, alpha >= 0, alpha <= 10 else { return nil }
-        self.red = red
-        self.green = green
-        self.blue = blue
-        self.alpha = alpha
+    init?(r red: Float, g green: Float, b blue: Float) {
+        guard red >= 0, red <= 255, green >= 0, green <= 255, blue >= 0, blue <= 255 else { return nil }
+        
+        self.red = red / 255
+        self.green = green / 255
+        self.blue = blue / 255
     }
 }
