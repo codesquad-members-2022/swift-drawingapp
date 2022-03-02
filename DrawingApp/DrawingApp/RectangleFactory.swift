e242d98c0e8a311bb7bc297254b9744b5a5bd2cc
Jinsujin <sylpid003@gmail.com>
2022-03-03 02:24:53 +0900

    Feat: ShapeFactoryType- enum 으로 객체를 생성할 수 있는 팩토리 구현
    직사각형, 정사각형인지에 따라 인자를 다르게 받아 객체생성하여 반환

--- DrawingApp/DrawingApp/RectangleFactory.swift
+++ DrawingApp/DrawingApp/RectangleFactory.swift
@@ -7,20 +7,54 @@
 
 import Foundation
 
+enum ShapeFactoryType {
+    case rectangle(width: Double, height: Double)
+    case square(length: Double)
+    
+    func make() -> Rectangle {
+        switch self {
+        case .rectangle(let width, let height):
+            return RectangleFactory.make(size: Rectangle.Size(width: width, height: height))
+        case .square(let length):
+            return RectangleFactory.make(size: Rectangle.Size(width: length, height: length))
+        }
+    }
+}
+
 
 final class RectangleFactory {
     static func make(colorType: Color.Standard, x: Double = 0, y: Double, width: Double = 100, height: Double = 100) -> Rectangle {
         let uid = UniqueID.generate()
-        return Rectangle(uid: uid, point: Rectangle.Point(x: x, y: y), size: Rectangle.Size(width: width, height: height), color: colorType.rgb, alpha: 1)
+        return Rectangle(uid: uid,
+                         point: Rectangle.Point(x: x, y: y),
+                         size: Rectangle.Size(width: width, height: height),
+                         color: colorType.rgb,
+                         alpha: 1)
     }
     
     static func make(x: Double, y: Double, width: Double, height: Double) -> Rectangle {
         let uid = UniqueID.generate()
-        return Rectangle(uid: uid, point: Rectangle.Point(x: x, y: y), size: Rectangle.Size(width: width, height: height), color: Color.Standard.white.rgb, alpha: 1)
+        return Rectangle(uid: uid,
+                         point: Rectangle.Point(x: x, y: y),
+                         size: Rectangle.Size(width: width, height: height),
+                         color: Color.Standard.white.rgb, alpha: 1)
     }
     
-    static func make(origin: Rectangle.Point, size: Rectangle.Size) -> Rectangle {
+    static func make(x: Int, y: Int, width: Int, height: Int) -> Rectangle {
         let uid = UniqueID.generate()
-        return Rectangle(uid: uid, point: origin, size: size, color: Color.Standard.white.rgb, alpha: 1)
+        return Rectangle(uid: uid,
+                         point: Rectangle.Point(x: Double(x), y: Double(y)),
+                         size: Rectangle.Size(width: Double(width), height: Double(height)),
+                         color: Color.Standard.white.rgb,
+                         alpha: 1)
+    }
+    
+    static func make(origin: Rectangle.Point = .init(x: 0, y: 0), size: Rectangle.Size) -> Rectangle {
+        let uid = UniqueID.generate()
+        return Rectangle(uid: uid,
+                         point: origin,
+                         size: size,
+                         color: Color.Standard.white.rgb,
+                         alpha: 1)
     }
 }
