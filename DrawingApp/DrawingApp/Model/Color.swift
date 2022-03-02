35f05f850bc87656e5d8219dd7d4a4292510d191
Song TaeHwan <axoghks@gmail.com>
2022-03-02 10:56:39 +0900

    refactor: hide propteries of struct and class from outside

--- DrawingApp/DrawingApp/Model/Color.swift
+++ DrawingApp/DrawingApp/Model/Color.swift
@@ -14,28 +14,21 @@ struct Color {
     static let blue = Color(red: 0, green: 0, blue: 255)
     static let white = Color(red: 255, green: 255, blue: 255)
     
-    var red: Double
-    var green: Double
-    var blue: Double
-    var alpha: Alpha
+    let red: Double
+    let green: Double
+    let blue: Double
+    let alpha: Alpha
     
-    init(red: Double, green: Double, blue: Double, alpha: Alpha = .ten) {
+    init(red: Double, green: Double, blue: Double, alpha: Alpha = .opaque) {
         self.red = red
         self.green = green
         self.blue = blue
-        self.alpha = alpha
-    }
-    
-    init(red: Int, green: Int, blue: Int, alpha: Alpha = .ten) {
-        self.red = Double(red)
-        self.green = Double(green)
-        self.blue = Double(blue)
         self.alpha = alpha
     }
 }
 
 extension Color: CustomStringConvertible, Equatable {
     var description: String {
-        return "R: \(self.red), G: \(self.green), B: \(self.blue)"
+        return "R: \(self.red), G: \(self.green), B: \(self.blue), Alph: \(self.alpha)"
     }
 }
--- DrawingApp/DrawingApp/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -10,11 +10,9 @@ import Foundation
 class Rectangle {
     // MARK: - Properties
     let id: String
-    
-    var backgroundColor: Color = .white
-    var alpha: Alpha = .ten
-    var point: Point
-    var size: Size
+    let backgroundColor: Color = .white
+    let point: Point
+    let size: Size
     
     // MARK: - Initializers
     init(id: String, point: Point, size: Size) {
@@ -23,17 +21,17 @@ class Rectangle {
         self.size = size
     }
     
-    convenience init(id: String, x: Double, y: Double, width: Double, height: Double) {
-        let point = Point(x: x, y: y)
-        let size = Size(width: width, height: height)
-        self.init(id: id, point: point, size: size)
+    init(id: String, x: Double, y: Double, width: Double, height: Double) {
+        self.id = id
+        self.point = Point(x: x, y: y)
+        self.size = Size(width: width, height: height)
     }
 }
 
 extension Rectangle: CustomStringConvertible {
     var description: String {
         return """
-        Rect (\(self.id)), \(self.point.x), \(self.size.height), \(self.backgroundColor), \(self.alpha)
+        Rect (\(self.id)), \(self.point), \(self.size), \(self.backgroundColor)
         """
     }
 }
