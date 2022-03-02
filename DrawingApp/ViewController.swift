32546576e59f1e6f6b63ee9c0e143597f725b8e0
wnsxor1993 <wnsxor1993@naver.com>
2022-03-02 14:08:12 +0900

    Feat: 좌표 판단 기능 추가 - 기존 RectangleArray 클래스를 요구사항에 맞춰 Plane 구조체로 변경 - Plane 구조체에 좌표를 통해 사각형 존재 여부 기능 추가

--- DrawingApp/ViewController.swift
+++ DrawingApp/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import os
 
 class ViewController: UIViewController {
-    private var rectangles = RectangleArray()
+    private var rectangles = Plane()
     
     @IBOutlet weak var rectangleButton: UIButton!
     
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     @IBAction func makeRandomRectangle(_ sender: Any) {
         rectangles.makeRectangle(viewWidth: self.view.frame.width, viewHeight: self.rectangleButton.frame.minY)
         
-        guard let rectangle = rectangles.nowMadeRectangle() else{
+        guard rectangles.count() > 0, let rectangle = rectangles[rectangles.count() - 1] else{
             let alert = UIAlertController(title: "Warning", message: "작성된 사각형이 없습니다.", preferredStyle: .alert)
             let okayAction = UIAlertAction(title: "Okay", style: UIAlertAction.Style.cancel)
             alert.addAction(okayAction)
