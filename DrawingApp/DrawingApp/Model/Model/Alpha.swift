d735dc0612a132c9f0d160316760163061c7dd38
BumgeunSong <isp1195@gmail.com>
2022-03-02 15:18:53 +0900

    Create Point, Size struct & conform String Convertible

--- DrawingApp/DrawingApp/Model/Model/Alpha.swift
+++ DrawingApp/DrawingApp/Model/Model/Alpha.swift
@@ -7,12 +7,16 @@
 
 import Foundation
 
-struct Alpha {
+struct Alpha: CustomStringConvertible {
     var value: Float
     
     init?(value: Float) {
         guard 0 <= value, value <= 10 else { return nil }
         
         self.value = value / 10
+    }
+    
+    var description: String {
+        "Alpha: \(value)"
     }
 }
---
+++ DrawingApp/DrawingApp/Model/Model/Size.swift
@@ -0,0 +1,17 @@
+//
+//  Size.swift
+//  DrawingApp
+//
+//  Created by Bumgeun Song on 2022/03/02.
+//
+
+import Foundation
+
+struct Size: CustomStringConvertible {
+    var width: Double
+    var height: Double
+    
+    var description: String {
+        "W\(width), H\(height)"
+    }
+}
