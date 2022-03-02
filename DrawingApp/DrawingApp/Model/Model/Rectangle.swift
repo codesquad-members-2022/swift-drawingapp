b5779b6606e63138fee8579b82f2cedeeb93c420
BumgeunSong <isp1195@gmail.com>
2022-03-02 20:07:19 +0900

    Add ColorMutable, AlphaMutable protocol

--- DrawingApp/DrawingApp/Model/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Model/Rectangle.swift
@@ -7,12 +7,20 @@
 
 import Foundation
 
-protocol viewModel {
+protocol ViewModel {
     var center: Point { get }
     func contains(_ point: Point) -> Bool
 }
 
-class Rectangle: viewModel, CustomStringConvertible {
+protocol ColorMutable {
+    func transform(to color: Color)
+}
+
+protocol AlphaMutable {
+    func transform(to alpha: Alpha)
+}
+
+class Rectangle: ViewModel {
     private var id: String
     private var origin: Point
     private var size: Size
@@ -27,18 +35,25 @@ class Rectangle: viewModel, CustomStringConvertible {
         self.alpha = alpha
     }
     
-    func contains(_ point: Point) -> Bool {
-        return (origin.x...origin.x+size.width).contains(point.x)
-        && (origin.y...origin.y+size.height).contains(point.y)
-    }
-    
     var center: Point {
         Point(x: origin.x + (size.width / 2),
               y: origin.y + (size.height / 2))
     }
     
+    func contains(_ point: Point) -> Bool {
+        return (origin.x...origin.x+size.width).contains(point.x)
+        && (origin.y...origin.y+size.height).contains(point.y)
+    }
+}
+
+extension Rectangle: ColorMutable {
+    func transform(to color: Color) {
+        self.color = color
+    }
+}
+
+extension Rectangle: CustomStringConvertible {
     var description: String {
         return "(\(id)), \(origin), \(size), \(color), \(alpha)"
     }
 }
-
