216b4615110cbfd14b50abae5f6fda1f2fbe48f0
Song TaeHwan <axoghks@gmail.com>
2022-03-02 15:36:20 +0900

    refactor: Refactor Rectangle and ShapeFactory

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
     // MARK: - View Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        let rect = ShapeFactory.makeRectangle()
-        print(rect)
+        
+        for i in 0..<4 {
+            let rect = ShapeFactory.makeRandomRectangle()
+            print("Rect \(i+1): \(rect)")
+        }
+        
     }
 }
 
