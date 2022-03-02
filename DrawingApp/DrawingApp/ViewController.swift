b033e290a506c9ae22bd10d72747550a3576d248
Jinsujin <sylpid003@gmail.com>
2022-03-02 23:52:09 +0900

    Feat: Rectangle 에 CustomStringConvertible 적용

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -13,6 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let rect1 = Rectangle(point: Rectangle.Point(x:10, y:200), size: Rectangle.Size(width: 150, height: 120), color: Color(red: 245, green: 0, blue: 245), alpha: 9)
-        print(rect1)
+        print("Rect1", rect1)
     }
 }
