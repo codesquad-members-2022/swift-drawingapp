babe2d355036c6ac3d2154d584dbf49f02f1a775
Jinsujin <sylpid003@gmail.com>
2022-03-03 02:06:57 +0900

    Feat: RectangleFactory 생성 및 구현
    - Color 구조체에 enum 추가: 팩토리에서 사용 위함
    - UniqueID 생성의 인자 초기값 설정
    - Rectangle 생성자에서 uid 인자로 받도록 수정

--- DrawingApp/DrawingApp/Color.swift
+++ DrawingApp/DrawingApp/Color.swift
@@ -26,6 +26,31 @@ struct Color {
     }
 }
 
+extension Color {
+    enum Standard {
+        case red
+        case green
+        case blue
+        case white
+        case black
+        
+        var rgb: Color {
+            switch self {
+            case .red:
+                return Color(red: 255, green: 0, blue: 0)
+            case .green:
+                return Color(red: 0, green: 255, blue: 0)
+            case .blue:
+                return Color(red: 0, green: 0, blue: 255)
+            case .white:
+                return Color(red: 255, green: 255, blue: 255)
+            case .black:
+                return Color(red: 0, green: 0, blue: 0)
+            }
+        }
+    }
+}
+
 fileprivate struct ColorExtension {
     static func adjustRange(value: Int) -> Int {
         if value < 0 {
--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let rect1 = Rectangle(point: Rectangle.Point(x:10, y:200), size: Rectangle.Size(width: 150, height: 120), color: Color(red: 245, green: 0, blue: 245), alpha: 9)
+        let rect1 = Rectangle(uid: "1111", point: Rectangle.Point(x:10, y:200), size: Rectangle.Size(width: 150, height: 120), color: Color(red: 245, green: 0, blue: 245), alpha: 9)
         print("Rect1", rect1)
+        
+        let rect2 = RectangleFactory.make(origin: Rectangle.Point(x: 0, y: 0), size: Rectangle.Size(width: 100, height: 100))
+        print("Rect2", rect2)
     }
 }
