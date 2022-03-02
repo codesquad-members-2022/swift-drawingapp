b00464fae85005a897a0779b61c23141e614e35a
Jinsujin <sylpid003@gmail.com>
2022-03-02 23:42:45 +0900

    Feat: Rectangle 클래스 작성, 초기화 메서드 구현

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -11,5 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let rect1 = Rectangle(point: Rectangle.Point(x:10, y:200), size: Rectangle.Size(width: 150, height: 120), color: Color(red: 245, green: 0, blue: 245), alpha: 9)
+        print(rect1)
     }
 }
