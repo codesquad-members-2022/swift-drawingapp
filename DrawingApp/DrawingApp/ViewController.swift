0129671d21b25d20d5e61af4599e8198fd158cd9
Jinsujin <sylpid003@gmail.com>
2022-03-03 03:26:38 +0900

    Feat: OS Log 를 커스텀해 Log 클래스 구현(싱글톤)

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -12,15 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let rect1 = Rectangle(uid: "1111", point: Rectangle.Point(x:10, y:200), size: Rectangle.Size(width: 150, height: 120), color: Color(red: 245, green: 0, blue: 245), alpha: 9)
-        print("Rect1", rect1)
+        do {
+            try Log.shared.config()
+        } catch {
+            print(error.localizedDescription)
+        }
         
-        let rect2 = RectangleFactory.make(origin: Rectangle.Point(x: 0, y: 0), size: Rectangle.Size(width: 100, height: 100))
-        print("Rect2", rect2)
-        
-        let rect3 = ShapeFactoryType.rectangle(width: 100, height: 120).make()
-        let rect4 = ShapeFactoryType.square(length: 20).make()
-        print(rect3)
-        print(rect4)
+        let rect = ShapeFactoryType.square(length: 20).make()
+        Log.shared.print(logLevel: .default, message: rect.description)
     }
 }
