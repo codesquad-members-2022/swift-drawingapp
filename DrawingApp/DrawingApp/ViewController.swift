0fc8acead19e0b593aee767ac17347d77f4d4196
asqw887 <asqw887@gmail.com>
2022-03-02 16:13:01 +0900

    프로젝트 초기 셋팅

---
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by 최예주 on 2022/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
