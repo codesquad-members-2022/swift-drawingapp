d94ae2b6333301f00656cc3561aa849a22298e24
BumgeunSong <isp1195@gmail.com>
2022-03-02 16:33:00 +0900

    Create Plane tests & add test case

---
+++ DrawingApp/DrawingAppTests/PlaneTests.swift
@@ -0,0 +1,41 @@
+//
+//  DrawingAppTests.swift
+//  DrawingAppTests
+//
+//  Created by Bumgeun Song on 2022/02/28.
+//
+
+import XCTest
+@testable import DrawingApp
+
+class PlaneTests: XCTestCase {
+
+    func testExample() throws {
+        let two = 1 + 1
+        XCTAssertEqual(two, 2)
+    }
+    
+    func testInitialSetup() throws {
+        let plane = Plane()
+        plane.setUpInitialModels()
+        XCTAssertEqual(plane.rectangleCount, 4)
+    }
+    
+    func testAddRectangle() throws {
+        let plane = Plane()
+        plane.addRectangle()
+        XCTAssertEqual(plane.rectangleCount, 1)
+    }
+    
+    func testSelectedWhenTapped() {
+        let plane = Plane()
+        plane.setUpInitialModels()
+        
+        for testIndex in 0..<plane.rectangleCount {
+            let testPoint = plane[testIndex].center
+            plane.tap(on: testPoint)
+            XCTAssertNotNil(plane.selected)
+            XCTAssertTrue(plane.selected!.contains(testPoint))
+        }
+    }
+}
