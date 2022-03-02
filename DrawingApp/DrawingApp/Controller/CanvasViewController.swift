0edb04831f37a98312035ff2c7983de948146d30
BumgeunSong <isp1195@gmail.com>
2022-03-02 16:44:20 +0900

    Create Split View Controller in Storyboard & connect it to code

--- DrawingApp/DrawingApp/Controller/CanvasViewController.swift
+++ DrawingApp/DrawingApp/Controller/CanvasViewController.swift
@@ -1,14 +1,13 @@
 //
-//  ViewController.swift
+//  CanvasViewController.swift
 //  DrawingApp
 //
 //  Created by Bumgeun Song on 2022/02/28.
 //
 
 import UIKit
-import OSLog
 
-class ViewController: UIViewController {
+class CanvasViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ DrawingApp/DrawingApp/Controller/PanelViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PanelViewController.swift
+//  DrawingApp
+//
+//  Created by Bumgeun Song on 2022/03/02.
+//
+
+import UIKit
+
+class PanelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
