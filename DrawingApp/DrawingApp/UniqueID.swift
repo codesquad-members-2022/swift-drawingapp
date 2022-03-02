5c654b99b2bf6c0363d4e81f6d09c564233397e1
Jinsujin <sylpid003@gmail.com>
2022-03-03 00:45:51 +0900

    Feat: Rectangle 랜덤 아이디 생성 구현

---
+++ DrawingApp/DrawingApp/UniqueID.swift
@@ -0,0 +1,31 @@
+//
+//  UniqueID.swift
+//  DrawingApp
+//
+//  Created by jsj on 2022/03/03.
+//
+
+import Foundation
+
+
+struct UniqueID {
+    static func generate(split digit: Int, stringCount: Int) -> String {
+        return (0..<digit).map{ _ in randomStrings(count: stringCount) }.joined(separator: "-")
+    }
+    
+    static private func randomStrings(count: Int) -> String {
+        return (0..<count).map{ _ in randomUnicode() }.joined()
+    }
+    
+    static private func randomUnicode() -> String {
+        let numberOrString = Int.random(in: 0...1)
+        if numberOrString == 0 {
+            let randomNumber = Int.random(in: 48...57)
+            let number = String(UnicodeScalar(randomNumber)!)
+            return number
+        }
+        let randomAlphabet = Int.random(in: 97...122)
+        let alphabet = String(UnicodeScalar(randomAlphabet)!)
+        return alphabet
+    }
+}
