b7067c62747193ad7d6a244bdb9fd09c68a07108
Song TaeHwan <axoghks@gmail.com>
2022-03-02 10:31:58 +0900

    refactor: Separate Data.swift into several files by struct
    Add color presets to Color and randomElement method to Alpha

---
+++ DrawingApp/DrawingApp/Model/Point.swift
@@ -0,0 +1,29 @@
+//
+//  Point.swift
+//  DrawingApp
+//
+//  Created by 송태환 on 2022/03/02.
+//
+
+import Foundation
+
+struct Point {
+    var x: Double
+    var y: Double
+    
+    init(x: Int, y: Int) {
+        self.x = Double(x)
+        self.y = Double(y)
+    }
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+}
+
+extension Point: CustomStringConvertible, Equatable {
+    var description: String {
+        return "X: \(self.x), Y: \(self.y)"
+    }
+}
