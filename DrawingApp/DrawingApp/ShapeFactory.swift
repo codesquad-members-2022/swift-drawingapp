b12dca018df1892de3d933fd77739755a4adfb7a
Song TaeHwan <axoghks@gmail.com>
2022-03-03 11:36:41 +0900

    refactor: Refactor Rectangle and ShapeFactory

--- DrawingApp/DrawingApp/ShapeFactory.swift
+++ DrawingApp/DrawingApp/ShapeFactory.swift
@@ -14,7 +14,7 @@ enum ShapeFactory {
         var identifier: String = ""
         
         for i in 1...length {
-            if let char = Constants.Letter.alphanumeric.randomElement() {
+            if let char = String.alphanumeric.randomElement() {
                 identifier += String(char)
             }
             
@@ -32,44 +32,62 @@ enum ShapeFactory {
         return identifier
     }
     
-    private static func generateRandomPoint() -> Point {
-        let range = Constants.Point.range
-        let x = Double.random(in: range, digits: 2)
-        let y = Double.random(in: range, digits: 2)
+    static func makeRectangle(x: Double = 0, y: Double = 0, width: Double = 30, height: Double = 30) -> Rectangle {
+        let id = self.generateIdentifier(length: 9, delimiter: "-")
+        let alpha = Alpha.randomElement()
         
-        return Point(x: x, y: y)
+        return Rectangle(id: id, x: x, y: y, width: width, height: height, alpha: alpha)
     }
     
-    private static func generateRandomColor() -> Color {
-        let range = Constants.Color.range
+    static func makeRandomRectangle() -> Rectangle {
+        let id = self.generateIdentifier(length: 9, delimiter: "-")
+        let point = PointFactory.makeType()
+        let size = SizeFactory.makeType()
+        let color = ColorFactory.makeType()
+        let alpha = Alpha.randomElement()
+        
+        return Rectangle(id: id, origin: point, size: size, color: color, alpha: alpha)
+    }
+}
+
+protocol TypeFactory {
+    associatedtype T
+    static func makeType() -> T
+}
+
+enum ColorFactory: TypeFactory {
+    typealias T = Color
+    
+    static func makeType() -> Color {
+        let range = Color.range
         let red = Double.random(in: range).rounded()
         let green = Double.random(in: range).rounded()
         let blue = Double.random(in: range).rounded()
-        let alpha = Alpha.randomElement()
         
-        return Color(red: red, green: green, blue: blue, alpha: alpha)
+        return Color(red: red, green: green, blue: blue)
     }
+}
+
+enum SizeFactory: TypeFactory {
+    typealias T = Size
     
-    private static func generateRandomSize() -> Size {
-        let range = Constants.Size.range
+    static func makeType() -> Size {
+        let range = Size.range
         let width = Double.random(in: range, digits: 2)
         let height = Double.random(in: range, digits: 2)
         
         return Size(width: width, height: height)
     }
+}
+
+enum PointFactory: TypeFactory {
+    typealias T = Point
     
-    static func makeRectangle(x: Double = 0, y: Double = 0, width: Double = 30, height: Double = 30) -> Rectangle {
-        let id = self.generateIdentifier(length: 9, delimiter: "-")
+    static func makeType() -> Point {
+        let range = Point.range
+        let x = Double.random(in: range, digits: 2)
+        let y = Double.random(in: range, digits: 2)
         
-        return Rectangle(id: id, x: x, y: y, width: width, height: height)
-    }
-    
-    static func makeRandomRectangle() -> Rectangle {
-        let id = self.generateIdentifier(length: 9, delimiter: "-")
-        let point = self.generateRandomPoint()
-        let size = self.generateRandomSize()
-        let color = self.generateRandomColor()
-        
-        return Rectangle(id: id, point: point, size: size, color: color)
+        return Point(x: x, y: y)
     }
 }
