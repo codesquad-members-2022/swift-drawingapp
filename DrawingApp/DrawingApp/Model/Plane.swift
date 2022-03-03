a1be8c75e22122ea7fd9cc2e31610dbd970d9971
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-03 13:26:51 +0900

    Add: Plane.swift 생성 및 Plane 구조체 구현
    - Rectangle 객체배열 프로퍼티, 배열 내 객체 추가 메소드, 서브스크립트 우선 구현

--- DrawingApp/DrawingApp/Model/Plane.swift
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+struct Plane{
+    
+    private var rectangles:[Rectangle] = []
+    var count: Int{
+        return rectangles.count
+    }
+    
+    mutating func addRectangle(rectangle: Rectangle){
+        self.rectangles.append(rectangle)
+    }
+    
+    subscript(index: Int = 0)-> Rectangle?{
+        if(index <= 0 || index >= self.rectangles.count){
+            return nil
+        }
+        return self.rectangles[index]
+    }
+}
