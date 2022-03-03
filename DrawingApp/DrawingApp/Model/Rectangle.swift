2bc3b0c76abd5e505fc4fd1f708706e8fc602622
Song TaeHwan <axoghks@gmail.com>
2022-03-03 11:36:41 +0900

    refactor: Refactor Rectangle and ShapeFactory

--- DrawingApp/DrawingApp/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -13,20 +13,23 @@ class Rectangle {
     let backgroundColor: Color
     let point: Point
     let size: Size
+    let alpha: Alpha
     
     // MARK: - Initializers
-    init(id: String, point: Point, size: Size, color: Color = .white) {
+    init(id: String, origin: Point, size: Size, color: Color = .white, alpha: Alpha) {
         self.id = id
-        self.point = point
+        self.point = origin
         self.size = size
         self.backgroundColor = color
+        self.alpha = alpha
     }
     
-    init(id: String, x: Double, y: Double, width: Double, height: Double, color: Color = .white) {
+    init(id: String, x: Double, y: Double, width: Double, height: Double, color: Color = .white, alpha: Alpha) {
         self.id = id
         self.point = Point(x: x, y: y)
         self.size = Size(width: width, height: height)
         self.backgroundColor = color
+        self.alpha = alpha
     }
 }
 
