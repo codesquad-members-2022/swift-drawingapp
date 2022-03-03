807f4784237fef2f1609eebdfed57d474dbe4a9a
Jinsujin <sylpid003@gmail.com>
2022-03-03 16:28:04 +0900

    Ref: Rectanble팩토리의 역할에 맞지 않는 메서드 제거 & 분리

--- DrawingApp/DrawingApp/Point.swift
+++ DrawingApp/DrawingApp/Point.swift
@@ -10,4 +10,16 @@ import Foundation
 struct Point {
     let x: Double
     let y: Double
+    
+    static let zero = Point(x: 0, y: 0)
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+    
+    init(x: Int, y: Int) {
+        self.x = Double(x)
+        self.y = Double(y)
+    }
 }
