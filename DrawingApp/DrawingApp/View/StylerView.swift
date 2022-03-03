bd611d237f3c41e04ff3c12ff68026bae637f518
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-03 22:50:18 +0900

    Feat: StylerView 클래스 UIView 설정 추가
    - UIView 배경, 사이즈 설정
    - 서브 뷰로 들어갈 텍스트라벨, 슬라이더 추가

--- DrawingApp/DrawingApp/View/StylerView.swift
+++ DrawingApp/DrawingApp/View/StylerView.swift
@@ -3,4 +3,53 @@ import UIKit
 
 class StylerView: UIView{
     
+    private var rectangleColorTextLabel: UILabel = UILabel()
+    private var rectangleColorValueField: UILabel = UILabel()
+    private var rectangleAlphaTextLabel: UILabel = UILabel()
+    private var rectangleAlphaSlider: UISlider = UISlider()
+    
+    init(frame: CGRect, backgroundColor: UIColor){
+        super.init(frame: frame)
+        self.backgroundColor = backgroundColor
+        setRectangleColorInformationView()
+        setRectangleAlphaInformationView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setRectangleColorInformationView(){
+        self.rectangleColorTextLabel.text = "배경색"
+        self.rectangleColorTextLabel.frame = CGRect(x: self.frame.width*0.1,
+                                                    y: self.frame.height*0.07,
+                                                    width: 100,
+                                                    height: self.frame.height*0.05)
+        self.addSubview(self.rectangleColorTextLabel)
+        
+        self.rectangleColorValueField.frame = CGRect(x: self.frame.width*0.1,
+                                                     y: self.rectangleColorTextLabel.frame.maxY+10,
+                                                     width: self.frame.width*0.75,
+                                                     height: self.frame.height*0.05)
+        self.rectangleColorValueField.layer.cornerRadius = 10
+        self.rectangleColorValueField.layer.borderWidth = 1
+        self.addSubview(self.rectangleColorValueField)
+    }
+    
+    private func setRectangleAlphaInformationView(){
+        self.rectangleAlphaTextLabel.text = "투명도"
+        self.rectangleAlphaTextLabel.frame = CGRect(x: self.frame.width*0.1,
+                                                    y: self.rectangleColorValueField.frame.maxY+25,
+                                                    width: 100,
+                                                    height: self.frame.height*0.05)
+        self.addSubview(self.rectangleAlphaTextLabel)
+        
+        self.rectangleAlphaSlider.frame = CGRect(x: self.frame.width*0.1,
+                                                 y: self.rectangleAlphaTextLabel.frame.maxY+10,
+                                                 width: self.frame.width*0.75,
+                                                 height: 10)
+        self.rectangleAlphaSlider.value = 10
+        self.addSubview(self.rectangleAlphaSlider)
+    }
+    
 }
