5402c5a4d2573aecd7d0e2194688b96ed29e9bbe
asqw887 <asqw887@gmail.com>
2022-03-03 23:03:06 +0900

    feature : Rectangle, RectangleCreator Protocol

--- DrawingApp/DrawingApp/Rectangle.swift
+++ DrawingApp/DrawingApp/Rectangle.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+
+protocol RectangleCreator {
+    
+    func createRandomRectangle() -> Rectangle
+    func createRectangle(size: Size, position: Point) -> Rectangle
+    func createRectangle(x: Double, y: Double, width: Double, height: Double) -> Rectangle
+    func createColoredRectangle(size: Size, position: Point, color: Color, alpha: Alpha) -> Rectangle
+    
+}
+
+
+class Rectangle: CustomStringConvertible{
+    
+    private var ID: String
+    private var size: Size
+    private var position: Point
+    private var backgroundColor: Color
+    private var alpha: Alpha
+    
+    required init(ID: String, size: Size, position: Point, backgroundColor: Color, alpha: Alpha) {
+        self.ID = ID
+        self.size = size
+        self.position = position
+        self.backgroundColor = backgroundColor
+        self.alpha = alpha
+    }
+    
+    var description: String {
+        return "\(ID), \(size), \(position), \(backgroundColor), \(alpha)"
+    }
+
+}
