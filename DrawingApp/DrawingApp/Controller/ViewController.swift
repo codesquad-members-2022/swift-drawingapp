6bad311b55655c561121511bf37b4d2fcb2c9624
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-03 22:51:08 +0900

    Feat: ViewController에 CanvasView, StyleView 서브 뷰로 추가

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -4,14 +4,35 @@ import OSLog
 class ViewController: UIViewController {
     
     private var logger: Logger = Logger()
+    private var canvasView: CanvasView?
+    private var stylerView: StylerView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let factory = RectangleFactory()
-        for index in 0..<4{
-            let rectangle = factory.createRenctangle()
-            logger.debug("Rectangle\(index+1) : \(rectangle)")
-        }
+        initializeAllUIViews()
     }
+    
+    private func initializeAllUIViews(){
+        setCanvasView()
+        setStylerView()
+    }
+    
+    private func setCanvasView(){
+        let frame = CGRect(x: self.view.frame.minX, y: self.view.frame.minY, width: self.view.frame.width*0.8, height: self.view.frame.height)
+        let canvasView = CanvasView(frame: frame, backGroundColor: .lightGray, buttonActionClosure: {
+            print("closure")
+        })
+        self.canvasView = canvasView
+        self.view.addSubview(canvasView)
+    }
+    
+    private func setStylerView(){
+        guard let canvasView = self.canvasView else { return }
+        let frame = CGRect(x: canvasView.frame.width, y: self.view.frame.minY, width: self.view.frame.width - canvasView.frame.width, height: self.view.frame.height)
+        let stylerView = StylerView(frame: frame, backgroundColor: .white)
+        self.stylerView = stylerView
+        self.view.addSubview(stylerView)
+    }
+
 }
 
