4a971a00429ef058d20be1e2f3d3ebd14bcef325
sol <hskim6521@naver.com>
2022-03-03 14:13:52 +0900

    Feat: Plane 객체 선언
    해당 객체는 생성된 Rectangle객체를 보관
    요구사항에 맞는 기능을 제공하도록 설정
    Plane의 isRectangleExist(in:) 메서드를 위해 Rectangle 클래스에 hasSamePoint(_:) 메서드 선언
    Rectangle클래스의 hasSamePoint(_:) 메서드를 위해 Point 타입 클래스는 Equatable 프로토콜 채택

---
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -0,0 +1,26 @@
+//
+//  Plane.swift
+//  DrawingApp
+//
+//  Created by 김한솔 on 2022/03/03.
+//
+
+import Foundation
+import UIKit
+
+class Plane {
+    private var rectangles = [Rectangle]()
+    var count: Int {
+        return rectangles.count
+    }
+    
+    subscript(_ index: Int) -> Rectangle {
+        return rectangles[index]
+    }
+    
+    public func isRectangleExist(in point: Point) -> Bool {
+        return rectangles.filter { rectangle in
+            rectangle.hasSamePoint(point)
+        }.count != 0
+    }
+}
