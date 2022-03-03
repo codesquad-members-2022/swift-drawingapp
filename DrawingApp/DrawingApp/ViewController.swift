99c20b676bf42ebed9fd9fd11e087343adbc42bf
sol <hskim6521@naver.com>
2022-03-03 16:29:27 +0900

    Refactor: generateButton의 동작 방식 변경
    UIAction을 이용해 generateButton의 이벤트 동작 설정해주는 방식으로 변경

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     
     let plane = Plane()
     var factory: RectangleFactory?
-    weak var generateRectangleButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,25 +24,22 @@ class ViewController: UIViewController {
         self.factory = factory
         factory.delegate = self
         
-        generateRectangleButton?.addTarget(self, action: #selector(generateRectangleButtonTouched), for: .touchUpInside)
     }
     
     func addGenerateRectangleButton() {
+        let buttonUIAction = UIAction { _ in
+            let _ = self.factory?.generateRandomRectangle()
+        }
+        let generateButton = UIButton(type: .custom, primaryAction: buttonUIAction)
         let buttonWidth = 100.0
         let buttonHeight = 100.0
         let buttonX = self.view.safeAreaLayoutGuide.layoutFrame.width/2.0 - buttonWidth/2.0
         let buttonY = self.view.safeAreaLayoutGuide.layoutFrame.height - buttonHeight
-        let generateButton = UIButton.init(frame: CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight))
+        generateButton.frame = CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight)
         generateButton.backgroundColor = .gray
-        generateButton.isEnabled = true
         generateButton.setTitle("사각형 생성", for: .normal)
         
-        self.generateRectangleButton = generateButton
         self.view.addSubview(generateButton)
-    }
-    
-    @objc func generateRectangleButtonTouched() {
-        let newRectangle = factory?.generateRandomRectangle()
     }
 
 
