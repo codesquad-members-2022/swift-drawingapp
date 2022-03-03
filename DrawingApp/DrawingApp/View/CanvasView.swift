60f5ba7c92847bfe07cfd1f3a4e989e71185c9a3
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-03 22:00:39 +0900

    Feat: CanvasView 클래스 UIView 설정 추가
    - CanvasView 규격, 배경색 설정
    - 서브 뷰로 사각형 추가 버튼 추가
    - 버튼 배경색, 타이틀 문자, 테두리 스타일 설정

--- DrawingApp/DrawingApp/View/CanvasView.swift
+++ DrawingApp/DrawingApp/View/CanvasView.swift
@@ -3,4 +3,38 @@ import UIKit
 
 class CanvasView: UIView{
     
+    private var generatingButton: UIButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setGeneratingButton()
+    }
+        
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setGeneratingButton(){
+        let buttonWidth = self.frame.width*0.15
+        let buttonHeight = self.frame.height*0.15
+        self.generatingButton.frame = CGRect(x: 0, y: self.frame.height-buttonHeight, width: buttonWidth, height: buttonHeight*0.8)
+        self.generatingButton.center.x = self.center.x
+        
+        self.generatingButton.backgroundColor = .white
+        self.generatingButton.layer.cornerRadius = 10
+        self.generatingButton.layer.borderWidth = 2
+        
+        self.generatingButton.setTitle("사각형🖌", for: .normal)
+        self.generatingButton.setTitleColor(.black, for: .normal)
+        self.generatingButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        
+        self.addSubview(generatingButton)
+    }
+    
+    func setGeneratingButtonAction(buttonActionClosure: @escaping ()->Void){
+        let buttonAction:UIAction = UIAction(title: ""){ _ in
+            buttonActionClosure() }
+        self.generatingButton.addAction(buttonAction, for: .touchDown)
+    }
+
 }
