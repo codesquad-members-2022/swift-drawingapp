cbe119aab9756ac4ef52eeca29ffd1f803a94981
asqw887 <asqw887@gmail.com>
2022-03-03 23:02:09 +0900

    feature : Alpha,Color,Point,Size 구현

--- DrawingApp/DrawingApp/Color.swift
+++ DrawingApp/DrawingApp/Color.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Color: CustomStringConvertible{
+    
+    private var red: Int = 0
+    private var green: Int = 0
+    private var blue: Int = 0
+    
+    init(R: Int, G: Int, B: Int)  {
+        red = R
+        green = G
+        blue = B
+    }
+    
+    init() {
+        red = Int.random(in: 0...255)
+        green = Int.random(in: 0...255)
+        blue = Int.random(in: 0...255)
+    }
+    
+    var description: String {
+        return "R : \(red), G: \(green), B: \(blue)"
+    }
+    
+}
--- DrawingApp/DrawingApp/Point.swift
+++ DrawingApp/DrawingApp/Point.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
-struct Position{
+struct Point: CustomStringConvertible{
     
-    var x: Double
-    var y: Double
+    private var x: Double
+    private var y: Double
     
+    init(x: Double, y: Double){
+        self.x = x
+        self.y = y
+    }
+    
+    init(){
+        let randomX = Double.random(in: 0...900.0)
+        let randomY = Double.random(in: 0...650.0)
+        self.init(x: randomX, y: randomY)
+    }
+    
+    var description: String {
+        return "X : \(x), Y: \(y)"
+    }
+
 }
