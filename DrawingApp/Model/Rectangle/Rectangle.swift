2399836c34de521e4dbb4ad2a0050dc4534ca43b
wnsxor1993 <wnsxor1993@naver.com>
2022-03-03 15:57:53 +0900

    Feat: 속성 변경 기능 구현 - 선택된 사각형의 RGB와 Alpha 값을 slider를 통해 변경 가능

--- DrawingApp/Model/Rectangle/Rectangle.swift
+++ DrawingApp/Model/Rectangle/Rectangle.swift
@@ -11,8 +11,8 @@ final class Rectangle: CustomStringConvertible{
     private let id: String
     private let size: MySize
     private let point: MyPoint
-    private let color: RGBColor
-    private let alpha: Alpha
+    private var color: RGBColor
+    private var alpha: Alpha
     
     var description: String{
         let description = "[\(id)] : (X: \(point.xValue()), Y:\(point.yValue())) / (W: \(size.widthValue()), H:\(size.heightValue())) / (R: \(color.redValue()), G: \(color.greenValue()), B: \(color.blueValue())) / Alpha: \(showAlpha())"
@@ -37,6 +37,35 @@ final class Rectangle: CustomStringConvertible{
     
     func showAlpha() -> Alpha{
         return alpha
+    }
+    
+    func changeColor(color: RGBColor){
+        self.color = color
+    }
+    
+    func changeAlpha(alpha: Double){
+        switch alpha{
+        case 0.9..<1:
+            self.alpha = .nine
+        case 0.8..<0.9:
+            self.alpha = .eight
+        case 0.7..<0.8:
+            self.alpha = .seven
+        case 0.6..<0.7:
+            self.alpha = .six
+        case 0.5..<0.6:
+            self.alpha = .five
+        case 0.4..<0.5:
+            self.alpha = .four
+        case 0.3..<0.4:
+            self.alpha = .three
+        case 0.2..<0.3:
+            self.alpha = .two
+        case 0.0..<0.2:
+            self.alpha = .one
+        default:
+            self.alpha = .ten
+        }
     }
     
     init(id: String, size: MySize, point: MyPoint, color: RGBColor, alpha: Alpha){
--- DrawingApp/VC/MainViewController.swift
+++ DrawingApp/VC/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     let rightAttributerView = RightAttributerView()
     
     private var selectedRectangleView: UIView?
+    private var selectedRectangleIndex: Int?
     
     @IBOutlet weak var rectangleButton: UIButton!
     
@@ -23,6 +24,11 @@ class MainViewController: UIViewController {
         rectangleButton.layer.cornerRadius = 15
         self.view.addSubview(rightAttributerView)
         rightAttributerView.layout()
+        
+        rightAttributerView.redSlider.addTarget(self, action: #selector(self.changeRectangleAttribute), for: .valueChanged)
+        rightAttributerView.greenSlider.addTarget(self, action: #selector(self.changeRectangleAttribute), for: .valueChanged)
+        rightAttributerView.blueSlider.addTarget(self, action: #selector(self.changeRectangleAttribute), for: .valueChanged)
+        rightAttributerView.alphaSlider.addTarget(self, action: #selector(self.changeRectangleAttribute), for: .valueChanged)
         
         let tapGestureRecognizer = UITapGestureRecognizer()
         tapGestureRecognizer.delegate = self
@@ -46,13 +52,29 @@ class MainViewController: UIViewController {
         os_log("%@", "\(rectangle.description)")
         self.view.addSubview(rectangleView)
     }
+    
+    @objc func changeRectangleAttribute(){
+        guard let index = selectedRectangleIndex, let rectangle = self.rectangles[index], let rectView = selectedRectangleView else{
+            return
+        }
+        let newColor = RGBColor(red: rightAttributerView.redValue, green: rightAttributerView.greenValue, blue: rightAttributerView.blueValue)
+        let newAlpha = rightAttributerView.alphaValue
+        
+        rectangle.changeAlpha(alpha: newAlpha)
+        rectangle.changeColor(color: newColor)
+        
+        rectView.backgroundColor = UIColor(red: rectangle.showColor().redValue(), green: rectangle.showColor().greenValue(), blue: rectangle.showColor().blueValue(), alpha: rectangle.showAlpha().showValue())
+        
+        rightAttributerView.changeAllSliderValue()
+    }
 }
 
 extension MainViewController: UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
         let touchPoint = touch.location(in: self.view)
+        findSelectedRectangle(point: touchPoint)
         
-        guard let index = findSelectedRectangle(point: touchPoint), let rectangle = self.rectangles[index] else{
+        guard let index = selectedRectangleIndex, let rectangle = self.rectangles[index] else{
             return true
         }
         
@@ -64,9 +86,9 @@ extension MainViewController: UIGestureRecognizerDelegate {
         return true
     }
     
-    private func findSelectedRectangle(point: CGPoint) -> Int?{
+    private func findSelectedRectangle(point: CGPoint){
         guard let rectangleInPlane = rectangles.findRectangle(withX: point.x, withY: point.y) else{
-            return nil
+            return
         }
         
         self.view.subviews.forEach{ rectangle in
@@ -74,8 +96,7 @@ extension MainViewController: UIGestureRecognizerDelegate {
                 return
             }
             self.selectedRectangleView = rectangle
+            self.selectedRectangleIndex = rectangles.findRectangleIndex(rectangle: rectangleInPlane)
         }
-        
-        return rectangles.findRectangleIndex(rectangle: rectangleInPlane)
     }
 }
