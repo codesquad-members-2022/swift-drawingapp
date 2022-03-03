20c87e7a344d015505216c48e46952c4e40e9226
wnsxor1993 <wnsxor1993@naver.com>
2022-03-04 01:17:02 +0900

    Feat: Delegate 추가 - RightAttributerView Delegate 추가하여 Slider 액션 구현 - VC 내에 구현한 UIView 서칭 기능 Plane 내로 변경

---
+++ DrawingApp/View/Delegate/RightAttributerViewDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  RightAttributerViewDelegate.swift
+//  DrawingApp
+//
+//  Created by juntaek.oh on 2022/03/03.
+//
+
+import Foundation
+
+protocol RightAttributerViewDelegate{
+    func moveAlphaSlider()
+    func moveRedSlider()
+    func moveGreenSlider()
+    func moveBlueSlider()
+}
