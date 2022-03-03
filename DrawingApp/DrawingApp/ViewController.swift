f4a66f11f0d7cd47cbd090a78c71ac0910f3ad1f
sol <hskim6521@naver.com>
2022-03-03 15:59:43 +0900

    Feat: ViewController 클래스에 버튼 생성
    ViewController 클래스에 addGenerateRectangleButton() 메서드 추가
    ViewController 클래스 프로퍼티로 factory, generateRectangleButton 추가

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -11,18 +11,36 @@ import OSLog
 class ViewController: UIViewController {
     
     let plane = Plane()
+    var factory: RectangleFactory?
+    weak var generateRectangleButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addGenerateRectangleButton()
+        
         let currentScreenSize = (width: Double(self.view.safeAreaLayoutGuide.layoutFrame.width),
                                  height: Double(self.view.safeAreaLayoutGuide.layoutFrame.height))
         let factory = RectangleFactory(screenSize: currentScreenSize)
+        self.factory = factory
         factory.delegate = self
         let rect1 = factory.generateRandomRectangle()
         let rect2 = factory.generateRandomRectangle()
         let rect3 = factory.generateRandomRectangle()
         let rect4 = factory.generateRandomRectangle()
+    func addGenerateRectangleButton() {
+        let buttonWidth = 100.0
+        let buttonHeight = 100.0
+        let buttonX = self.view.safeAreaLayoutGuide.layoutFrame.width/2.0 - buttonWidth/2.0
+        let buttonY = self.view.safeAreaLayoutGuide.layoutFrame.height - buttonHeight
+        let generateButton = UIButton.init(frame: CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight))
+        generateButton.backgroundColor = .gray
+        generateButton.isEnabled = true
+        generateButton.setTitle("사각형 생성", for: .normal)
+        
+        self.generateRectangleButton = generateButton
+        self.view.addSubview(generateButton)
+    }
     }
 
 
