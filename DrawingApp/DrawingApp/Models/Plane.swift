4799dba7edf2b3dc3335ef934bfa138e79118ffa
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-03-03 22:44:49 +0900

    Update selecting view user interface
    스크린 뷰 안에 생성된 뷰를 선택할 수 있는 유저 인터페이스를 구현합니다.
    Plane 구조체에 필요한 인터페이스를 구현하였습니다.

--- DrawingApp/DrawingApp/Models/Plane.swift
+++ DrawingApp/DrawingApp/Models/Plane.swift
@@ -8,5 +8,30 @@
 import Foundation
 
 class Plane {
+    private var properties = [ViewRandomProperty]()
     
+    func addProperties(_ model: ViewRandomProperty) {
+        properties.append(model)
+    }
+    
+    func getRectangleCount() -> Int {
+        properties.count
+    }
+    
+    func getRectangleProperty(at index: Int) -> ViewRandomProperty? {
+        guard properties.count-1 >= index else {
+            return nil
+        }
+        
+        return properties[index]
+    }
+    
+    func hasAnyRectangle(in rect: RectPoint) -> Bool {
+        properties.contains {
+            $0.point.x >= rect.x
+            && $0.point.y >= rect.y
+            && $0.point.x+$0.size.width <= rect.x
+            && $0.point.y+$0.size.height <= rect.y
+        }
+    }
 }
