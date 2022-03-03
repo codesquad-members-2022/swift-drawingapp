36c6421bb7d679a9017c7aee6f4e4f122f90ebec
Damagucci-Juice <jsr9874@gmail.com>
2022-03-03 20:53:57 +0900

    Feat : 4개의 직사각형 모델을 os_log() 로 프린트

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -2,9 +2,15 @@ import UIKit
 import OSLog
 
 class ViewController: UIViewController {
-
+    var rectangleArray: [Factory.RectangleView] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        for index in 1...4 {
+            let rect = Factory.createRandomRectangle(name: "Rect\(index)")
+            rectangleArray.append(rect)
+        }
+        for rectangle in rectangleArray {
+            os_log("\(rectangle.name) \(rectangle.description)")
+        }
     }
 }
