05df47a70ea066065936d01858364bcff6637a4c
asqw887 <asqw887@gmail.com>
2022-03-03 23:05:07 +0900

    test : Rectangle 로그 출력

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import os
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let factory = RectangleFactory()
+        let rect1 = factory.createRandomRectangle()
+        let rect2 = factory.createRectangle(x: 100, y: 150, width: 300.0, height: 300.0)
+        
+        let log = Logger()
+        log.info("Rect1 : \(rect1)")
+        log.info("Rect2 : \(rect2)")
     }
-
-
 }
 
