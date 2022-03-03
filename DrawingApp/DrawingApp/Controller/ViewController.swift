f15083ea86d6ca3821965841d3c1cd5b531afb5d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-03 23:33:01 +0900

    Feat: CanvasView의 버튼 클릭시 사각형 그리는 기능 추가
    - 버튼 클릭시 뷰컨트롤러의 drawRectangle() 메소드 실행
    - Rectangle 인스턴스 생성해서 여기에 맞는 UIView 인스턴스 생성
    - 생성한 UIView를 Plane에 추가한 후, CanvasView의 서브 뷰로 추가

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -6,6 +6,8 @@ class ViewController: UIViewController {
     private var logger: Logger = Logger()
     private var canvasView: CanvasView?
     private var stylerView: StylerView?
+    private var rectangleFactory: RectangleFactory = RectangleFactory()
+    private var plane: Plane = Plane()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,20 +20,48 @@ class ViewController: UIViewController {
     }
     
     private func setCanvasView(){
-        let frame = CGRect(x: self.view.frame.minX, y: self.view.frame.minY, width: self.view.frame.width*0.8, height: self.view.frame.height)
-        let canvasView = CanvasView(frame: frame, backGroundColor: .lightGray, buttonActionClosure: {
-            print("closure")
-        })
+        let frame = CGRect(x: self.view.frame.minX,
+                           y: self.view.frame.minY,
+                           width: self.view.frame.width*0.8,
+                           height: self.view.frame.height)
+        let canvasView = CanvasView(frame: frame,
+                                    backGroundColor: .lightGray,
+                                    buttonActionClosure: self.drawRectangle)
         self.canvasView = canvasView
         self.view.addSubview(canvasView)
     }
     
     private func setStylerView(){
         guard let canvasView = self.canvasView else { return }
-        let frame = CGRect(x: canvasView.frame.width, y: self.view.frame.minY, width: self.view.frame.width - canvasView.frame.width, height: self.view.frame.height)
-        let stylerView = StylerView(frame: frame, backgroundColor: .white)
+        let frame = CGRect(x: canvasView.frame.width,
+                           y: self.view.frame.minY,
+                           width: self.view.frame.width - canvasView.frame.width,
+                           height: self.view.frame.height)
+        let stylerView = StylerView(frame: frame,
+                                    backgroundColor: .white)
         self.stylerView = stylerView
         self.view.addSubview(stylerView)
+    }
+    
+    func drawRectangle(){
+        guard let canvasView = self.canvasView else { return }
+        let rectangle = rectangleFactory.createRenctangle(maxX: canvasView.bounds.maxX - 50,
+                                                          maxY: canvasView.bounds.maxY - 250,
+                                                          minWidth: 150,
+                                                          minHeight: 150,
+                                                          maxWidth: 180,
+                                                          maxHeight: 180)
+        let rectangleView = UIView(frame: CGRect(x: rectangle.point.x,
+                                                 y: rectangle.point.y,
+                                                 width: rectangle.size.width,
+                                                 height: rectangle.size.height))
+        rectangleView.backgroundColor = UIColor(red: rectangle.backgroundColor.r,
+                                                green: rectangle.backgroundColor.g,
+                                                blue: rectangle.backgroundColor.b,
+                                                alpha: CGFloat(rectangle.alpha.opacity))
+        
+        plane.addRectangle(rectangle)
+        canvasView.addSubview(rectangleView)
     }
 
 }
