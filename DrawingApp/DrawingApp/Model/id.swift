ffc70e80182fff0fea236b9305607380a4237d96
sol <hskim6521@naver.com>
2022-03-04 15:31:52 +0900

    Refactor: 랜덤한 ID를 생성하는 위치를 ID 클래스 내부로 이동
    ID 클래스에서 랜덤한 아이디를 생성, 검증하도록 역할 확장

--- DrawingApp/DrawingApp/Model/id.swift
+++ DrawingApp/DrawingApp/Model/id.swift
@@ -19,6 +19,19 @@ struct ID {
 
     }
     
+    init() {
+        self.description = ID.generateRandomID()
+    }
+    
+    private static func generateRandomID() -> String {
+        func generateRandomString(length: Int) -> String {
+            return String(NSUUID().uuidString.prefix(length))
+        }
+        
+        let randomIDParts = [generateRandomString(length: 3), generateRandomString(length: 3), generateRandomString(length: 3)]
+        return randomIDParts.joined(separator: "-")
+    }
+    
     private static func isCorrectFormat(_ id: String) -> Bool {
         let correctFormat = "^[a-zA-Z0-9]..[-][a-zA-Z0-9]..[-][a-zA-Z0-9].."
         if let _ = id.range(of: correctFormat, options: .regularExpression) {
