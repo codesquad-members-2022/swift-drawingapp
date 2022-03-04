a4f276ea67abf3aafae2e44645a08c090c555e3e
bibi <bibi6666667@kakao.com>
2022-03-04 22:12:27 +0900

    Size, Point, BackgroundColor 클래스 생성

---
+++ DrawingApp/DrawingApp/Point.swift
@@ -0,0 +1,18 @@
+//
+//  Point.swift
+//  DrawingApp
+//
+//  Created by Bibi on 2022/03/04.
+//
+
+import Foundation
+
+class Point {
+    private let x: Double
+    private let y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+}
