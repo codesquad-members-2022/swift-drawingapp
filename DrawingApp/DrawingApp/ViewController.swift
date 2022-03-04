98a6521db37bb66fd05369b09f3a4ae642ebd440
sungju-kim <wnwn0719@gmail.com>
2022-03-04 14:05:04 +0900

    Refactor: Color, Alpha init 변경에 따른 옵셔널 바인딩 추가

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -9,20 +9,20 @@ import UIKit
 import OSLog
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let screenSize = (Double(self.view.safeAreaLayoutGuide.layoutFrame.size.width), Double(self.view.safeAreaLayoutGuide.layoutFrame.size.height))
         let rectangleFactory = RectangleFactory(screenSize: screenSize)
-        let rect1 = rectangleFactory.makeRandomRectangle()
-        let rect2 = rectangleFactory.makeRandomRectangle()
-        let rect3 = rectangleFactory.makeRandomRectangle()
-        let rect4 = rectangleFactory.makeRandomRectangle()
+        guard let rect1 = rectangleFactory.makeRandomRectangle() else {return}
+        guard let rect2 = rectangleFactory.makeRandomRectangle() else {return}
+        guard let rect3 = rectangleFactory.makeRandomRectangle() else {return}
+        guard let rect4 = rectangleFactory.makeRandomRectangle() else {return}
         for rect in [rect1,rect2,rect3,rect4] {
             os_log("\(rect)")
         }
     }
-
-
+    
+    
 }
 
