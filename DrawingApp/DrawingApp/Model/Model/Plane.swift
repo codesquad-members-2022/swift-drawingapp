0fc17944c9682b41c9f851501919b03e8dd7f258
BumgeunSong <isp1195@gmail.com>
2022-03-04 16:27:06 +0900

    Add notification name, post to Plane

--- DrawingApp/DrawingApp/Model/Model/Plane.swift
+++ DrawingApp/DrawingApp/Model/Model/Plane.swift
@@ -7,17 +7,11 @@
 
 import Foundation
 
-protocol PlaneCanvasDelegate {
-    func didAddViewModels(_ new: [ViewModel])
-    func didSelectViewModels(_ new: ViewModel?, _ old: ViewModel?)
-    func didMutateColorViewModels(_ mutated: ColorMutable)
-    func didMutateAlphaViewModels(_ mutated: AlphaMutable)
-}
-
-protocol PlanePanelDelegate {
-    func didSelectViewModels(_ selected: ViewModel?)
-    func didMutateColorViewModels(_ mutated: ColorMutable)
-    func didMutateAlphaViewModels(_ mutated: AlphaMutable)
+extension Notification.Name {
+    static let addViewModel = Notification.Name("addViewModel")
+    static let selectViewModel = Notification.Name("selectViewModel")
+    static let mutateColorViewModel = Notification.Name("mutateColorViewModel")
+    static let mutateAlphaViewModel = Notification.Name("mutateAlphaViewModel")
 }
 
 class Plane {
@@ -29,9 +23,6 @@ class Plane {
     }
     
     var selected: ViewModel?
-    var canvasDelegate: PlaneCanvasDelegate?
-    var panelDelgate: PlanePanelDelegate?
-    
     
     var rectangleCount: Int {
         viewModels.filter { $0 is Rectangle }.count
@@ -44,7 +35,7 @@ class Plane {
     func addRectangle() {
         let newRectangle = Factory.createRectangle()
         viewModels.append(newRectangle)
-        canvasDelegate?.didAddViewModels([newRectangle])
+        NotificationCenter.default.post(name: .addViewModel, object: newRectangle)
     }
     
     func tap(on point: Point) {
@@ -52,21 +43,18 @@ class Plane {
         self.selected = viewModels.last(where: { viewModel in
             viewModel.contains(point)
         })
-        panelDelgate?.didSelectViewModels(selected)
-        canvasDelegate?.didSelectViewModels(selected, oldSelected)
+        NotificationCenter.default.post(name: .selectViewModel, object: (old: oldSelected, new: selected))
     }
     
-    func transform(to color: Color) {
+    func transform(to color: Color = Factory.createColor()) {
         guard let mutableViewModel = selected as? ColorMutable else { return }
         mutableViewModel.transform(to: color)
-        canvasDelegate?.didMutateColorViewModels(mutableViewModel)
-        panelDelgate?.didMutateColorViewModels(mutableViewModel)
+        NotificationCenter.default.post(name: .mutateColorViewModel, object: mutableViewModel)
     }
     
     func transform(to alpha: Alpha) {
         guard let mutableViewModel = selected as? AlphaMutable else { return }
         mutableViewModel.transform(to: alpha)
-        canvasDelegate?.didMutateAlphaViewModels(mutableViewModel)
-        panelDelgate?.didMutateAlphaViewModels(mutableViewModel)
+        NotificationCenter.default.post(name: .mutateAlphaViewModel, object: mutableViewModel)
     }
 }
