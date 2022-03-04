50c357c06c1e7b7118cb78908c362a97580b8b36
shingha <shingha1124@gmail.com>
2022-03-04 17:11:26 +0900

    [fix] 컬러값을 Int -> Uint로 변경

--- Drawingapp/Drawingapp/Sources/Model/Color.swift
+++ Drawingapp/Drawingapp/Sources/Model/Color.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct Color: CustomStringConvertible {
-    let r: Int
-    let g: Int
-    let b: Int
+    let r: UInt
+    let g: UInt
+    let b: UInt
     
     var hexColor: String {
         "#\(String(r, radix: 16))\(String(g, radix: 16))\(String(b, radix: 16))"
@@ -20,7 +20,7 @@ struct Color: CustomStringConvertible {
         "R: \(r), G: \(g), B: \(b)"
     }
     
-    init(r: Int, g: Int, b: Int) {
+    init(r: UInt, g: UInt, b: UInt) {
         self.r = r < 0 ? 0 : r > 255 ? 255 : r
         self.g = g < 0 ? 0 : g > 255 ? 255 : g
         self.b = b < 0 ? 0 : b > 255 ? 255 : b
