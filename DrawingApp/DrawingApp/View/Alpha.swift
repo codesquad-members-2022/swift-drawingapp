95575f369714bbb28313f0742265a2520750d731
sanghee <anshee97@naver.com>
2022-03-04 14:39:55 +0900

    Feat: Add Alpha class

---
+++ DrawingApp/DrawingApp/View/Alpha.swift
@@ -0,0 +1,28 @@
+//
+//  Alpha.swift
+//  DrawingApp
+//
+//  Created by Selina on 2022/03/04.
+//
+
+import Foundation
+
+enum Alpha: Int {
+    case one = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+    case nine = 9
+    case ten = 10
+}
+
+
+extension Alpha: CustomStringConvertible {
+    var description: String {
+        return "\(self.rawValue)"
+    }
+}
