9275eba4278d931878d7f683e98f23a8f6800a63
sanghee <anshee97@naver.com>
2022-03-04 11:43:34 +0900

    Feat: Add BackgroundColor class

---
+++ DrawingApp/DrawingApp/View/BackgroundColor.swift
@@ -0,0 +1,27 @@
+//
+//  BackgroundColor.swift
+//  DrawingApp
+//
+//  Created by Selina on 2022/03/04.
+//
+
+import Foundation
+
+class BackgroundColor {
+    private let red: Int
+    private let green: Int
+    private let blue: Int
+    
+    init(red: Int, green: Int, blue: Int) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+}
+
+
+extension BackgroundColor: CustomStringConvertible {
+    var description: String {
+        return "R\(red), G\(green), B:\(blue)"
+    }
+}
