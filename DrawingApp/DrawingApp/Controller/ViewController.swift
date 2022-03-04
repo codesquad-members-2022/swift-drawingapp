007c8933ec49c4df9c40a47b29f5e4e0675589c5
sanghee <anshee97@naver.com>
2022-03-04 16:32:56 +0900

    Feat: Add system log function

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import os
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let factory = RectangleFactory()
+        
+        let rect1 = factory.createRectangle()
+        let rect2 = factory.createRectangle()
+        let rect3 = factory.createRectangle()
+        let rect4 = factory.createRectangle()
+        
+        let rectList = [rect1, rect2, rect3, rect4]
+        let log = Logger()
+        for rectangle in rectList {
+            log.info("Rect: \(rectangle)")
+        }
     }
-
-
 }
 
