d632a7aee2dbb6ed0ce92e3a45a6827ab6f63a08
kimdongjune <jun36817@naver.com>
2022-03-04 11:59:14 +0900

    Rectangle에 맞게 속성을 변경하는 matchProperty 메소드 추가 및 uniqueId 값 설정.

--- DrawingApp/DrawingApp/view/RectangleView.swift
+++ DrawingApp/DrawingApp/view/RectangleView.swift
@@ -9,12 +9,34 @@ import Foundation
 import UIKit
 
 class RectangleView: UIView{
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    private var uniqueId: String?
+    
+    init(){
+        super.init(frame: CGRect())
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
+    
+    func setUniqueId(id: String){
+        self.uniqueId = id
+    }
+    
+    func idValue() -> String{
+        return self.uniqueId ?? ""
+    }
+    
+    func matchProperty(rectangle: Rectangle){
+        self.uniqueId = rectangle.idValue()
+        let point = rectangle.pointValue()
+        let size = rectangle.sizeValue()
+        let rgb = rectangle.colorValue()
+        let alpha = rectangle.alphaValue()
+        frame = CGRect(x: point.xValue(), y: point.yValue(), width: size.widthValue(), height: size.heightValue())
+        backgroundColor = UIColor(cgColor: CGColor(red: rgb.rValue()/255, green: rgb.gValue()/255, blue: rgb.bValue()/255, alpha: 1))
+        self.alpha = CGFloat(alpha)
+    }
+    
 }
 
