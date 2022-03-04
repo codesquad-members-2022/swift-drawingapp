1c22ac1244f26408f49f184d90dd717161b941e7
bibi <bibi6666667@kakao.com>
2022-03-04 22:12:27 +0900

    Size, Point, BackgroundColor 클래스 생성

---
+++ DrawingApp/DrawingApp/BackgroundColor.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundColor.swift
+//  DrawingApp
+//
+//  Created by Bibi on 2022/03/04.
+//
+
+import Foundation
+
+class BackgroundColor {
+    private let red: Int
+    private let green: Int
+    private let blue: Int
+    
+    init(R: Int, G: Int, B: Int) {
+        self.red = R
+        self.green = G
+        self.blue = B
+    }
+}
---
+++ DrawingApp/DrawingApp/Size.swift
@@ -0,0 +1,18 @@
+//
+//  Size.swift
+//  DrawingApp
+//
+//  Created by Bibi on 2022/03/04.
+//
+
+import Foundation
+
+class Size {
+    private let width: Double
+    private let height: Double
+    
+    init(width: Double, height: Double) {
+        self.width = width
+        self.height = height
+    }
+}
