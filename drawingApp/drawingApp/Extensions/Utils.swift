d2f521a53416c0eaf02addb6b2d582d0b979b527
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-03-05 00:35:38 +0900

    [ADD]Color, Double Extensions

--- drawingApp/drawingApp/Extensions/Utils.swift
+++ drawingApp/drawingApp/Extensions/Utils.swift
@@ -10,14 +10,39 @@ import UIKit
 
 extension Double {
     var trim : Double {
-        let str = String(format: "%.1f", self)
+        let str = String(format: "%.f", self)
         return Double(str)!
+    }
+    
+    var scaleRGB : Double {
+        return self/255.0
+    }
+    
+    var scaleAlhpa : Double {
+        return self/10.0
     }
 }
 
 extension CGFloat {
     var trim : Double {
-        let str = String(format: "%.1f", self)
+        let str = String(format: "%.0f", self)
         return Double(str)!
     }
+}
+
+extension Color {
+    
+    var tohexString : String {
+        let rgb:Int = (Int)(self.red)<<16 | (Int)(self.green)<<8 | (Int)(self.blue)<<0
+        return String(format:"#%06x", rgb)
+    }
+    
+   static func getRandomColor() -> Color {
+        let red = Double.random(in: 0..<255)
+        let green = Double.random(in: 0..<255)
+        let blue = Double.random(in: 0..<255)
+        return Color(r: red, g: green, b: blue)
+    }
+    
+    
 }
