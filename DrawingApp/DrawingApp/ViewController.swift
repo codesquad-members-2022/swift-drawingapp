4afd69ca108de3330ab531c6eb8485dd331c5e67
bibi <bibi6666667@kakao.com>
2022-03-04 22:32:13 +0900

    ShapeFactory를 위한 프로토콜과 ShapeFactory 생성

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let factory = ShapeFactory()
+        let rectangle = factory.createShape(shapeType: .rectangle, size: Size(width: 10, height: 10), point: Point(x: 10, y: 10), color: BackgroundColor(R: 0, G: 0, B: 0), alpha: 10)
     }
 
 
