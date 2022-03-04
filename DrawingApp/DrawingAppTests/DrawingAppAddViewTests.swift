e6ddb8697bac80cbc394a153010448009ccd27b1
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-03-04 16:20:17 +0900

    Test step2 and Refactor codes
    모델 테스트를 위해 유닛 테스트 코드를 변경하고 관련 코드들도 수정하였습니다.
    모델 관련 타입을 따로 파일 정의하여 떼어놓고, 모두 CustomStringConvertible 프로토콜을 구현했습니다.
    일련의 작업들은 테스트를 용이하게 하기 위한 작업들이었습니다.

--- DrawingApp/DrawingAppTests/DrawingAppAddViewTests.swift
+++ DrawingApp/DrawingAppTests/DrawingAppAddViewTests.swift
@@ -9,20 +9,42 @@ import XCTest
 @testable import DrawingApp
 
 class DrawingAppAddViewTests: XCTestCase {
-
+    
+    let storyboard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+    
     func testDrawViewExceeded() throws {
         
         let factory = FactoryViewRandomProperty()
-        factory.delegate = ViewController()
+        let vc = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        vc.loadView()
+        
+        if let vc = vc as? MasterViewDelegate {
+            factory.delegate = vc
+        } else {
+            XCTFail("MainViewController not implemeting MasterViewDelegate")
+        }
         
         for i in 1...100 {
-            
-            guard let model = factory.makeRandomView(as: "RandomView #\(i)") else {
+            if factory.makeRandomView(as: "RandomView #\(i)") == nil {
                 XCTFail()
-                continue
+                break
             }
-            
-            print(model)
         }
+        
+        guard let testModel = factory.makeRandomView(as: "Test") else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertFalse(testModel.setPoint(RectPoint(x: -1, y: -1)), "Model set point negative value not execute intentionally.")
+        XCTAssertTrue(testModel.setPoint(RectPoint(x: 1, y: 1)), "Model set point positive value not execute intentionally.")
+        
+        XCTAssertFalse(testModel.setSize(RectSize(width: -1, height: -1)), "Model set size negative value not execute intentionally.")
+        XCTAssertTrue(testModel.setSize(RectSize(width: 1, height: 1)), "Model set size positive value not execute intentionally.")
+        
+        XCTAssertFalse(testModel.setAlpha(-1), "Model set alpha negative value not execute intentionally.")
+        XCTAssertTrue(testModel.setAlpha(1), "Model set alpha positive value not execute intentionally.")
+        
+        LoggerUtil.debugLog(model: testModel)
     }
 }
