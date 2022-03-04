b8d8787bef5e7ce9d90ba73ef86a8217d55ecfcb
bibi <bibi6666667@kakao.com>
2022-03-04 11:46:20 +0900

    DrawingApp 프로젝트 생성

---
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Bibi on 2022/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
