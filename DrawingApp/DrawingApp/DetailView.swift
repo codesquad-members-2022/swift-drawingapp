1578709227b46d8e473d36c1799ece392747ef5b
P_SeoB <akdlsjtm21@naver.com>
2022-03-05 13:43:15 +0900

    [Refactory] DetailView ComputedPropery -> StoreProperty

--- DrawingApp/DrawingApp/DetailView.swift
+++ DrawingApp/DrawingApp/DetailView.swift
@@ -10,16 +10,16 @@ import UIKit
 class DetailView: UIView {
     
     //배경색
-    var backgroundColorLabel:UILabel {
+    var backgroundColorLabel:UILabel = {
         let lable = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 50))
         lable.text = "배경색"
         lable.textColor = .black
         lable.font = .systemFont(ofSize: 18)
         return lable
-    }
+    }()
     
     //배경색 rgb정보
-    var backgroundColorButton:UIButton {
+    var backgroundColorButton:UIButton = {
         let button = UIButton(frame: CGRect(x: 20, y: 70, width: 150, height: 50))
         button.setTitle("########", for: .normal)
         button.layer.cornerRadius = 10
@@ -27,40 +27,28 @@ class DetailView: UIView {
         button.layer.borderColor = UIColor.black.cgColor
         button.setTitleColor(.black, for: .normal)
         return button
-    }
+    }()
     
     //투명도 label
-    var alphaLabel:UILabel {
+    var alphaLabel:UILabel = {
         let lable = UILabel(frame: CGRect(x: 20, y: 160, width: 180, height: 50))
-        lable.text = "투명도"
+        lable.text = "투명도 0"
         lable.textColor = .black
         lable.font = .systemFont(ofSize: 18)
         return lable
-    }
+    }()
     
     //투명도 Slider
-    var alphaSlider:UISlider {
+    var alphaSlider:UISlider = {
         let slider = UISlider(frame: CGRect(x: 20, y: 200, width: 150, height: 50))
         slider.minimumValue = Alpha.minValue
         slider.maximumValue = Alpha.maxValue
         slider.isContinuous = false
         slider.tintColor = .black
 
-        slider.addTarget(
-            self,
-            action: #selector(sliderValueChange(_ :)),
-            for: .valueChanged
-        )
-
         return slider
-    }
+    }()
     
-    
-   //TODO: Change Alpha
-    @objc func sliderValueChange(_ sender:UISlider) {
-    }
-    
-
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -79,6 +67,5 @@ class DetailView: UIView {
             self.addSubview($0)
         }
         
-
     }
 }
