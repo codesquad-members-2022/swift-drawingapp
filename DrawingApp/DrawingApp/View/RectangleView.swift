44831d56337a7a0bcda50f859ffede4f214368a6
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-06 21:51:38 +0900

    Add: RectangleView.swift 추가
    - UIView를 상속받는 RectangleView 클래스 추가

---
+++ DrawingApp/DrawingApp/View/RectangleView.swift
@@ -0,0 +1,14 @@
+import Foundation
+import UIKit
+
+class RectangleView: UIView{
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+        
+}
