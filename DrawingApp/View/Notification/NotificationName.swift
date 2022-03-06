0c8648bc4b27f3624aafd65112277076c44e8efe
wnsxor1993 <wnsxor1993@naver.com>
2022-03-06 23:08:00 +0900

    Feat: Notification 구현 - Observer 패턴 연습을 위한 Notification 구현

---
+++ DrawingApp/View/Notification/NotificationName.swift
@@ -0,0 +1,18 @@
+//
+//  RightAttributerViewDelegate.swift
+//  DrawingApp
+//
+//  Created by juntaek.oh on 2022/03/03.
+//
+
+import Foundation
+
+extension Notification.Name{
+    static let makeRectangle = Notification.Name("makeRectangle")
+    static let tapGesture = Notification.Name("tapGesture")
+    
+    static let changeAlpha = Notification.Name("changeAlpha")
+    static let changeRedColor = Notification.Name("changeRedColor")
+    static let changeGreenColor = Notification.Name("changeGreenColor")
+    static let changeBlueColor = Notification.Name("changeBlueColor")
+}
