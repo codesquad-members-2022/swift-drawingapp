4ad64e13b41b228b9b41d6ee640fe7d6860a017b
sol <hskim6521@naver.com>
2022-03-06 22:06:47 +0900

    Feat: 앱의 backgroundColor 버튼을 누르면 선택된 View의 backgroundColor를 랜덤하게 바꿔주는 기능 추가
    ViewController에서 선택된 View를 프로퍼티로 가지도록 touchedView 프로퍼티 선언
    Rectangle 객체의 backgroundColor를 랜덤하게 변화시키는 changeBackgroundColorRandomly() 메서드 구현
    ViewController로부터 특정 View의 색깔을 바꾸라는 지시를 받으면 Plane에서 해당하는 Rectangle의 색을 바꿔주는 changeBackgroundColorOfRectanlge(id:) 메서드 추가
    Rectangle 객체의 배경 색이 바뀌면 특정 View의 색깔도 바뀌는 Delegate 패턴을 위한 ChangeRectangleBackgroundColorDelegate 프로토콜 선언
    ViewController가 해당 역할을 위임받아 View의 색깔을 바꿀 수 있도록 하는 rectangleDidChangeBackgroundColor(to: alpha:) 메서드 구현

--- DrawingApp/DrawingApp/Model/Plane.swift
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -10,6 +10,7 @@ import Foundation
 class Plane {
     private var rectangles = [Rectangle]()
     var generateRectangleViewDelegate: GenerateRectangleViewDelegate?
+    var changeRectangleBackgroundColorDelegate: ChangeRectangleBackgroundColorDelegate?
     var count: Int {
         return rectangles.count
     }
@@ -31,5 +32,15 @@ class Plane {
         guard let newRectangle = RectangleFactory.generateRandomRectangle(in: frame) else { return }
         rectangles.append(newRectangle)
         generateRectangleViewDelegate?.planeDidAddRectangle(newRectangle)
+    }
+    
+    public func changeBackGroundColorOfRectangle(id: ID) {
+        for rectangle in rectangles {
+            if rectangle.id == id {
+                rectangle.changeBackgroundColorRandomly()
+                changeRectangleBackgroundColorDelegate?
+                    .rectangleDidChangeBackgroundColor(to: rectangle.backgroundColor, alpha: rectangle.alpha)
+            }
+        }
     }
 }
