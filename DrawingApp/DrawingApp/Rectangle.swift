fa63568699f334a0215076dfe2b0b48b6ce7aacc
bibi <bibi6666667@kakao.com>
2022-03-07 23:05:59 +0900

    Shape와 Rectangle에 팩토리 패턴 적용

--- DrawingApp/DrawingApp/Rectangle.swift
+++ DrawingApp/DrawingApp/Rectangle.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class Rectangle: Shape {
+    let id: String
+
+    var size: Size
     
+    var point: Point
+    
+    var backgroundColor: BackgroundColor
+    
+    var alpha: Int
+    
+    required init(id: String, size: Size, point: Point, backgroundColor: BackgroundColor, alpha: Int) {
+        self.id = id
+        self.size = size
+        self.point = point
+        self.backgroundColor = backgroundColor
+        self.alpha = alpha
+    }
 }
