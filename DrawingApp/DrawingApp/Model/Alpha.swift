7a045ea2bf7719e6d6b1109f35bebf38ed481bc8
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-07 13:45:57 +0900

    [Refactor] Alpha의 rawValue 얻기 위한 value 프로퍼티 생성 및 구현

--- DrawingApp/DrawingApp/Model/Alpha.swift
+++ DrawingApp/DrawingApp/Model/Alpha.swift
@@ -8,21 +8,46 @@
 import Foundation
 
 enum Alpha: Double {
-    case level1 = 0.1
-    case level2 = 0.2
-    case level3 = 0.3
-    case level4 = 0.4
-    case level5 = 0.5
-    case level6 = 0.6
-    case level7 = 0.7
-    case level8 = 0.8
-    case level9 = 0.9
-    case level10 = 1.0
+    case level1
+    case level2
+    case level3
+    case level4
+    case level5
+    case level6
+    case level7
+    case level8
+    case level9
+    case level10
+    
+    var value: Double {
+        switch(self) {
+        case .level1:
+            return 0.1
+        case .level2:
+            return 0.2
+        case .level3:
+            return 0.3
+        case .level4:
+            return 0.4
+        case .level5:
+            return 0.5
+        case .level6:
+            return 0.6
+        case .level7:
+            return 0.7
+        case .level8:
+            return 0.8
+        case .level9:
+            return 0.9
+        case .level10:
+            return 1.0
+        }
+    }
 }
 
 extension Alpha: CustomStringConvertible {
     var description: String {
-        return "Alpha: \(self.rawValue)"
+        return "Alpha: \(self.value)"
     }
 }
 
