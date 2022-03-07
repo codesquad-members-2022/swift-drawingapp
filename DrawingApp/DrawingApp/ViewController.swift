17424466c7556476495ca91f4dfe1b4efe554759
Damagucci-Juice <jsr9874@gmail.com>
2022-03-07 20:53:27 +0900

    Feat : UIView에 tapGestureRecognizer 연결해서 작은 테스트

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -1,8 +1,21 @@
 import UIKit
 import OSLog
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet private weak var tappableView: UIView! {
+        didSet {
+            tappableView.backgroundColor = .red
+        }
+    }
+
+    @objc func didTapView(_ sender: UITapGestureRecognizer) {
+        print("did tap view", sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapView(_:)))
+        
+        tappableView.addGestureRecognizer(tapGestureRecognizer)
     }
 }
