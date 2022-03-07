c7c8c352587bf255a45d058b330d5d75f398c907
asqw887 <asqw887@gmail.com>
2022-03-07 21:48:04 +0900

    refector : 불필요한 코드 삭제

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -13,13 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let factory = RectangleFactory()
-        let rect1 = factory.createRandomRectangle()
-        let rect2 = factory.createRectangle(x: 100, y: 150, width: 300.0, height: 300.0)
-        
-        let log = Logger()
-        log.info("Rect1 : \(rect1)")
-        log.info("Rect2 : \(rect2)")
     }
 }
 
