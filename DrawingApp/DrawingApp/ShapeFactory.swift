95e598f479b594f16d747b02c61a0ab190b81b40
bibi <bibi6666667@kakao.com>
2022-03-07 23:07:40 +0900

    ShapeFactory에서 랜덤 아이디를 만들어 Rectangle을 만들도록 변경

--- DrawingApp/DrawingApp/ShapeFactory.swift
+++ DrawingApp/DrawingApp/ShapeFactory.swift
@@ -9,9 +9,19 @@ import Foundation
 
 class ShapeFactory: ShapeCreator {
     func createShape(shapeType: ShapeType, size: Size, point: Point, color: BackgroundColor, alpha: Int) -> Shape {
+        let id = generateId()
         switch shapeType {
         case .rectangle:
-            return Rectangle(size: size, point: point, backgroundColor: color, alpha: alpha)
+            return Rectangle(id: id, size: size, point: point, backgroundColor: color, alpha: alpha)
         }
+    }
+    
+    private func generateId() -> String {
+        let uuidId = UUID().uuidString.split(separator: "-").joined(separator: "")
+        let thirdIndex = uuidId.index(uuidId.startIndex, offsetBy: 3)
+        let sixthIndex = uuidId.index(thirdIndex, offsetBy: 3)
+        let ninethIndex = uuidId.index(sixthIndex, offsetBy: 3)
+        let result = uuidId[..<thirdIndex] + "-" + uuidId[thirdIndex..<sixthIndex] + "-" +  uuidId[sixthIndex..<ninethIndex]
+        return String(result)
     }
 }
