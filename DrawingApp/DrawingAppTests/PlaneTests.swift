a8999f8a400a343a229ddac308f85a67d3f353e1
BumgeunSong <isp1195@gmail.com>
2022-03-07 17:58:52 +0900

    Add test case for photo & drag feature

--- DrawingApp/DrawingAppTests/PlaneTests.swift
+++ DrawingApp/DrawingAppTests/PlaneTests.swift
@@ -21,11 +21,18 @@ class PlaneTests: XCTestCase {
         XCTAssertEqual(plane.rectangleCount, 1)
     }
     
+    func testAddPhoto() throws {
+        let plane = Plane()
+        plane.addPhoto(data: Data())
+        XCTAssertEqual(plane.photoCount, 1)
+    }
+    
     func testSelectedWhenTapped() {
         let plane = Plane()
         (0..<5).forEach { _ in plane.addRectangle() }
+        (0..<5).forEach { _ in plane.addPhoto(data: Data()) }
         
-        for testIndex in 0..<plane.rectangleCount {
+        for testIndex in 0..<plane.viewModelCount {
             let testPoint = plane[testIndex].center
             plane.tap(on: testPoint)
             XCTAssertNotNil(plane.selected)
@@ -36,6 +43,7 @@ class PlaneTests: XCTestCase {
     func testSelectedWhenEmptyTapped() {
         let plane = Plane()
         (0..<5).forEach { _ in plane.addRectangle() }
+        (0..<5).forEach { _ in plane.addPhoto(data: Data()) }
         
         let emptyPoint = Point(x: Double(0), y: Double(0))
         plane.tap(on: emptyPoint)
@@ -49,10 +57,11 @@ class PlaneTests: XCTestCase {
         let testPoint = plane[0].center
         plane.tap(on: testPoint)
         
-        let color = Factory.createColor()
+        guard let selected = plane.selected as? ColorMutable else { return }
+        
+        let color = Rectangle.createColor()
         plane.transform(to: color)
         
-        guard let selected = plane.selected as? ColorMutable else { return }
         XCTAssertEqual(selected.color.red, color.red)
         XCTAssertEqual(selected.color.green, color.green)
         XCTAssertEqual(selected.color.blue, color.blue)
@@ -61,6 +70,7 @@ class PlaneTests: XCTestCase {
     func testTransformAlpha() {
         let plane = Plane()
         (0..<5).forEach { _ in plane.addRectangle() }
+        (0..<5).forEach { _ in plane.addPhoto(data: Data()) }
         
         let testPoint = plane[0].center
         plane.tap(on: testPoint)
@@ -70,5 +80,18 @@ class PlaneTests: XCTestCase {
         
         guard let selected = plane.selected as? AlphaMutable else { return }
         XCTAssertEqual(selected.alpha.value, alpha.value)
+    }
+    
+    func testTransformOrigin() {
+        let plane = Plane()
+        (0..<5).forEach { _ in plane.addRectangle() }
+        (0..<5).forEach { _ in plane.addPhoto(data: Data()) }
+        
+        for i in 0..<plane.viewModelCount {
+            let point = Rectangle.createPoint()
+            plane.transform(plane[i].id, to: point)
+            XCTAssertEqual(plane[i].origin.x, point.x)
+            XCTAssertEqual(plane[i].origin.y, point.y)
+        }
     }
 }
