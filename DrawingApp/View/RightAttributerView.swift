367ceac32d442e35d99c9884ec9a4249c1caa2fe
wnsxor1993 <wnsxor1993@naver.com>
2022-03-07 10:38:46 +0900

    Chore: Notification Name 위치 수정 - post 선언된 위치에 해당 Notification Name 선언

--- DrawingApp/View/RightAttributerView.swift
+++ DrawingApp/View/RightAttributerView.swift
@@ -169,3 +169,10 @@ extension RightAttributerView{
         NotificationCenter.default.post(name: .changeBlueColor, object: nil)
     }
 }
+
+extension Notification.Name{
+    static let changeAlpha = Notification.Name("changeAlpha")
+    static let changeRedColor = Notification.Name("changeRedColor")
+    static let changeGreenColor = Notification.Name("changeGreenColor")
+    static let changeBlueColor = Notification.Name("changeBlueColor")
+}
