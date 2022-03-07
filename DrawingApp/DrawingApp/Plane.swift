8b5756910cbab6ba480bd10dfe19fa87d75b04da
asqw887 <asqw887@gmail.com>
2022-03-07 21:43:45 +0900

    feature : Plane 모델 구현

--- DrawingApp/DrawingApp/Plane.swift
+++ DrawingApp/DrawingApp/Plane.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+
+struct Plane{
+    
+    var rectangles: [Rectangle] = []
+    
+    func count() -> Int{
+        return rectangles.count
+    }
+    
+    func getRectangle(index: Int) -> Rectangle{
+        return rectangles[index]
+    }
+    
+    mutating func add(rectangle: Rectangle){
+        rectangles.append(rectangle)
+    }
+    
+    func isExist(_ point: Point) -> Bool{
+        
+        for rect in rectangles {
+            if (rect.leftTopPoint.x >= point.x && rect.leftTopPoint.y >= point.y) && (rect.rightBottomPoint.x <= point.x && rect.rightBottomPoint.y <= point.y) {
+                return true
+                }
+            }
+        return false
+    }
+    
+}
