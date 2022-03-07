dae580301009dc64562064e5b27467e15c1b00e7
asqw887 <asqw887@gmail.com>
2022-03-07 21:00:19 +0900

    test : Plane 테스트 코드

--- DrawingApp/PlaneTest/PlaneTest.swift
+++ DrawingApp/PlaneTest/PlaneTest.swift
@@ -6,24 +6,56 @@
 //
 
 import XCTest
+@testable import DrawingApp
 
 class PlaneTest: XCTestCase {
 
+    var plane: Plane!
+    var rect: Rectangle!
+//    var fectory = RectangleFactory!
+    var factory: RectangleFactory!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        plane = Plane()
+        factory = RectangleFactory()
+        rect = factory.createRandomRectangle()
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testAdd() throws {
+        plane.add(rectangle: rect)
+        XCTAssertEqual(plane.count(), 1, "Plane 객체에 사각형이 추가되지 않았습니다. ")
     }
+    
+    func testGetRectangle() throws {
+        plane.add(rectangle: rect)
+        XCTAssertTrue(plane.getRectangle(index: 0) === rect, "getRectangle error")
+    }
+    
+    
+    func testCGPointToDoublePoint() throws {
+        let cgpoint = CGPoint(x: 100.0, y: 100.0)
+        CGPointToDoublePoint(cgpoint)
+        XCTAssertFalse(CGPointToDoublePoint(cgpoint), Point(x: 100.0, y: 100.0))
+    }
+    
+    func testisExsit() throws{
+        // 특정 좌표를 받아 Double로 변환 -> 따로 구현
+        // plane.Rectangles 안에 포함되는 좌표가 있는지 확인 -> 따로 구현
+        // XCTassetTrue
+        let point = CGPoint(x: 100.0, y: 100.0)
+//        let point: Point = CGPointToDoublePoint(point: CGPoint(x: 100.0, y: 100.0))
+        let existTestRectangle = factory.createRectangle(size: Size(width: 100.0, height: 100.0), position: Point(x: 100.0, y: 100.0))
+        plane.add(rectangle: existTestRectangle)
+        XCTAssertFalse(isExist(point, in plane.rectangles), "\(point)을 포함하는 사각형이 존재하지 않음")
+    }
+    
+    
+    
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
