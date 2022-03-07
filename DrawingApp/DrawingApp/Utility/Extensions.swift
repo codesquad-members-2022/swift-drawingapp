8eaddbb7dd7e4ac989cacfff12e44ae15e83634a
Song TaeHwan <axoghks@gmail.com>
2022-03-07 12:43:14 +0900

    refactor: Remove custom convenience initializer from UIColor

--- DrawingApp/DrawingApp/Utility/Extensions.swift
+++ DrawingApp/DrawingApp/Utility/Extensions.swift
@@ -48,25 +48,6 @@ extension UIColor {
         return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
     }
     
-    convenience init(hexString: String, alpha: CGFloat = 1.0) {
-        var hexString = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        
-        if (hexString.hasPrefix("#")) {
-            hexString = String(hexString.dropFirst())
-        }
-        
-        let color = UInt(hexString, radix: 16) ?? 0
-        let mask = 0x000000FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
-        let red   = CGFloat(r) / 255.0
-        let green = CGFloat(g) / 255.0
-        let blue  = CGFloat(b) / 255.0
-        
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    }
-    
     func toHexString() -> String {
         var red: CGFloat = 0
         var green: CGFloat = 0
