c5bf603a3424492507f7cf32abf53e418bf2c6cd
bibi <bibi6666667@kakao.com>
2022-03-07 23:05:59 +0900

    Shape와 Rectangle에 팩토리 패턴 적용

--- DrawingApp/DrawingApp/Shape.swift
+++ DrawingApp/DrawingApp/Shape.swift
@@ -7,24 +7,12 @@
 
 import Foundation
 
-class Shape {
-    let id: String = {
-        let uuidId = UUID().uuidString.split(separator: "-").joined(separator: "")
-        let thirdIndex = uuidId.index(uuidId.startIndex, offsetBy: 3)
-        let sixthIndex = uuidId.index(thirdIndex, offsetBy: 3)
-        let ninethIndex = uuidId.index(sixthIndex, offsetBy: 3)
-        let result = uuidId[..<thirdIndex] + "-" + uuidId[thirdIndex..<sixthIndex] + "-" +  uuidId[sixthIndex..<ninethIndex]
-        return String(result)
-    }()
-    let size: Size
-    let point: Point
-    let backgroundColor: BackgroundColor
-    let alpha: Int // 1-10 사이값
+protocol Shape {
+    var id: String { get }
+    var size: Size { get set }
+    var point: Point { get set }
+    var backgroundColor: BackgroundColor { get set }
+    var alpha: Int { get set } // 1-10 사이값
     
-    init(size: Size, point: Point, backgroundColor: BackgroundColor, alpha: Int) {
-        self.size = size
-        self.point = point
-        self.backgroundColor = backgroundColor
-        self.alpha = alpha
-    }
+    init(id: String, size: Size, point: Point, backgroundColor: BackgroundColor, alpha: Int)
 }
