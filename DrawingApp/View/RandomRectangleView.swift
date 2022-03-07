8caf1f7d910e7840dd12be4b8d36e409e5f8ba9b
wnsxor1993 <wnsxor1993@naver.com>
2022-03-08 01:12:49 +0900

    Chore: 전체적인 로직 리팩토링 - Notification 로직 리팩토링 - Delegate 로직 재생성

--- DrawingApp/View/RandomRectangleView.swift
+++ DrawingApp/View/RandomRectangleView.swift
@@ -9,13 +9,8 @@ import UIKit
 import os
 
 class RandomRectangleView: UIView {
-    init(id: String, point: MyPoint, size: MySize, color: RGBColor, alpha: Alpha){
-        let frame = CGRect(x: point.x, y: point.y, width: size.width, height: size.height)
+    override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        self.frame = frame
-        self.backgroundColor = UIColor(red: color.redValue(), green: color.greenValue(), blue: color.blueValue(), alpha: alpha.showValue())
-        self.restorationIdentifier = id
     }
     
     required init?(coder: NSCoder) {
