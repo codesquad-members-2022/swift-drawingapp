c7492ba6d6e5394cd5734794de3a683fceeab050
sanghee <anshee97@naver.com>
2022-03-08 10:26:43 +0900

    Test: Add DrawingAppTests

---
+++ DrawingApp/DrawingAppTests/DrawingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppTests.swift
+//  DrawingAppTests
+//
+//  Created by 안상희 on 2022/03/08.
+//
+
+import XCTest
+
+class DrawingAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
