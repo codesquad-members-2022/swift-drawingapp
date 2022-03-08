efbf94ea7728c79cae6e8836008b9f6bdb92f1c7
sanghee <anshee97@naver.com>
2022-03-08 15:09:01 +0900

    Feat: Add hexValue to Color

--- DrawingApp/DrawingApp/Model/Color.swift
+++ DrawingApp/DrawingApp/Model/Color.swift
@@ -11,6 +11,7 @@ class Color {
     private let red: Int
     private let green: Int
     private let blue: Int
+    private let hexValue: String
     
     init(red: Int, green: Int, blue: Int) {
         let minValue = 0
@@ -19,12 +20,17 @@ class Color {
         self.red = red < minValue ? minValue : (red > maxValue ? maxValue : red)
         self.green = green < minValue ? minValue : (green > maxValue ? maxValue : green)
         self.blue = blue < minValue ? minValue : (blue > maxValue ? maxValue : blue)
+        
+        let hexRed = String(red, radix: 16)
+        let hexGreen = String(green, radix: 16)
+        let hexBlue = String(blue, radix: 16)
+        self.hexValue = hexRed + hexGreen + hexBlue
     }
 }
 
 
 extension Color: CustomStringConvertible {
     var description: String {
-        return "R:\(red), G:\(green), B:\(blue)"
+        return "R:\(red), G:\(green), B:\(blue), hexValue: \(hexValue)"
     }
 }
