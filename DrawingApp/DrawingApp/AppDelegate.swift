30e9e009fad929b4472f8ce28631ff28277ccd4f
Damagucci-Juice <jsr9874@gmail.com>
2022-03-08 22:13:09 +0900

    Feat : Rectangle 구조체에 자신의 영역값을 반환하는 메서드 추가

--- DrawingApp/DrawingApp/AppDelegate.swift
+++ DrawingApp/DrawingApp/AppDelegate.swift
@@ -12,4 +12,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
-
