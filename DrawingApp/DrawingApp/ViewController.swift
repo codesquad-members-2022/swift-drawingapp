74f4a54661cfb10769271546f3def7fa19d25c90
Jinsujin <sylpid003@gmail.com>
2022-03-08 15:46:25 +0900

    Feat: 생성한 Rect 를 ViewController 에서 화면 그리기
    - PlaneDelegate 작성
    - ViewController 에서 Model의 delegate이벤트를 받아 화면에 UiView 그림
    - Rectangle property 를 외부에서 가져오기 위해 private 제거

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -22,10 +22,24 @@ class ViewController: UIViewController {
         
         let rect = ShapeFactoryType.square(length: 20).make()
         Log.shared.print(logLevel: .default, message: rect.description)
+        
+        model.delegate = self
     }
+    
+    // MARK:- Outlets
+    @IBOutlet weak var canvas: UIView!
     
     // MARK:- Actions
     @IBAction func touchedCreateRect(_ sender: Any) {
         model.createRect()
+    }
+}
+
+// MARK:- PlaneDelegate
+extension ViewController: PlaneDelegate {
+    func didCreate(rect: Rectangle) {
+        let view = UIView(frame: CGRect(origin: CGPoint(x: rect.point.x, y: rect.point.y), size: CGSize(width: rect.size.width, height: rect.size.height)))
+        view.backgroundColor = .red
+        canvas.addSubview(view)
     }
 }
