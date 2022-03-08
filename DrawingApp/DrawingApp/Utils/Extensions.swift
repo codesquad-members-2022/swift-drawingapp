d124964b4b6e6945b7084a18ad5404e153a3d036
Jinsujin <sylpid003@gmail.com>
2022-03-08 23:41:16 +0900

    Feat: 선택한 Rectangle의 컬러 정보를 TextField 에 보여주기

--- DrawingApp/DrawingApp/Utils/Extensions.swift
+++ DrawingApp/DrawingApp/Utils/Extensions.swift
@@ -11,4 +11,14 @@ extension UIColor {
     convenience init(color: Color) {
         self.init(red: CGFloat(color.red) / 255.0, green: CGFloat(color.green) / 255.0, blue: CGFloat(color.blue) / 255.0, alpha: 1.0)
     }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return NSString(format:"#%06x", rgb) as String
+    }
 }
