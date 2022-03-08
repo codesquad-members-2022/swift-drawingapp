60f2b31dc54c4da1801823229245f4aa097832b1
Damagucci-Juice <jsr9874@gmail.com>
2022-03-08 16:46:33 +0900

    4개의 UIView가 각각 터치가 되도록 설정, 그러나 배경 View는 탭이 되지 않음

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -1,23 +1,39 @@
 import UIKit
 import OSLog
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
     var plane: Plane = Plane()
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        print(sender.location(in: self.view))
+    }
+    
+    func shouldRectanglesBeTouched() {
+        for view in self.view.subviews {
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+            view.addGestureRecognizer(tap)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addRectangles()
         showRectangleViews()
+        shouldRectanglesBeTouched()
     }
     
-    func showRectangleViews() {
+    func addRectangles() {
         for _ in 0..<4 {
             self.plane.addRectangle()
         }
-        
+    }
+    
+    func showRectangleViews() {
         for index in 0..<plane.rectangleCount {
             let rectangle = plane[index]
             let rectView = UIView(frame: CGRect(x: rectangle.getPoint().x, y: rectangle.getPoint().y, width: rectangle.getSize().width, height: rectangle.getSize().height))
             rectView.backgroundColor = UIColor(red: CGFloat(rectangle.getColor().R)/255.0, green: CGFloat(rectangle.getColor().G)/255.0, blue: CGFloat(rectangle.getColor().B)/255.0, alpha: CGFloat(rectangle.getAlpha().rawValue)/10.0)
+            print(rectangle.getAlpha().rawValue)
             self.view.addSubview(rectView)
         }
     }
