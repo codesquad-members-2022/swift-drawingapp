aeb71b65168fd8f55058bfeef3e8e67f7588a9fd
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-03-08 17:35:07 +0900

    [REFACTOR]Subclassing relative Views into PanelView.swift

--- drawingApp/drawingApp/View/PanelView.swift
+++ drawingApp/drawingApp/View/PanelView.swift
@@ -7,41 +7,94 @@
 
 import Foundation
 import UIKit
+protocol PanelViewDelegate {
+    func didRondomizeColor()
+    func didChangeAlpha()
+}
 
 class PanelView : UIView {
+    
+    private var colorRondomizeButton : UIButton!
+    private var alphaStepper : UIStepper!
+    
+    private var stepperLabel : UILabel!
+    private var colorTitleLabel : UILabel!
+    private var alphaTitleLabel : UILabel!
+    
+    var delegate : PanelViewDelegate?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = UIColor.systemGray3
+        setupLables()
+        setupButtons()
         loadSubViews()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        self.backgroundColor = UIColor.systemGray3
+        setupLables()
+        setupButtons()
         loadSubViews()
     }
     
   
     
     private func loadSubViews() {
-        self.backgroundColor = UIColor.systemGray3
+        addSubview(colorTitleLabel)
+        addSubview(alphaTitleLabel)
+        addSubview(stepperLabel)
+        addSubview(colorRondomizeButton)
+        addSubview(alphaStepper)
+    }
+    
+    private func setupLables() {
         
-        let colorLabel = makeLabel()
-        let alphaLabel = makeLabel()
+        self.colorTitleLabel = UILabel()
+        self.alphaTitleLabel = UILabel()
+        self.stepperLabel = UILabel()
         
-        colorLabel.text = "배경색"
-        alphaLabel.text = "투명도"
-        colorLabel.frame = CGRect(x: self.bounds.minX+20, y: self.bounds.minY+20, width: 160, height: 50)
-        alphaLabel.frame = CGRect(x: self.bounds.minX+20, y: self.bounds.minY+150, width: 160, height: 50)
+        colorTitleLabel.textAlignment = .left
+        alphaTitleLabel.textAlignment = .left
+        stepperLabel.textAlignment = .center
         
-        addSubview(colorLabel)
-        addSubview(alphaLabel)
+        colorTitleLabel.text = "배경색"
+        alphaTitleLabel.text = "투명도"
+        stepperLabel.text = "0"
+        
+        colorTitleLabel.frame = CGRect(x: self.bounds.minX+20, y: self.bounds.minY+20, width: 160, height: 50)
+        alphaTitleLabel.frame = CGRect(x: self.bounds.minX+20, y: self.bounds.minY+150, width: 160, height: 50)
+        stepperLabel.frame = CGRect(x: self.bounds.minX, y: self.bounds.minY + 185.0, width: 160, height: 50)
+    }
+    
+    private func setupButtons() {
+        self.colorRondomizeButton = UIButton()
+        self.alphaStepper = UIStepper()
+        
+        colorRondomizeButton.addTarget(colorRondomizeButton, action: #selector(didTabColorRondomizeButton), for: .touchUpInside)
+        alphaStepper.addTarget(colorRondomizeButton, action: #selector(didTabAlphaStepper), for: .valueChanged)
+        
+        alphaStepper.minimumValue = 0
+        alphaStepper.maximumValue = 10
+        alphaStepper.wraps = false
+        alphaStepper.stepValue = 1
+        
+        colorRondomizeButton.backgroundColor = .tintColor
+        colorRondomizeButton.layer.cornerRadius = 10
+        colorRondomizeButton.setTitle("", for: .normal)
+       
+       
+        colorRondomizeButton.frame = CGRect(x: self.bounds.minX + 20, y: self.bounds.minY + 70.0, width: 160, height: 50)
+        alphaStepper.frame = CGRect(x: self.bounds.minX + 100, y: self.bounds.minY + 190.0, width: 160, height: 50)
     }
     
     
-    private func makeLabel() -> UILabel {
-        let label = UILabel()
-        label.textAlignment = .left
-        return label
-        
+    @objc func didTabColorRondomizeButton() {
+        delegate?.didRondomizeColor()
+    }
+    @objc func didTabAlphaStepper() {
+        delegate?.didChangeAlpha()
     }
     
 }
