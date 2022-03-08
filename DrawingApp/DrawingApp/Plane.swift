20c10b7e5604fdf449a857af2d59d06ea371a536
Jinsujin <sylpid003@gmail.com>
2022-03-08 22:38:09 +0900

    Fix: 터치할때 가장 나중에 추가된 사각형을 먼저 인식하도록 수정

--- DrawingApp/DrawingApp/Plane.swift
+++ DrawingApp/DrawingApp/Plane.swift
@@ -30,8 +30,8 @@ class Plane {
     
     func selectRect(inside point: Point) {
         var selectedRect: Rectangle?
-        // TODO: 위에 있는 Rect 가 가장 나중에 추가됨 => for문 거꾸로 돌아야 함
-        for rect in rectangles {
+        for i in stride(from: rectangles.count - 1, through: 0, by: -1) {
+            let rect = rectangles[i]
             if rect.contain(inside: point) {
                 selectedRect = rect
                 break
