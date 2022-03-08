33ae4a7893d73e6c8e850ce9693380de09bc4ada
P_SeoB <akdlsjtm21@naver.com>
2022-03-08 11:31:50 +0900

    Seperate Button & ButtonAction

--- DrawingApp/DrawingApp/ShapeElement/Point.swift
+++ DrawingApp/DrawingApp/ShapeElement/Point.swift
@@ -5,6 +5,8 @@
 //  Created by 박진섭 on 2022/03/01.
 //
 
+import CoreGraphics
+
 struct Point:CustomStringConvertible {
     
     var description: String {
@@ -19,12 +21,8 @@ struct Point:CustomStringConvertible {
     static let maxY:Double = 820.0
     
     static func random() -> Point {
-        //랜덤으로 생기는 사각형의 Point가 button이나 detailView를 가리지 않도록 빼주었습니다.
-        let buttonHight = 300.0
-        let detailViewWidth = 350.0
-        let randomXpoint = Double.random(in: 0.0...Point.maxX) - detailViewWidth
-        let randomYpoint = Double.random(in: 0.0...Point.maxY) - buttonHight
-        
+        let randomXpoint = Double.random(in: 0.0...Point.maxX)
+        let randomYpoint = Double.random(in: 0.0...Point.maxY)
         return Point(x: randomXpoint, y: randomYpoint)
     }
     
