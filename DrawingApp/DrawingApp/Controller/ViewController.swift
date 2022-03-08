b429ecbdf7861afae8ce0d7d5fe23c4bf4825449
sanghee <anshee97@naver.com>
2022-03-08 20:31:31 +0900

    Feat: Add RectangleDelegate

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var delegate: RectangleDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let factory = RectangleFactory()
-//        for _ in 0..<4 {
-//            factory.createRectangle()
-//        }
+        let factory = RectangleFactory()
+        for _ in 0..<4 {
+            let rectangle = factory.createRectangle()
+            delegate?.rectangleDidCreated(rectangle: rectangle)
+        }
     }
 }
 
+
+protocol RectangleDelegate {
+    func rectangleDidCreated(rectangle: Rectangle)
+}
