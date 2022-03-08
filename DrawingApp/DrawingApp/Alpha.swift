13151cc9c3c8d144f554127ed837d53deb93d0a8
bibi <bibi6666667@kakao.com>
2022-03-08 16:24:10 +0900

    투명도를 나타내는 Alpha 클래스 생성

---
+++ DrawingApp/DrawingApp/Alpha.swift
@@ -0,0 +1,25 @@
+//
+//  Alpha.swift
+//  DrawingApp
+//
+//  Created by Bibi on 2022/03/08.
+//
+
+import Foundation
+
+class Alpha {
+    private let value: Int
+    private let alphaMin = 1
+    private let alphaMax = 10
+    
+    init(_ value: Int) {
+        var adjustedValue = value
+        if value < alphaMin {
+            adjustedValue = alphaMin
+        }
+        if value > alphaMax {
+            adjustedValue = alphaMax
+        }
+        self.value = adjustedValue
+    }
+}
