394186d03be17eeea9f2bf7f1afc80e7e7f63082
BumgeunSong <isp1195@gmail.com>
2022-03-09 12:55:07 +0900

    Make ViewModel as Superclass to conform hashable

--- DrawingApp/DrawingApp/Model/Model/Photo.swift
+++ DrawingApp/DrawingApp/Model/Model/Photo.swift
@@ -9,21 +9,16 @@ import Foundation
 
 class Photo: ViewModel {
     
-    private(set) var id: ID
-    private(set) var origin: Point
-    private(set) var size: Size
     private(set) var data: Data
     private(set) var alpha: Alpha
 
     init(id: ID, origin: Point, size: Size, photo: Data, alpha: Alpha) {
-        self.id = id
-        self.origin = origin
-        self.size = size
         self.data = photo
         self.alpha = alpha
+        super.init(id: id, origin: origin, size: size)
     }
     
-    static func create(from data: Data) -> Photo {
+    static func random(from data: Data) -> Photo {
         let photoID = ID.random()
         let origin = Point.random()
         let size = Size.standard()
@@ -31,28 +26,11 @@ class Photo: ViewModel {
         
         return Photo(id: photoID, origin: origin, size: size, photo: data, alpha: alpha)
     }
-    
-    var center: Point {
-        Point(x: origin.x + (size.width / 2),
-              y: origin.y + (size.height / 2))
-    }
-
-    func contains(_ point: Point) -> Bool {
-        return (origin.x...origin.x+size.width).contains(point.x)
-        && (origin.y...origin.y+size.height).contains(point.y)
-    }
 }
-
 
 extension Photo: AlphaMutable {
-    func transform(to alpha: Alpha) {
+    func set(to alpha: Alpha) {
         self.alpha = alpha
-    }
-}
-
-extension Photo: OriginMutable {
-    func transform(to origin: Point) {
-        self.origin = origin
     }
 }
     
