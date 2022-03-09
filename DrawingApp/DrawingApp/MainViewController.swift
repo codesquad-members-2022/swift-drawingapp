e6eb044da4ab80c6f2c69a55d8cd99b62b30099d
P_SeoB <akdlsjtm21@naver.com>
2022-03-09 15:20:30 +0900

    [Refactory] ViewController

--- DrawingApp/DrawingApp/MainViewController.swift
+++ DrawingApp/DrawingApp/MainViewController.swift
@@ -8,88 +8,65 @@
 import UIKit
 import OSLog
 
-class MainViewController: UIViewController{
+final class MainViewController: UIViewController{
 
     //model
-    var plane = Plane()
+    private var plane = Plane()
     //view
-    var detailView = DetailView()
-    var rectangleView = RectangleView()
+    private var detailView = DetailView(frame: .zero)       //생성시 오해를 막고 기존 생성자와 관련있게 하기 위해 frame에 .zero를 대입했습니다.
+    private var button:UIButton = RectangleButton(frame: .zero)
     
-    //버튼 외형 설정
-    var button:UIButton = {
-        let button = UIButton()
-        
-        let width = 150.0
-        let height = 100.0
-        let x = (Point.maxX / 2) - (width / 2)
-        let y = (Point.maxY - height) - 30
-        
-        button.frame = CGRect(x: x, y: y, width: width, height: height)
-        button.tintColor = .black
-        
-        var configuration = UIButton.Configuration.plain()
-        configuration.title = "사각형"
-        
-        configuration.imagePlacement = .top
-        configuration.imagePadding = 20
-        
-        configuration.background.backgroundColor = .secondarySystemBackground
-        configuration.background.cornerRadius = 10
-        
-        button.configuration = configuration
-        
-        return button
-    }()
+    //그려진 retangleView를 Rectangle을 Key로 찾는 Dictionary로 만들어서 모델과 매칭을 시켜주었습니다.
+    private var retangleViews = [Rectangle:RectangleView]()
+    //선택된 rectangleView
+    private var seletedRectangleView:RectangleView?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        detailView.delegate = self
+        plane.delegate = self
         
-        setupViews()
-        configureTapGesture()
+        configureRectangleButton()
         configureDeatailView()
-        configureRectangleButtonTapped()
-        
+        configureTapGesture()
+        addViews()
     }
     
+    //TapGesture
     private func configureTapGesture() {
         let tapGesture = UITapGestureRecognizer()
         tapGesture.delegate = self
         self.view.addGestureRecognizer(tapGesture)
     }
     
-    
+    //DetailView
     private func configureDeatailView() {
-        detailView.frame = CGRect(
-            x: self.view.frame.maxX - 200,
-            y: 0,
-            width: 200,
-            height: self.view.frame.height
+        detailView.delegate = self
+        detailView.layer.zPosition = 1.0                        //생성되는 사각형에 겹쳐서 클릭이 안되거나 안보이는 경우를 막기위해 zPosition을 주었다.
+        
+        let inset:CGFloat = 200
+        detailView.frame = CGRect(x:self.view.frame.maxX - inset,
+                                  y: 0,
+                                  width: inset,
+                                  height: self.view.frame.height
         )
     }
     
-    
-    //버튼에 액션 추가
-    private func configureRectangleButtonTapped() {
-        let squareImage = UIImage(systemName: "square")
-        let highlightedImage = UIImage(systemName: "square.fill")
+    //사각형 추가 버튼 정의
+    private func configureRectangleButton() {
+        let width = 150.0
+        let height = 100.0
+        let x:CGFloat = (self.view.frame.maxX / 2.0) - (width / 2)
+        let y:CGFloat = (self.view.frame.maxY - height)
         
+        button.layer.zPosition = 1.0                                //생성되는 사각형에 겹쳐서 클릭이 안되거나 안보이는 경우를 막기위해 zPosition을 주었다.
+        button.frame = CGRect(x: x, y: y, width: width, height: height)
         button.addAction(makeRectangleAction(), for: .touchUpInside)
-        
-        //사각형 버튼 터치시 변화를 보여주기 위해 선언함.
-        button.configurationUpdateHandler = { button in
-            var configuration = button.configuration
-            configuration?.image = button.isHighlighted ? highlightedImage : squareImage
-            button.configuration = configuration
-        }
     }
     
     //버튼 액션정의
     private func makeRectangleAction() -> UIAction {
         let action = UIAction {_ in
-            
             let id = IDFactory.makeRandomID()
             let size = Size(width: 150, height: 120)
             let point = Point.random()
@@ -97,19 +74,14 @@ class MainViewController: UIViewController{
             let alpha = Alpha.random()
             
             let rect = Rectangle(id: id, origin: point, size: size, backGroundColor: rgb, alpha: alpha)
-            let rectView = UIView(rect: rect, rgb: rgb, alpha: alpha)
             
-            rectView.clipsToBounds = true
-            self.plane.rectangles.append(rect)                                  //모델에 rectangle을 추가
-            self.rectangleView.views.append(rectView)                           //view에 retangleView를 추가
-            
-            
-            self.view.addSubview(rectView)
+            self.plane.addRectangle(rectangle: rect)    //모델에 rectangle을 추가
         }
         return action
     }
     
-    private func setupViews() {
+    //Custom View추가
+    private func addViews() {
         view.addSubview(button)
         view.addSubview(detailView)
     }
@@ -119,65 +91,74 @@ class MainViewController: UIViewController{
 extension MainViewController:UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
         
-        let touchedView = self.view.hitTest(touch.location(in: self.view), with: nil)
-        //터치한 곳의 View에 사각형의 id가 있다면 사각형입니다.
-        if touchedView?.accessibilityIdentifier != nil {
-            //기존에 seletedRectanlge이 있다면 테두리를 원상복구 시키고 새롭게 선택된 rectangle의 테두리를 검게 칠합니다.
-            self.rectangleView.selectedView.layer.borderWidth = 0.0
-            
-            touchedView?.layer.borderWidth = 2.0
-            touchedView?.layer.borderColor = UIColor.black.cgColor
-            
-            changeBackgroundButtonTitle(view: touchedView)
-            
-            plane.selectedRectangle = plane.rectangles.first{ $0.id.description == touchedView?.accessibilityIdentifier }
+        //터치된 View의 origin x와 y값을 plane에게 넘겨줍니다.
+        guard let touchedView = touch.view else { return true }
+        if touchedView.frame.width == 1180 { seletedRectangleView?.layer.borderWidth = 0.0 } //빈 화면 클릭시 width을 초기화한다.
         
-            self.rectangleView.selectedView = touchedView ?? UIView()
-            
-            //detailView를 클릭했을시에도 테두리가 사라지지 않게 하기위해 빈화면크기의 View를 클릭했을 시에만 테두리를 없앱니다.
-        } else if touchedView?.frame.size == CGSize(width: 1180, height: 820){
-            self.rectangleView.selectedView.layer.borderWidth = 0.0
-        }
+        //Plane에게 touch된 View의 origin좌표를 넘겨준다.
+        let x:Double = touchedView.frame.origin.x
+        let y:Double = touchedView.frame.origin.y
+        plane.findSeletedRectangle(x: x, y: y)
         
         return true
-    }
-    
-    //현재 touch된 view의 id를 비교해서 RGB값을 가져오고 hex값으로 변환시켜 버튼title에 넣는다.
-    private func changeBackgroundButtonTitle(view:UIView?) {
-        let currentRectangle = plane.rectangles.first { $0.id.description == view?.accessibilityIdentifier }
-        let currentBackgroundColor = currentRectangle?.rgb.hexValue ?? " "
-        
-        self.detailView.backgroundColorButton.setTitle("\(currentBackgroundColor)", for: .normal)
     }
 }
 
 //MARK: -- Delegate메소드 실제 구현
 
-//슬라이더를 움직일때 마다 현재 클릭한 사각형의 alpha값을 바꾼다.
+//슬라이더를 움직일때 마다 현재 클릭한 모델 사각형의 alpha값을 바꾼다.
 extension MainViewController:DetailViewDelgate {
     func sliderViewEndEditing(sender: UISlider) {
-        let currentSliderValue = sender.value                                           //silder에서 넘어온 값
-        
-        let currentRectangle = plane.selectedRectangle                                   //현재 선택한 사각형 모델
-        let currentRectangleView = rectangleView.selectedView                            //현재 선택한 사각형 뷰
-        
-        currentRectangle?.alpha = Alpha(sender.value)                                    //모델의 값을 바꾼다
-        currentRectangleView.alpha = CGFloat(currentRectangle?.alpha.value ?? 0.0)       //바꾼 모델의 값을 View에 적용시킨다.
-        
-        self.detailView.alphaLabel.text = "투명도 \(currentSliderValue)"
+        let currentSliderValue = sender.value                      //silder에서 넘어온 값
+        plane.change(alpha: Alpha(currentSliderValue))             //모델의 값을 바꾼다.
     }
     
-    
-    //랜덤한 RGB값을 설정하고 현재 선택된 뷰의 배경색을 변경한다
+    //랜덤한 RGB값을 설정하고 현재 클릭한 모델 사각형의 rgb값을 변경한다
     func colorButtonTouched(sender:UIButton) {
         let randomRGB = RGB.random()
-        guard let currentRectangle = plane.selectedRectangle  else { return }                                   //현재 선택한 사각형 모델
-        let currentRectangleView = rectangleView.selectedView                                                   //현재 선택한 사각형 뷰
+        plane.change(color: randomRGB)
+    }
+}
+
+extension MainViewController:PlaneDelegate {
+    //바뀐 rgb값을 이용해서 View의 배경과 Label의 title을 바꿉니다.
+    func didChangeColor(seletedRectangle: Rectangle) {
+        let rgb = seletedRectangle.rgb
+        let alpha = seletedRectangle.alpha
+        let hexRGB = seletedRectangle.rgb.hexValue
         
-        currentRectangle.rgb = randomRGB                                                                        //모델의 값을 바꾼다.
+        seletedRectangleView?.backgroundColor = UIColor(rgb: rgb, alpha: alpha)
         
-        currentRectangleView.backgroundColor = UIColor(rgb: currentRectangle.rgb, alpha: currentRectangle.alpha)  //바꾼 모델의 값을 View에 적용한다.
+        detailView.backgroundColorButton.setTitle("\(hexRGB)", for: .normal)
+    }
+    
+    //찾은 Rectangle의 테두리를 바꿉니다.
+    func didFindRectangle(rectrangle: Rectangle) {
         
-        sender.setTitle("\(randomRGB.hexValue)", for: .normal)                                                  //text를 바꾼다.
+        seletedRectangleView?.layer.borderWidth = 0.0           //기존 seletedRectangleView초기화
+        
+        seletedRectangleView = retangleViews[rectrangle]        //View 찾기
+        
+        seletedRectangleView?.layer.borderWidth = 2.0           //테두리 바꾸기
+        seletedRectangleView?.layer.borderColor = UIColor.blue.cgColor
+    }
+    
+    //Rectangle이 추가가 됬으니 View에게 알립니다.
+    func didAddRectangle(rectangle: Rectangle) {
+        let rgb = rectangle.rgb
+        let alpha = rectangle.alpha
+        let rectView = RectangleView(rect: rectangle, rgb: rgb, alpha: alpha) //View를 만듬
+        
+        retangleViews[rectangle] = rectView             //Dictionary에 추가
+        self.view.addSubview(rectView)                  //화면에 추가
+        
+        //사각형 추가 버튼이나 detailView의 슬라이더가 추가된 사각형에 가려지더라도 이벤트를 발생시키기 위해서 최상단으로 View를 올렸습니다.
+        self.view.bringSubviewToFront(button)
+        self.view.bringSubviewToFront(detailView)
+    }
+    
+    //Alpha가 바뀌었으니 View에게 알립니다.
+    func didChangeAlpha(selectedRectangle: Rectangle) {
+        seletedRectangleView?.alpha = CGFloat(selectedRectangle.alpha.value)
     }
 }
