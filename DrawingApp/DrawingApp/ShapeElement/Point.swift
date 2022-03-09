1b52a3964d42c8c9799ba672ab8629981dac177b
P_SeoB <akdlsjtm21@naver.com>
2022-03-09 15:20:19 +0900

    [Chore] fix some point error

--- DrawingApp/DrawingApp/ShapeElement/Point.swift
+++ DrawingApp/DrawingApp/ShapeElement/Point.swift
@@ -5,8 +5,6 @@
 //  Created by 박진섭 on 2022/03/01.
 //
 
-import CoreGraphics
-
 struct Point:CustomStringConvertible {
     
     var description: String {
@@ -17,8 +15,8 @@ struct Point:CustomStringConvertible {
     var y:Double
     
     //min과 max는 타입 자체와 관련이 있기 때문에 Static으로 선언했습니다.
-    static let maxX:Double = 1180.0
-    static let maxY:Double = 820.0
+    static let maxX:Double = 980.0
+    static let maxY:Double = 720.0
     
     static func random() -> Point {
         let randomXpoint = Double.random(in: 0.0...Point.maxX)
@@ -31,5 +29,9 @@ struct Point:CustomStringConvertible {
         self.x = x
         self.y = y
     }
+    
+}
+
+extension Point:Hashable {
     
 }
