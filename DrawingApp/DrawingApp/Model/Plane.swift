d799be05042a0141b10d698c71e90130e6c1b26d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-11 00:56:19 +0900

    Refactor: CanvasView, StylerView 배경색, 투명도 바꾸는 로직 수정(Observer Pattern 적용)
    - NotificationCenter에 사각형 배경색, 투명도 바꾸는 옵저버 추가
    - 사용자 액션 입력(뷰) -> 뷰컨트롤러 -> 모델 -> 뷰 컨트롤러 -> 뷰 출력 의 흐름으로 모두 수정
    - 기존의 StylerViewDelegate, CanvasViewDelegate의 출력 끝나고 호출하던 ***Completed() 메소드 모두 제거
    - 제거한 해당 메소드를 모두 NotificationCenter.post로 대체

--- DrawingApp/DrawingApp/Model/Plane.swift
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -14,6 +14,7 @@ struct Plane:CustomStringConvertible{
     
     mutating func findMatchingRectangleModel(x: Double, y: Double){
         guard let rectangle = self[x,y] else {
+            self.selectedRectangleId = nil
             self.notificationCenter.post(name: .rectangleNotFoundFromPlane, object: nil)
             return
         }
@@ -24,6 +25,24 @@ struct Plane:CustomStringConvertible{
     mutating func addRectangle(_ rectangle: Rectangle){
         self.rectangles[rectangle.id] = rectangle
         self.notificationCenter.post(name: .rectangleAdded, object: rectangle, userInfo: nil)
+    }
+    
+    mutating func updateRectangleColor(rgb: [Double]){
+        guard let selectedRectangleId = self.selectedRectangleId else { return }
+        guard let rectangle = self.rectangles[selectedRectangleId] else { return }
+        rectangle.backgroundColor = Rectangle.Color(r: rgb[0]*255, g: rgb[1]*255, b: rgb[2]*255)
+        self.notificationCenter.post(name: .rectangleColorUpdated, object: rgb)
+    }
+    
+    mutating func updateRectangleAlpha(opacity: Int){
+        var opacity = opacity
+        guard let selectedRectangleId = self.selectedRectangleId else { return }
+        guard let rectangle = self.rectangles[selectedRectangleId] else { return }
+        if(opacity == 10){
+            opacity = opacity - 1
+        }
+        rectangle.alpha = Rectangle.Alpha.allCases[opacity]
+        self.notificationCenter.post(name: .rectangleAlphaUpdated, object: opacity)
     }
     
     private func isRectangleInsideTheRange(x: Double, y: Double, rectangle: Rectangle)-> Bool{
