f945504a0e00f953d519132ac8be37729d24079d
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-10 21:09:03 +0900

    [Refactor] createUIColor, createRectangleFrame 메소드 리팩토링 - Shapable 프로토콜의 extension에 구현 - 'get~'으로 이름 변경

--- DrawingApp/DrawingApp/Model/Shapable.swift
+++ DrawingApp/DrawingApp/Model/Shapable.swift
@@ -6,16 +6,39 @@
 //
 
 import Foundation
+import UIKit
 
 protocol Shapable: CustomStringConvertible {
     var id: Identifier { get }
     var size: Size { get }
     var point: Point { get }
     var backGroundColor: Color { get }
+    var alpha: Alpha { get }
 }
 
 extension Shapable {
+    
     var description: String {
         return "(\(self.id)), \(self.point), \(self.size), \(self.backGroundColor)"
+    }
+    
+    func getUIColor() -> UIColor {
+        let red = backGroundColor.red / Color.Range.upper
+        let green = backGroundColor.green / Color.Range.upper
+        let blue = backGroundColor.blue / Color.Range.upper
+        let alpha = alpha.value
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    func getFrame() -> CGRect {
+        let xCoordinate = point.x
+        let yCoordinate = point.y
+        
+        let width = size.width
+        let height = size.height
+        
+        let frame = CGRect(x: xCoordinate, y: yCoordinate, width: width, height: height)
+        return frame
     }
 }
