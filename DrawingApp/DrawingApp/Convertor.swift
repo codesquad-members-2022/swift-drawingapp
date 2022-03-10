e85161d950fc0b21a6ac1613ef25162677088d77
asqw887 <asqw887@gmail.com>
2022-03-11 04:11:22 +0900

    feature : Color->UIColor Convertor 구현

--- DrawingApp/DrawingApp/Convertor.swift
+++ DrawingApp/DrawingApp/Convertor.swift
@@ -9,3 +9,13 @@ import Foundation
 import UIKit
 
 
+struct Convertor {
+    
+    
+    static func convertColor(from color: Color) -> UIColor {
+        let R = CGFloat(color.red) / 255
+        let G = CGFloat(color.green) / 255
+        let B = CGFloat(color.blue) / 255
+        return UIColor(red: R, green: G, blue: B, alpha: 1)
+    }
+}
