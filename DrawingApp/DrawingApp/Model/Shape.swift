aaf99b44aa1f2e8142a14296943b43bfeb421a1a
bibi <bibi6666667@kakao.com>
2022-03-10 16:16:59 +0900

    protocol Shape에 필수 속성만 남기고 나머지 프로퍼티와 init()을 삭제

--- DrawingApp/DrawingApp/Model/Shape.swift
+++ DrawingApp/DrawingApp/Model/Shape.swift
@@ -9,10 +9,4 @@ import Foundation
 
 protocol Shape {
     var id: String { get }
-    var point: Point { get set }
-    var size: Size { get set }
-    var color: Color { get set }
-    var alpha: Alpha { get set } // 1-10 사이값
-    
-    init(id: String, point: Point, size: Size, color: Color, alpha: Alpha)
 }
