d9501cbcc24e1a1d39413e16cb352b1964d9895e
sanghee <anshee97@naver.com>
2022-03-10 15:24:39 +0900

    Feat: Add SideInspectorViewDelegate & Add rectangelButtonDelegate

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var delegate: SideInspectorViewDelegate?
     let sideInspectorView: SideInspectorView = {
         let view = SideInspectorView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -20,31 +21,10 @@ class ViewController: UIViewController {
     let factory = RectangleFactory()
     var plane = Plane()
     
-    
-    @objc func createRectangleButtonTapped(_ sender: UIButton) {
-        let rectangle = factory.createRectangle()
-        plane.addRectangle(rectangle)
-        
-        let myView = UIView(frame: CGRect(x: rectangle.point.x, y: rectangle.point.y, width: rectangle.size.width, height: rectangle.size.height))
-        myView.backgroundColor = UIColor(hex: rectangle.backgroundColor.hexValue)
-        view.addSubview(myView)
-    }
-    
-    
-    func createRectangleButton() {
-        self.button.frame = CGRect(x: x - 250, y: y - 150, width: 150, height: 75)
-        self.button.backgroundColor = UIColor.systemCyan
-        self.button.setTitle("사각형 생성", for: .normal)
-        self.button.tintColor = .white
-        self.button.addTarget(self, action: #selector(createRectangleButtonTapped(_:)), for: .touchUpInside)
-        self.view.addSubview(button)
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        createRectangleButton()
+        sideInspectorView.delegate = self
         
         setInspectorView()
     }
@@ -58,5 +38,18 @@ class ViewController: UIViewController {
             sideInspectorView.widthAnchor.constraint(equalToConstant: 200),
             sideInspectorView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
         ])
+    }
+}
+
+
+
+extension ViewController: SideInspectorViewDelegate {
+    func sideInspectorView(_ sideInspectorView: SideInspectorView, buttonDidTapped: UIButton) {
+        let rectangle = factory.createRectangle()
+        plane.addRectangle(rectangle)
+        
+        let myView = UIView(frame: CGRect(x: rectangle.point.x, y: rectangle.point.y, width: rectangle.size.width, height: rectangle.size.height))
+        myView.backgroundColor = UIColor(hex: rectangle.backgroundColor.hexValue)
+        view.addSubview(myView)
     }
 }
