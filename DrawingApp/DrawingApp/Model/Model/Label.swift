1104cfcd66a15ec43e565777b22cb86af9ab3d0a
BumgeunSong <isp1195@gmail.com>
2022-03-11 18:54:49 +0900

    Remove font color property

--- DrawingApp/DrawingApp/Model/Model/Label.swift
+++ DrawingApp/DrawingApp/Model/Model/Label.swift
@@ -10,12 +10,10 @@ import Foundation
 class Label: ViewModel {
     private(set) var text: String
     private(set) var fontSize: Float
-    private(set) var fontColor: Color
     
-    init(id: ID, origin: Point, size: Size, text: String, fontSize: Float, fontColor: Color) {
+    init(id: ID, origin: Point, size: Size, text: String, fontSize: Float) {
         self.text = text
         self.fontSize = fontSize
-        self.fontColor = fontColor
         super.init(id: id, origin: origin, size: size)
     }
     
@@ -25,21 +23,20 @@ class Label: ViewModel {
         let size = Size.standard()
         let text = dummyString()
         let fontSize = Float.random(in: 16...32)
-        let fontColor = Color.random()
         
-        return Label(id: labelID, origin: origin, size: size, text: text, fontSize: fontSize, fontColor: fontColor)
+        return Label(id: labelID, origin: origin, size: size, text: text, fontSize: fontSize)
     }
     
     static let dummyString: () -> String = {
         let lorem = "Slip inside the eye of your mind Don't you know you might find A better place to play You said that you'd never been But all the things that you've seen Slowly fade away And so Sally can wait She knows it's too late As we're walking on by Her soul slides away But don't look back in anger I heard you say"
         let dummyArray = lorem.components(separatedBy: " ")
-        let randomIndex = Int.random(in: 0..<dummyArray.count)
+        let randomIndex = Int.random(in: 0..<dummyArray.count-5)
         return dummyArray[randomIndex..<randomIndex+5].joined(separator: " ")
     }
 }
 
 extension Label: CustomStringConvertible {
     var description: String {
-        return "(\(id)), \(origin), \(size), \(text), \(fontColor), \(fontSize)p"
+        return "(\(id)), \(origin), \(size), \(text), \(fontSize)p"
     }
 }
