7688057cf6dd55329b7d4cb3b560b11176858032
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-11 23:00:11 +0900

    Refactor: Rectangle.Alpha 타입 구조체로 변경
    - 외부에서 opacity를 정수값으로 받아서 열거형의 allCases 배열의 인덱스로 활용하던 것에서, 구조체의 생성자 파라미터로 받아 내부에서 다시 초기화하도록 수정
    - Rectangle.Alpha 내부에 10가지 케이스를 가진 Opacity 열거형을 별도로 선언
    - Alpha의 생성자 내부에서 Opacity의 케이스 중 적절한 것을 골라 초기화하는 것

--- DrawingApp/DrawingApp/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -74,25 +74,39 @@ class Rectangle: CustomStringConvertible{
         }
     }
     
-    enum Alpha:Int, CaseIterable, CustomStringConvertible{
-        
-        case one = 1
-        case two = 2
-        case three = 3
-        case four = 4
-        case five = 5
-        case six = 6
-        case seven = 7
-        case eight = 8
-        case nine = 9
-        case ten = 10
-        
-        var description: String{
-            return "Alpha:\(String(self.rawValue))"
+    struct Alpha: CustomStringConvertible{
+        enum Opacity:Float, CaseIterable{
+            
+            case one = 0.1
+            case two = 0.2
+            case three = 0.3
+            case four = 0.4
+            case five = 0.5
+            case six = 0.6
+            case seven = 0.7
+            case eight = 0.8
+            case nine = 0.9
+            case ten = 1.0
+
         }
         
-        var opacity: Float{
-            return Float(self.rawValue) / 10
+        var opacity: Opacity = .ten
+        var description: String{
+            return "Alpha:\(String(self.opacity.rawValue))"
+        }
+        
+        init(opacity: Int){
+            self.opacity = Opacity.allCases[adjustRange(value: opacity)]
+        }
+        
+        private func adjustRange(value: Int)->Int{
+            if(value<0){
+                return 0
+            }else if(value >= Opacity.allCases.count){
+                return Opacity.allCases.count - 1
+            }else{
+                return value
+            }
         }
     }
     
