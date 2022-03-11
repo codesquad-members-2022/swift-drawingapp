20c7053debe16fa7ca923f97ff69090fb0387d78
sol <hskim6521@naver.com>
2022-03-11 12:42:43 +0900

    Refactor: userInfo에 사용될 String을 constant로 선언해서 사용하기 위한 K 구조체 선언

---
+++ DrawingApp/DrawingApp/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  DrawingApp
+//
+//  Created by 김한솔 on 2022/03/11.
+//
+
+import Foundation
+
+struct K {
+    struct PlaneRectangle {
+        static let changed = "ChangedRectangle"
+        static let specified = "specifiedRectangle"
+        static let added = "addedRectanlge"
+    }
+}
