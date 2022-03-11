54badcc581f6feb85212f9f8b046ce835fae9a7d
sanghee <anshee97@naver.com>
2022-03-11 14:34:53 +0900

    Refactor: View 분리 (TotalView = CanvasView + SideInspectorView)

--- DrawingApp/DrawingApp/Controller/ViewController.swift
+++ DrawingApp/DrawingApp/Controller/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     var delegate: SideInspectorViewDelegate?
-    let sideInspectorView: SideInspectorView = {
-        let view = SideInspectorView()
+    let totalView: TotalView = {
+        let view = TotalView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -24,42 +24,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        sideInspectorView.delegate = self
+        totalView.sideInspectorView.delegate = self
         
-        setInspectorView()
+        setTotalView()
     }
     
-    private func setInspectorView() {
-        view.addSubview(sideInspectorView)
+    
+    private func setTotalView() {
+        view.addSubview(totalView)
         
         NSLayoutConstraint.activate([
-            sideInspectorView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-            sideInspectorView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-            sideInspectorView.widthAnchor.constraint(equalToConstant: 200),
-            sideInspectorView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
+            totalView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            totalView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            totalView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            totalView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
         ])
-        
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        
-        
     }
 }
 
 
-
+// TODO: SideInspectorView의 영역을 제외한 부분에 사각형 그려지도록 하기
+// TODO: 입력을 처리하는 곳에서는 뷰 -> 뷰컨트롤러 -> 모델로 들어가는 입력 흐름만 처리                                                           
+// TODO: 아래 뷰를 생성하는 출력 흐름 분리해야함! 모델에서 뷰 컨트롤러를 거쳐서 다시 뷰를 추가하는 흐름. 모델 -> 뷰컨트롤러 -> 뷰 를 만들기
 extension ViewController: SideInspectorViewDelegate {
-    // TODO: SideInspectorView의 영역을 제외한 부분에 사각형 그려지도록 하기
     func sideInspectorView(_ sideInspectorView: SideInspectorView, buttonDidTapped: UIButton) {
-        let rectangle = factory.createRectangle()
-        plane.addRectangle(rectangle)
-        
-        let myView = UIView(frame: CGRect(x: rectangle.point.x, y: rectangle.point.y, width: rectangle.size.width, height: rectangle.size.height))
-        myView.backgroundColor = UIColor(hex: rectangle.backgroundColor.getHexValue())
-        view.addSubview(myView)
+            let rectangle = factory.createRectangle()
+            plane.addRectangle(rectangle)
+
+            let myView = UIView(frame: CGRect(x: rectangle.point.x, y: rectangle.point.y, width: rectangle.size.width, height: rectangle.size.height))
+            myView.backgroundColor = UIColor(hex: rectangle.backgroundColor.getHexValue())
+            view.addSubview(myView)
         
     }
 }
