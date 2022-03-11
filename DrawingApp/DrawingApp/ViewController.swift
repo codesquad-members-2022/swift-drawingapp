76fdc215c02adc65f1a522c456d108a5d58b6f37
bibi <bibi6666667@kakao.com>
2022-03-11 21:25:13 +0900

    ViewController에 UIGestureRecognizerDelegate 구현 및 터치 테스트

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -9,19 +9,24 @@ import UIKit
 import OSLog
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     let logger = Logger()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let factory = ShapeFactory()
-        let rectangle1 = factory.createRectangle(point: Point(x: 10, y: 200), size: Size(width: 150, height: 120), color: Color(r: 245, g: 0, b: 245), alpha: .nine)
-        let rectangle2 = factory.createRectangle(point: Point(x: 110, y: 180), size: Size(width: 150, height: 120), color: Color(r: 43, g: 124, b: 95), alpha: .five)
-        let rectangle3 = factory.createRectangle(point: Point(x: 590, y: 90), size: Size(width: 150, height: 120), color: Color(r: 98, g: 244, b: 15), alpha: .seven)
-        let rectangle4 = factory.createRectangle(point: Point(x: 330, y: 450), size: Size(width: 150, height: 120), color: Color(r: 125, g: 39, b: 99), alpha: .one)
-        logger.info("\(rectangle1 as! Rectangle)")
-        logger.info("\(rectangle2 as! Rectangle)")
-        logger.info("\(rectangle3 as! Rectangle)")
-        logger.info("\(rectangle4 as! Rectangle)")
+        
+        let tapGestureRecognizer = UITapGestureRecognizer()
+        tapGestureRecognizer.delegate = self
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+}
+
+extension ViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        print("touched!")
+        return true
     }
 }
