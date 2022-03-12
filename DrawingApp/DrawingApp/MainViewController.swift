9265f84dc6e47b111f106f4cd3ea6cda3dbc41d4
P_SeoB <akdlsjtm21@naver.com>
2022-03-12 23:17:41 +0900

    [ADD] ImageButton

--- DrawingApp/DrawingApp/MainViewController.swift
+++ DrawingApp/DrawingApp/MainViewController.swift
@@ -14,19 +14,22 @@ final class MainViewController: UIViewController{
     private var plane = Plane()
     //view
     private var detailView = DetailView(frame: .zero)       //생성시 오해를 막고 기존 생성자와 관련있게 하기 위해 frame에 .zero를 대입했습니다.
-    private var button:UIButton = RectangleButton(frame: .zero)
+    private var rectangleButton:UIButton = RectangleButton(frame: .zero)
+    private var imageButton:UIButton = ImageButton(frame: .zero)
     
     //그려진 retangleView를 모델(Rectangle)을 Key로 찾는 Dictionary로 만들어서 모델과 매칭을 시켜주었습니다.
     private var retangleViews = [Rectangle:RectangleView]()
     //선택된 rectangleView
     private var seletedRectangleView:RectangleView?
     
+    private let imagePicker = UIImagePickerController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNotificationCenter()
         
+        configureImageButton()
         configureRectangleButton()
         configureDeatailView()
         configureTapGesture()
@@ -53,6 +56,25 @@ final class MainViewController: UIViewController{
         )
     }
     
+    private func configureImageButton() {
+        let width = 150.0
+        let height = 100.0
+        let x:CGFloat = (self.view.frame.maxX / 2.0) - (width / 2) - width
+        let y:CGFloat = (self.view.frame.maxY - height)
+        
+        imageButton.layer.zPosition = 1.0
+        imageButton.frame = CGRect(x: x, y: y, width: width, height: height)
+//        imageButton.addAction(imageInputToView(), for: .touchUpInside)
+    }
+    
+//    private func imageInputToView() -> UIAction {
+//        let action:UIAction = { _ in
+//
+//        }
+//        return action
+//    }
+    
+    
     //사각형 추가 버튼 Frame정의 및 Action추가.
     private func configureRectangleButton() {
         let width = 150.0
@@ -60,13 +82,14 @@ final class MainViewController: UIViewController{
         let x:CGFloat = (self.view.frame.maxX / 2.0) - (width / 2)
         let y:CGFloat = (self.view.frame.maxY - height)
         
-        button.layer.zPosition = 1.0                                //생성되는 사각형에 겹쳐서 안보이는 경우를 막기위해 zPosition을 주었다.
-        button.frame = CGRect(x: x, y: y, width: width, height: height)
-        button.addAction(makeAddRectangleAction(), for: .touchUpInside)
+        rectangleButton.layer.zPosition = 1.0                                //생성되는 사각형에 겹쳐서 안보이는 경우를 막기위해 zPosition을 주었다.
+        
+        rectangleButton.frame = CGRect(x: x, y: y, width: width, height: height)
+        rectangleButton.addAction(addRectangleAction(), for: .touchUpInside)
     }
     
     //버튼 액션 - 사각형 추가
-    private func makeAddRectangleAction() -> UIAction {
+    private func addRectangleAction() -> UIAction {
         let action = UIAction {_ in
             let id = IDFactory.makeRandomID()
             let size = Size(width: 150, height: 120)
@@ -83,8 +106,9 @@ final class MainViewController: UIViewController{
     
     //Custom View추가
     private func addViews() {
-        view.addSubview(button)
+        view.addSubview(rectangleButton)
         view.addSubview(detailView)
+        view.addSubview(imageButton)
     }
 }
 
@@ -158,7 +182,7 @@ extension MainViewController:UIGestureRecognizerDelegate {
         
         self.retangleViews[newRectangle] = rectangleView
         
-        self.view.bringSubviewToFront(button)
+        self.view.bringSubviewToFront(rectangleButton)
         self.view.bringSubviewToFront(detailView)
     }
     
@@ -180,10 +204,6 @@ extension MainViewController:UIGestureRecognizerDelegate {
         self.detailView.backgroundColorButton.setTitle("\(seletedRectangle.rgb.hexValue)", for: .normal)
     }
 }
-
-
-
-
 
 //MARK: -- DetailViewDelegate
 //슬라이더를 움직일때 마다 현재 클릭한 모델 사각형의 alpha값을 바꾼다.
--- DrawingApp/DrawingApp/View/ImageButton.swift
+++ DrawingApp/DrawingApp/View/ImageButton.swift
@@ -1,36 +1,36 @@
 //
-//  RectangleButton.swift
+//  ImageButton.swift
 //  DrawingApp
 //
-//  Created by 박진섭 on 2022/03/08.
+//  Created by 박진섭 on 2022/03/11.
 //
 
 import UIKit
 
 //button을 정의하는 코드가 너무 길어져서 ViewController에서 Button을 분리해서 선언했습니다.
-final class RectangleButton:UIButton {
+final class ImageButton:UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.frame = frame
-        configureRectangleButton()
-        configureRectangleButtonTapped()
+        configureImageButton()
+        configureImageButtonTapped()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         self.frame = frame
-        configureRectangleButton()
-        configureRectangleButtonTapped()
+        configureImageButton()
+        configureImageButtonTapped()
     }
     
     
     //사각형 추가 버튼
-    private func configureRectangleButton() {
+    private func configureImageButton() {
         self.tintColor = .black
         
         var configuration = UIButton.Configuration.plain()
-        configuration.title = "사각형"
+        configuration.title = "사진"
         
         configuration.imagePlacement = .top
         configuration.imagePadding = 20
@@ -45,9 +45,9 @@ final class RectangleButton:UIButton {
     
     
     //버튼 액션 - highLigted
-    private func configureRectangleButtonTapped() {
-        let squareImage = UIImage(systemName: "square")
-        let highlightedImage = UIImage(systemName: "square.fill")
+    private func configureImageButtonTapped() {
+        let squareImage = UIImage(systemName: "photo")
+        let highlightedImage = UIImage(systemName: "photo.fill")
         
         //사각형 버튼 터치시 변화를 보여주기 위해 선언함.
         self.configurationUpdateHandler = { button in
