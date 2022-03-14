142c7bd412487a5a397d661c742b385ee988d25d
P_SeoB <akdlsjtm21@naver.com>
2022-03-14 11:43:11 +0900

    [Rename] rectangle -> PlaneRectangle

--- DrawingApp/DrawingApp/Model/PlaneRectangle.swift
+++ DrawingApp/DrawingApp/Model/PlaneRectangle.swift
@@ -1,19 +1,20 @@
 //
-//  Rectangle.swift
+//  PlaneRectangle.swift
 //  DrawingApp
 //
-//  Created by 박진섭 on 2022/03/01.
+//  Created by 박진섭 on 2022/03/14.
 //
 
-final class Rectangle:CustomStringConvertible {
+
+final class PlaneRectangle:CustomStringConvertible,Rectangle {
     
     var description: String {
         return "\(id), \(origin), \(size),\(rgb), \(alpha)"
     }
     
-    private let id:ID
-    let origin:Point
-    let size:Size
+    var id:ID
+    var origin:Point
+    var size:Size
     var rgb:RGB
     var alpha:Alpha
     
@@ -28,12 +29,11 @@ final class Rectangle:CustomStringConvertible {
 }
 
 //선택한 Rectangle모델을 찾기위해 Dictionary를 선언했기에 Hashable프로토콜을 채택한다.
-extension Rectangle:Hashable {
+extension PlaneRectangle:Hashable {
     func hash(into hasher: inout Hasher) {
         hasher.combine(self.id)
     }
-    static func == (lhs: Rectangle, rhs: Rectangle) -> Bool {
+    static func == (lhs: PlaneRectangle, rhs: PlaneRectangle) -> Bool {
         lhs.id == rhs.id
     }
 }
-
