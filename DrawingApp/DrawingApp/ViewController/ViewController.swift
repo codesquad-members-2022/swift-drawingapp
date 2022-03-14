b7997667c5a6f4303ee2545be5b8e5f6ca035e57
asqw887 <asqw887@gmail.com>
2022-03-14 17:48:32 +0900

    fix : border alpha 조정

--- DrawingApp/DrawingApp/ViewController/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController/ViewController.swift
@@ -37,16 +37,17 @@ extension ViewController: RectangleViewDelegate {
         let touchedPoint = Point(x: point.x, y: point.y)
         if let selected = plane.ExistRectangle(at: touchedPoint) {
             
+            // 다른 사각형을 선택했을때, 이전 사각형 테두리 지워주기
             if rectangleViews[selected] != selectedRectangleView {
                 selectedRectangleView?.layer.borderWidth = 0
             }
             
+            // 선택한 사각형에 대한 속성을 컨트롤러 뷰에 표현, 테두리 표시
             selectedRectangleView = rectangleViews[selected]
             controllerView.alphaSlider.setValue(Float(selectedRectangleView?.alpha ?? 1.0) * 10 , animated: true)
             controllerView.backgroundButton.setTitle(selectedRectangleView?.backgroundColor?.convertHex(), for: .normal)
             selectedRectangleView?.layer.borderWidth = 5
-            plane.selectedRectangle = selected
-            
+            selectedRectangleView?.layer.borderColor = CGColor(red: 1, green: 0, blue: 0, alpha: 1)
         } else {
             selectedRectangleView?.layer.borderWidth = 0
         }
@@ -58,17 +59,18 @@ extension ViewController: PlaneDelegate {
 
     func RectangleDidAdd(_ rectangle: Rectangle) {
         let rect = UIView(frame: CGRect(x: rectangle.position.x, y: rectangle.position.y, width: rectangle.size.width, height: rectangle.size.height))
-        rect.backgroundColor = Convertor.convertColor(from: rectangle.backgroundColor)
-        rect.alpha = Double(rectangle.alpha.rawValue) * 0.1
+//        rect.backgroundColor = Convertor.convertColor(from: rectangle.backgroundColor)
+        let alpha = Double(rectangle.alpha.rawValue) * 0.1
+        let color = Convertor.convertColor(from: rectangle.backgroundColor, alpha: alpha)
+        rect.backgroundColor = color
+        
         self.canvasView.addSubview(rect)
-        rect.layer.borderColor = CGColor(red: 1, green: 0, blue: 0, alpha: 1)
         rect.isUserInteractionEnabled = false
         
         rectangleViews[rectangle] = rect
     }
     
     func BackgroundDidChanged() {
-        // 뷰의 배경색 버튼 Label을 변경 (16진수로 변환한다)
         let hexString = selectedRectangleView?.backgroundColor?.convertHex()
         controllerView.backgroundButton.setTitle(hexString, for: .normal)
     }
@@ -86,7 +88,8 @@ extension ViewController: ControllerViewDelegate {
     
     func backgroundButtonDidTouch() {
         let changedColor = Color.randomColor()
-        selectedRectangleView?.backgroundColor = Convertor.convertColor(from: changedColor)
+        guard let selected = selectedRectangleView else { return }
+        selected.backgroundColor = Convertor.convertColor(from: changedColor, alpha: selected.alpha)
         plane.changeBackgroundColor(to: changedColor)
         
         // 모델에게 해당 Rect의 색이 변경되었음을 알려주기
