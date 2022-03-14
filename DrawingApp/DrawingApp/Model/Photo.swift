dfef565f84ee48c24125db09843a1a473d23f048
sol <hskim6521@naver.com>
2022-03-14 17:34:38 +0900

    Feat: 사진을 포함하는 사각형 객체를 구현하기 위해 Photo 클래스 선언
    Plane, ViewController에서 Photo와 Rectangle을 한꺼번에 처리가 가능하도록 Photo 클래스가 Rectangularable 프로토콜을 채택
    RectangleFactory에 새로운 Photo 인스턴스를 생성하는 makePhoto(in:image:) 메서드 선언

---
+++ DrawingApp/DrawingApp/Model/Photo.swift
@@ -0,0 +1,58 @@
+//
+//  Photo.swift
+//  DrawingApp
+//
+//  Created by 김한솔 on 2022/03/14.
+//
+
+import Foundation
+import UIKit
+
+class Photo: Rectangularable {
+    private let id: ID
+    private(set) var size: Size
+    private(set) var point: Point
+    private(set) var backgroundColor: BackgroundColor
+    private(set) var alpha: Alpha
+    private(set) var image: UIImage
+    
+    func isPointInArea(_ point: Point) -> Bool {
+        return point.x >= self.point.x && point.x <= self.point.x + self.size.width &&
+        point.y >= self.point.y && self.point.y <= self.point.y + self.size.height
+    }
+    
+    func changeBackgroundColor(to newColor: BackgroundColor) {
+        self.backgroundColor = newColor
+    }
+    
+    func changeAlphaValue(to newAlpha: Alpha) {
+        self.alpha = newAlpha
+    }
+    
+    init(id: ID, size: Size, point: Point, image: UIImage, alpha: Alpha) {
+        self.id = id
+        self.size = size
+        self.point = point
+        self.alpha = alpha
+        self.image = image
+        self.backgroundColor = BackgroundColor.init(r: 0, g: 0, b: 0)
+    }
+}
+
+extension Photo: CustomStringConvertible {
+    var description: String {
+        return id.description
+    }
+}
+
+extension Photo: Equatable {
+    static func == (lhs: Photo, rhs: Photo) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+extension Photo: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
