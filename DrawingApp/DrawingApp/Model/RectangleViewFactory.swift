57454fa7b921c43665fbba009270548abad4c427
sungju-kim <wnwn0719@gmail.com>
2022-03-14 17:29:05 +0900

    Feat: RectangleViewFactory - makePhotoView 구현

--- DrawingApp/DrawingApp/Model/RectangleViewFactory.swift
+++ DrawingApp/DrawingApp/Model/RectangleViewFactory.swift
@@ -10,14 +10,21 @@ import UIKit
 
 struct RectangleViewFactory {
 
-    static func makeView(of rectangle : Rectangle) -> RectangleView {
+    static func makeView(of rectangle : Rectangle) -> RectangleView? {
         let size = rectangle.size
         let position = rectangle.position
-        let color = rectangle.backGroundColor
+        guard let color = rectangle.backGroundColor else {return nil}
         let alpha = rectangle.alpha
         let rectangleFrame = CGRect(x: position.x , y: position.y, width: size.width, height: size.height)
         return RectangleView(from: rectangleFrame, color: color, alpha: alpha)
     }
     
-    
+    static func makePhotoView(of photoRectangle: PhotoRectangle) -> PhotoRectangleView? {
+        let size = photoRectangle.size
+        let position = photoRectangle.position
+        let image = photoRectangle.backGroundImage
+        let alpha = photoRectangle.alpha
+        let rectangleFrame = CGRect(x: position.x , y: position.y, width: size.width, height: size.height)
+        return PhotoRectangleView(from: rectangleFrame, image: image, alpha: alpha)
+    }
 }
