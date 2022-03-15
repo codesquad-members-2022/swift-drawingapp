7669aead5db6dddc7be277eefd828716d35daa7d
asqw887 <asqw887@gmail.com>
2022-03-16 01:49:54 +0900

    refector : 입출력 구분

--- DrawingApp/DrawingApp/Convertor.swift
+++ DrawingApp/DrawingApp/Convertor.swift
@@ -10,12 +10,19 @@ import UIKit
 
 
 struct Convertor {
-    static func convertColor(from color: Color, alpha : CGFloat = 1.0) -> UIColor {
+    static func convertColor(from color: Color, alpha : Alpha) -> UIColor {
         let R = CGFloat(color.red) / 255
         let G = CGFloat(color.green) / 255
         let B = CGFloat(color.blue) / 255
-        return UIColor(red: R, green: G, blue: B, alpha: alpha)
+        let alphaValue = CGFloat(alpha.rawValue) * 0.1
+        return UIColor(red: R, green: G, blue: B, alpha: alphaValue)
     }
+    
+    static func colorToHexString(_ color: Color) -> String {
+        let hexString = String.init(format: "#%02lX%02lX%02lX", color.red, color.green, color.blue)
+        return hexString
+    }
+
 }
 
 extension UIColor  {
--- DrawingApp/DrawingApp/Model/Plane.swift
+++ DrawingApp/DrawingApp/Model/Plane.swift
@@ -9,8 +9,10 @@ import Foundation
 
 protocol PlaneDelegate {
     func RectangleDidAdd(_ rectangle: Rectangle)
-    func BackgroundDidChanged()
-    func alpahDidChanged()
+    func BackgroundDidChanged(_ rectangle: Rectangle)
+    func alpahDidChanged(_ rectangle: Rectangle)
+    func rectangleDidMutate(_ rectangle: Rectangle)
+//    func didCancleRectangle()
 }
 
 
@@ -33,12 +35,13 @@ struct Plane{
         delegate?.RectangleDidAdd(rectangle)
     }
     
-    func ExistRectangle(at point: Point) -> Rectangle?{
+    mutating func ExistRectangle(at point: Point) -> Rectangle?{
      
         for rect in rectangles {
             if (rect.leftTopPoint.x <= point.x && rect.leftTopPoint.y <= point.y) && (rect.rightBottomPoint.x >= point.x && rect.rightBottomPoint.y >= point.y) {
                 return rect
                 }
+            
             }
         return nil
     }
@@ -46,15 +49,37 @@ struct Plane{
     func changeBackgroundColor(to color: Color){
         selectedRectangle?.changedBackGroundColor(to: color)
         // 배경색 모델이 바뀌었으므로 컨트롤러에게 전달
-        delegate?.BackgroundDidChanged()
+        delegate?.BackgroundDidChanged(selectedRectangle!)
         
     }
     
     func changeAlpha(value : Int){
         selectedRectangle?.changedAlpha(to: value)
-        delegate?.alpahDidChanged()
+        delegate?.alpahDidChanged(selectedRectangle!)
     }
     
+//    func testrect(_ rectangle: Rectangle?) {
+//        if let rectangle = rectangle {
+//            delegate?.rectangleDidMutate(rectangle)
+//        } else {
+//            delegate?.rectangleDidMutate(nil)
+//        }
+//    }
+  
+    mutating func setSelectedRectangle(_ selected: Rectangle){
+        if selected != selectedRectangle {
+            self.selectedRectangle = selected
+            delegate?.rectangleDidMutate(selected)
+            delegate?.BackgroundDidChanged(selected)
+            delegate?.alpahDidChanged(selected)
+        }
+    }
+    
+    
+    
+    
+    
+ 
 
     
 }
