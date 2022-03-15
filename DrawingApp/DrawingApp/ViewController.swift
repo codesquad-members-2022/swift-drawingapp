f5c8bbf14921549195885df33adbf52710547249
OceanShape <oceanshape700@gmail.com>
2022-03-15 13:50:19 +0900

    feat: Add ViewController.createSquare()

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let idElement: String = "abcdefghijklmnopqrstuvwxyz0123456789"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let s = createSquare(Size(100, 100), Point(400, 500), 128, 128, 128, 5)
+        print(s)
+    }
+    
+    func createSquare(_ size: Size, _ point: Point, _ r: Int, _ g: Int, _ b: Int, _ a: Int) -> Square {
+        var id = String((0 ..< 9).map{ _ in idElement.randomElement()! })
+        id.insert("-", at: id.index(id.startIndex, offsetBy: 3))
+        id.insert("-", at: id.index(id.startIndex, offsetBy: 7))
+        return Square(id, Size(100, 100), Point(400, 500), 128, 128, 128, 5)
     }
 
-
 }
-
