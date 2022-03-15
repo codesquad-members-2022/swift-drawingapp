30113731aa4445952e36ad2b334cd0b38f16de2d
sungju-kim <wnwn0719@gmail.com>
2022-03-15 12:00:13 +0900

    Refactor: PhotoRectangleView addSubview 분리

--- DrawingApp/DrawingApp/View/PhotoRectangleView.swift
+++ DrawingApp/DrawingApp/View/PhotoRectangleView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PhotoRectangleView: RectangleView {
-    var photoImageView : UIImageView = {
+    private var photoImageView : UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleToFill
@@ -17,17 +17,20 @@ class PhotoRectangleView: RectangleView {
     
     convenience init(from rectangleFrame: CGRect, imageData: Data, alpha: Alpha) {
         self.init(frame: rectangleFrame)
+        layoutImageView()
         setImage(imageData: imageData)
         setAlpha(alpha: alpha)
     }
     
-    func setImage(imageData : Data) {
-        self.photoImageView.image = UIImage(data: imageData)
+    private func layoutImageView() {
         self.addSubview(photoImageView)
         photoImageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         photoImageView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        
+    }
+    
+    func setImage(imageData : Data) {
+        self.photoImageView.image = UIImage(data: imageData)
     }
 }
