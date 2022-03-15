82960d5a964c193868faa215c82ca9fd91291d09
bibi <bibi6666667@kakao.com>
2022-03-15 11:14:26 +0900

    사각형 버튼 클릭으로 랜덤 위치에 사각형 만들기 구현

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -9,17 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var plane = Plane()
+    let shapeFactory = ShapeFactory()
     
     @IBOutlet weak var rectangleButton: UIButton!
     
     @IBOutlet weak var rgbValue: UILabel!
     
     @IBOutlet weak var alphaValue: UILabel!
-    //    @IBOutlet weak var tappableView: UIView! {
-//        didSet {
-//            tappableView.backgroundColor = .blue
-//        }
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +30,21 @@ class ViewController: UIViewController {
 
     @objc func didTapView(_ sender: UITapGestureRecognizer) {
         print("didTapView", sender)
+        let touchCGPoint = sender.location(in: self.view)
+        let touchPoint = Point(x: touchCGPoint.x, y: touchCGPoint.y)
+        print(plane.isThereARectangle(point: touchPoint))
+    }
+    
+    @IBAction func drawRectangle(_ sender: Any) {
+        let bounds = UIScreen.main.bounds
+        let randomX = Int.random(in: 1...Int(bounds.width))
+        let randomY = Int.random(in: 1...Int(bounds.height))
+        print(randomX, randomY)
+        let rectangle = shapeFactory.createRandomRectangle(point: Point(x: Double(randomX), y: Double(randomY)))
+        let rectangleView = UIView(frame: CGRect(x: randomX, y: randomY, width: 150, height: 120))
+        rectangleView.backgroundColor = .blue
+        self.view.addSubview(rectangleView)
+        plane.addRectangle(rectangle)
     }
     
     @IBAction func resetRGB(_ sender: Any) {
