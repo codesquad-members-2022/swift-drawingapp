c8d362187f796edbef8a48e06873be3c8e773218
OceanShape <oceanshape700@gmail.com>
2022-03-15 12:37:23 +0900

    feat: Add Square&Size&Point class

---
+++ DrawingApp/DrawingApp/Model/Size.swift
@@ -0,0 +1,18 @@
+//
+//  Size.swift
+//  DrawingApp
+//
+//  Created by 허태양 on 2022/03/10.
+//
+
+import Foundation
+
+class Size {
+    var Width: Double
+    var Height: Double
+
+    init(_ width: Double, _ height: Double) {
+        self.Width = width
+        self.Height = height
+    }
+}
---
+++ DrawingApp/DrawingApp/Model/Square.swift
@@ -0,0 +1,28 @@
+//
+//  Square.swift
+//  DrawingApp
+//
+//  Created by 허태양 on 2022/03/10.
+//
+
+import Foundation
+
+class Square {
+    var id: String
+    var size: Size
+    var point: Point
+    var R: Int
+    var G: Int
+    var B: Int
+    var A: Int
+
+    init(_ id: String, _ size: Size, _ point: Point, _ r: Int, _ g: Int, _ b: Int, _ a: Int) {
+        self.id = id
+        self.size = size
+        self.point = point
+        self.R = r
+        self.G = g
+        self.B = b
+        self.A = a
+    }
+}
