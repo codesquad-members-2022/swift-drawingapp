50290cba5d4a83c8081658d744a3dd65f6bb627c
sungju-kim <wnwn0719@gmail.com>
2022-03-15 11:35:14 +0900

    Chore: backGroundColor => backgroundColor 변경

--- DrawingApp/DrawingApp/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Model/Rectangle.swift
@@ -11,13 +11,13 @@ class Rectangle {
     let id : Id = Id()
     var size : Size
     var position : Position
-    var backGroundColor : Color?
+    var backgroundColor : Color?
     var alpha : Alpha
     
     init(size: Size, position : Position, color : Color?, alpha : Alpha) {
         self.size = size
         self.position = position
-        self.backGroundColor = color
+        self.backgroundColor = color
         self.alpha = alpha
     }
     
@@ -28,11 +28,11 @@ extension Rectangle : CustomStringConvertible, Equatable {
          return lhs.id === rhs.id &&
                 lhs.size === rhs.size &&
                 lhs.position === rhs.position &&
-                lhs.backGroundColor === rhs.backGroundColor &&
+                lhs.backgroundColor === rhs.backgroundColor &&
                 lhs.alpha === rhs.alpha
     }
     
     var description: String {
-        return "(\(self.id)), \(position), \(self.size), \(String(describing: self.backGroundColor)) , \(self.alpha)"
+        return "(\(self.id)), \(position), \(self.size), \(String(describing: self.backgroundColor)) , \(self.alpha)"
     }
 }
