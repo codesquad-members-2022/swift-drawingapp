a3277f8423a807bd94bf82bfb07655b762273433
sanghee <anshee97@naver.com>
2022-03-16 02:20:18 +0900

    Feat: Add TapGesture to CanvasView

---
+++ DrawingApp/DrawingApp/CanvasViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasViewDelegate.swift
+//  DrawingApp
+//
+//  Created by 안상희 on 2022/03/16.
+//
+
+import Foundation
+
+protocol CavasViewDelegate {
+    func canvasViewDidTouched(x: Double, y: Double)
+}
