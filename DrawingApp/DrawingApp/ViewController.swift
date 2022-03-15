abaeb65b057f4f82ce2afc3f4b654f7ae7786aa3
OceanShape <oceanshape700@gmail.com>
2022-03-15 16:07:44 +0900

    test: Add os_log for Square

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import OSLog
 
 class ViewController: UIViewController {
     let idElement: String = "abcdefghijklmnopqrstuvwxyz0123456789"
@@ -13,15 +14,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let s = createSquare(Size(100, 100), Point(400, 500), 128, 128, 128, 5)
-        print(s)
+        var squareAry : [Square] = []
+        
+        for i in 1..<5 {
+            squareAry.append(createSquare(Size(Double(i * 100), Double(i * 200)), Point(Double(i * 300), Double(i * 400)), i + 10, i + 20, i + 30, 2 * i + 1))
+        }
+        
+        for i in 0..<4 {
+            os_log("Rect%@ %@", "\(i)", "\(squareAry[i])")
+        }
     }
     
     func createSquare(_ size: Size, _ point: Point, _ r: Int, _ g: Int, _ b: Int, _ a: Int) -> Square {
         var id = String((0 ..< 9).map{ _ in idElement.randomElement()! })
         id.insert("-", at: id.index(id.startIndex, offsetBy: 3))
         id.insert("-", at: id.index(id.startIndex, offsetBy: 7))
-        return Square(id, Size(100, 100), Point(400, 500), 128, 128, 128, 5)
+        return Square(id, size, point, r, g, b, a)
     }
 
 }
