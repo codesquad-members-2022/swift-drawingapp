03ec4cb7328e2e47ba3866b5cdbd4b7b8eb05f70
wnsxor1993 <wnsxor1993@naver.com>
2022-03-15 22:06:21 +0900

    Chore: Copy 메서드 개선 - UIView extension에서 다운캐스팅을 통한 copy 메서드 호출 기능 구현

--- DrawingApp/View/UIView.swift
+++ DrawingApp/View/UIView.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 import UIKit
+import os
 
-extension UIView: ViewDragable{}
+extension UIView: ViewDragable{
+    func copyCustomView() -> UIView{
+        if let rectView = self as? RectangleView, let extraView = rectView.copy() as? UIView{
+            return extraView
+        } else if let imageView = self as? ImageView, let extraView = imageView.copy() as? UIView{
+            return extraView
+        } else{
+            os_log("There is no match downcasting")
+            return UIView()
+        }
+    }
+    
+}
