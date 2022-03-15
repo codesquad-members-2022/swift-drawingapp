318e80cf81d593c412a765bcf393885912b78f38
sanghee <anshee97@naver.com>
2022-03-15 14:56:54 +0900

    Feat: Add tapGesture

--- DrawingApp/DrawingApp/View/CanvasView.swift
+++ DrawingApp/DrawingApp/View/CanvasView.swift
@@ -6,19 +6,27 @@
 //
 
 import Foundation
+import os
 import UIKit
 
 /// 사각형이 그려지는 뷰
 class CanvasView: UIView {
 
+    private var touchPositionOfX = 0.0
+    private var touchPositionOfY = 0.0
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
+        
+        setTapGesture()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         translatesAutoresizingMaskIntoConstraints = false
+        
+        setTapGesture()
     }
     
     func drawRectangle(rectangle: Rectangle) {
@@ -26,5 +34,24 @@ class CanvasView: UIView {
         let myView = UIView(frame: CGRect(x: rectangle.point.x, y: rectangle.point.y, width: rectangle.size.width, height: rectangle.size.height))
         myView.backgroundColor = UIColor(hex: rectangle.backgroundColor.getHexValue())
         addSubview(myView)
+    }
+    
+    func setTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        self.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        let log = Logger()
+        log.info("좌표 X: \(self.touchPositionOfX), Y: \(self.touchPositionOfY)")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = event?.allTouches?.first {
+            let loc: CGPoint = touch.location(in: touch.view)
+            
+            touchPositionOfX = loc.x
+            touchPositionOfY = loc.y
+        }
     }
 }
