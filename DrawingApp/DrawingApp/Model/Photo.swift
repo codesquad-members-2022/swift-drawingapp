743044d9691f06c39c69de42e258271d62444e55
sol <hskim6521@naver.com>
2022-03-15 18:45:28 +0900

    Refactor: Rectangularable 프로토콜에 정의된 backgroundColorButtonShouldBecomeHidden 프로퍼티 제거
    새로 타입이 추가될때마다 이런 프로퍼티를 추가하기보다는, 특정 Behavior를 가진 프로토콜이 있나없나로 무언가를 판단할 수 있도록 수정

--- DrawingApp/DrawingApp/Model/Photo.swift
+++ DrawingApp/DrawingApp/Model/Photo.swift
@@ -14,10 +14,6 @@ protocol imageDataHavable {
 class Photo: AnyRectangularable, imageDataHavable {
     private(set) var image: Data
     
-    override var backgroundColorButtonShouldBecomeHidden: Bool {
-        return true
-    }
-    
     init(size: Size, point: Point, image: Data, alpha: Alpha) {
         self.image = image
         super.init(size: size, point: point, alpha: alpha)
