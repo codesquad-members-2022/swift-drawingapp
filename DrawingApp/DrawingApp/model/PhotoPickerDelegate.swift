ab8c0403e00602ea1ecee715e9c6e22c086f419b
kimdongjune <jun36817@naver.com>
2022-03-15 19:10:00 +0900

    plane의 구조를 변경하여 CustomViewEntity의 타입으로 models배열에 모두 담았습니다. ( 상위 클래스 타입 ), Photo가 필요로 하는 기능들을 구현하였습니다.

---
+++ DrawingApp/DrawingApp/model/PhotoPickerDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  PhotoFactory.swift
+//  DrawingApp
+//
+//  Created by 김동준 on 2022/03/14.
+//
+
+import Foundation
+import UIKit
+
+class PhotoPickerDelegate: NSObject{
+    enum Notification{
+        enum Event{
+            static let getPhotoFromDevice = Foundation.Notification.Name("getPhotoFromDevice")
+        }
+        enum Key{
+            case photoData
+        }
+    }
+}
+extension PhotoPickerDelegate: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var newImage: UIImage? = nil
+        if let possibleImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage { // 수정된 이미지가 있을 경우
+            newImage = possibleImage
+        } else if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { // 오리지널 이미지가 있을 경우
+            newImage = possibleImage
+        }
+        guard let pngData = newImage?.pngData() else {
+            return
+        }
+        NotificationCenter.default.post(name: PhotoPickerDelegate.Notification.Event.getPhotoFromDevice, object: self, userInfo: [PhotoPickerDelegate.Notification.Key.photoData : pngData])
+        picker.dismiss(animated: true)
+    }
+}
+
+protocol PhotoCallBack{
+    func photoCallBack(photo: UIImage)
+}
---
+++ DrawingApp/DrawingApp/view/PhotoView.swift
@@ -0,0 +1,72 @@
+//
+//  PhotoView.swift
+//  DrawingApp
+//
+//  Created by 김동준 on 2022/03/15.
+//
+
+import Foundation
+import UIKit
+
+class PhotoView: CustomBaseView{
+    private lazy var imageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.frame = self.bounds
+        return imageView
+    }()
+    override init(size: ViewSize, point: ViewPoint){
+        super.init(size: size, point: point)
+        setImageView()
+        backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setImageView()
+    }
+    
+    private func setImageView(){
+        addSubview(self.imageView)
+    }
+    
+    func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
+        
+        let cgimage = image.cgImage!
+        let contextImage: UIImage = UIImage(cgImage: cgimage)
+        let contextSize: CGSize = contextImage.size
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        var cgwidth: CGFloat = CGFloat(width)
+        var cgheight: CGFloat = CGFloat(height)
+        
+        // See what size is longer and create the center off of that
+        if contextSize.width > contextSize.height {
+            posX = ((contextSize.width - contextSize.height) / 2)
+            posY = 0
+            cgwidth = contextSize.height
+            cgheight = contextSize.height
+        } else {
+            posX = 0
+            posY = ((contextSize.height - contextSize.width) / 2)
+            cgwidth = contextSize.width
+            cgheight = contextSize.width
+        }
+        
+        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+        
+        // Create bitmap image from context using the rect
+        let imageRef: CGImage = cgimage.cropping(to: rect)!
+        
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
+        
+        return image
+    }
+    
+    func setImage(imageData: Data){
+        imageView.contentMode = .scaleAspectFit
+        guard let image = UIImage(data: imageData) else { return }
+        let cropImage = cropToBounds(image: image, width: 512.0, height: 512.0)
+        self.imageView.image = cropImage
+    }
+}
