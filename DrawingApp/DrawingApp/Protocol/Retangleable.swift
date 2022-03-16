2dab86dbbd813cf3edae967b9b1a1d2a20aa250d
P_SeoB <akdlsjtm21@naver.com>
2022-03-16 12:01:02 +0900

    [Rename] Rectangle Protocol -> Rectangleable

--- DrawingApp/DrawingApp/Protocol/Retangleable.swift
+++ DrawingApp/DrawingApp/Protocol/Retangleable.swift
@@ -1,13 +1,13 @@
 //
-//  Rectangle0.swift
+//  Retangleable.swift
 //  DrawingApp
 //
 //  Created by 박진섭 on 2022/03/14.
 //
 
 //사각형의 기본 메서드 프로토콜.
-protocol Rectangle {
-    init(id:ID,origin:Point,size:Size,rgb:RGB, alpha:Alpha)
+protocol Retangleable {
+    init(id:ID,origin:Point,size:Size,rgb:RGB?, alpha:Alpha?)
     func changeOrigin(_ origin:Point)
     func changeSize(_ size:Size)
     func changeAlpha(_ alpha:Alpha)
