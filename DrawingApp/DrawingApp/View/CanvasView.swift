5490d150cbdebff7620a4972185e52268a892502
asqw887 <asqw887@gmail.com>
2022-03-16 11:44:15 +0900

    refector : xib 뷰 불러오기

--- DrawingApp/DrawingApp/View/CanvasView.swift
+++ DrawingApp/DrawingApp/View/CanvasView.swift
@@ -17,34 +17,25 @@ class CanvasView: UIView {
     var delgate: RectangleViewDelegate?
     @IBOutlet var rectangleAddButton: UIButton!
     
+    
     override init(frame: CGRect){
         super.init(frame: frame)
-        customInit()
     }
     
     required init?(coder: NSCoder){
         super.init(coder: coder)
-        customInit()
+
     }
     
-    func customInit() {
-            if let view = Bundle.main.loadNibNamed("CanvasView", owner: self, options: nil)?.first as? UIView {
-                view.frame = self.bounds
-                addSubview(view)
-            }
-        rectangleAddButton.layer.cornerRadius = 15
-        }
-    
-    
+
     @IBAction func addRectangleTouched(_ sender: Any) {
         delgate?.addRectangleButtonDidTouch()
     }
     
     @IBAction func tapAction(_ sender: UITapGestureRecognizer) {
-        let touchedPoint = sender.location(in: sender.view)
-        delgate?.canvasDidTab(at: touchedPoint)
+            let touchedPoint = sender.location(in: sender.view)
+            delgate?.canvasDidTab(at: touchedPoint)
     }
-    
     
 
         
