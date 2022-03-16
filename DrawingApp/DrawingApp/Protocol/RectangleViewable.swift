8ce1fb150444440684240f01811ff2412c84a426
P_SeoB <akdlsjtm21@naver.com>
2022-03-16 12:24:12 +0900

    [Rename] Protocols

--- DrawingApp/DrawingApp/Protocol/RectangleViewable.swift
+++ DrawingApp/DrawingApp/Protocol/RectangleViewable.swift
@@ -1,11 +1,10 @@
 //
-//  RectangleView.swift
+//  RectangleViewable.swift
 //  DrawingApp
 //
 //  Created by 박진섭 on 2022/03/16.
 //
 
-protocol RectangleViewAble {
-    func setupFrameWithPlaneRectangle()
-    func 
+protocol RectangleViewable {
+    func setupFrameWithRectangle(rect:Rectangleable)
 }
