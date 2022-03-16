257ad097499a8d1b348f28440e2f32495b7c80e3
P_SeoB <akdlsjtm21@naver.com>
2022-03-16 15:23:36 +0900

    [Chore] fix some optional value in models

--- DrawingApp/DrawingApp/Factory/RectangleFactory.swift
+++ DrawingApp/DrawingApp/Factory/RectangleFactory.swift
@@ -5,11 +5,10 @@
 //  Created by 박진섭 on 2022/03/14.
 //
 
-
 //Image가 들어가있는 Rectangle, 기본 plane Rectangle들을 Rectangle로 묶어서 Factory메서드를 사용할수 있을것 같아 선언
 final class RectangleFactory:RectangleCreator {
     
-    func makeRectangle(type:Rectangle.Type) -> Rectangle {
+    func makeRectangle(type:Rectangleable.Type) -> Rectangleable {
         let id = IDFactory.makeRandomID()
         let size = Size(width: 150, height: 120)
         let origin = Point.random()
