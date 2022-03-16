00dd47de8635e4a3a2b3039356f904e42b65331a
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-16 15:49:32 +0900

    Add: AbstractRectangleView, ColorRectangleView, ImageRectangleView 파일 추가

---
+++ DrawingApp/DrawingApp/View/ImageRectangleView.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UIKit
+
+class ImageRectangleView: AbstractRectangleView{
+    
+    private var imageView: UIImageView = UIImageView()
+    
+    required init(rectangle: Rectangle){
+        super.init(rectangle: rectangle)
+        if let rectangle = rectangle as? ImageRectangle{
+            self.imageView.image = UIImage(data: rectangle.backgroundImage)
+            self.imageView.frame = self.bounds
+            self.addSubview(self.imageView)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
