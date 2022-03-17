db511f388f4cb0189a00eaf01cb004c897b68093
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-17 16:43:41 +0900

    Refactor: RectangleApplicable 프로토콜 속성 정의
    - point,alpha에 대해서는 우선 get,set모두 허용

--- DrawingApp/DrawingApp/Model/RectangleApplicable.swift
+++ DrawingApp/DrawingApp/Model/RectangleApplicable.swift
@@ -1,8 +1,9 @@
 import Foundation
 
-protocol RectangleApplicable{
+protocol RectangleApplicable: CustomStringConvertible{
     var id: Id {get}
     var size: Size {get}
-    var point: Point {get}
-    var alpha: Alpha {get}
+    var point: Point {get set}
+    var alpha: Alpha {get set}
+    var description: String { get }
 }
