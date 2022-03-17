dd86e2e342d427c720f146c29012208e98bcf454
asqw887 <asqw887@gmail.com>
2022-03-17 20:42:34 +0900

    refector : code refectoring

--- DrawingApp/DrawingApp/NotificationCenter.swift
+++ DrawingApp/DrawingApp/NotificationCenter.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension Notification.Name{
+    static let rectangleDidAdd = NSNotification.Name("rectangeDidAdd")
+    static let backgroundDidChagned = NSNotification.Name("backgroundDidAdd")
+    static let alpahDidChanged = NSNotification.Name("alpahDidChanged")
+    static let rectangleDidMutate = NSNotification.Name("rectangleDidMutate")
+}
+
