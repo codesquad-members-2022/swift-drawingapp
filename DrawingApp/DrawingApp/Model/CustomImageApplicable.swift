5dff063f4fba4a5c545314324e0fe12bf8e3d75f
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-17 16:44:08 +0900

    Refactor: CustomStringApplicable, RandomColorApplicable 프로토콜 속성 정의

--- DrawingApp/DrawingApp/Model/CustomImageApplicable.swift
+++ DrawingApp/DrawingApp/Model/CustomImageApplicable.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 protocol CustomImageApplicable{
-    
+    var backgroundImage: Data {get}
 }
--- DrawingApp/DrawingApp/Model/RandomColorApplicable.swift
+++ DrawingApp/DrawingApp/Model/RandomColorApplicable.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 protocol RandomColorApplicable{
-    
+    var backgroundColor: Color {get}
+    func updateBackgroundColor(newColor: Color)
 }
