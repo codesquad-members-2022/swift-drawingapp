cb71decf394ac72cef4f358f96612bc8f03e10e2
Jason <ggj00125@gmail.com>
2022-03-17 16:04:54 +0900

    Add: 프로젝트 생성 및 Docs 폴더 추가로 각 step README 파일 관리

---
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Jason on 2022/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
