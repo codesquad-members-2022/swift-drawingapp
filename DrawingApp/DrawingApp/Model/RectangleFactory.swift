28f66b998d4c95f42dfabf1bc87f1462ffea5b13
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-17 16:58:08 +0900

    Refactor: RectangleFactory 내부 메소드의 리턴타입을 모두 프로토콜타입으로 수정

--- DrawingApp/DrawingApp/Model/RectangleFactory.swift
+++ DrawingApp/DrawingApp/Model/RectangleFactory.swift
@@ -2,16 +2,16 @@ import Foundation
 
 class RectangleFactory{
     
-    static func createImageRectangle(maxX: Double, maxY: Double, minWidth: Double, minHeight: Double, maxWidth: Double, maxHeight: Double, image: Data)-> ImageRectangle{
+    static func createImageRectangle(maxX: Double, maxY: Double, minWidth: Double, minHeight: Double, maxWidth: Double, maxHeight: Double, image: Data)-> RectangleApplicable & CustomImageApplicable{
         let id = createRandomStringId()
         let point = createRandomPoint(maxX: maxX, maxY: maxY)
         let size = createRandomRectangleSize(minWidth: minWidth, minHeight: minHeight, maxWidth: maxWidth, maxHeight: maxHeight)
-        let alpha = Rectangle.Alpha.init(opacity: 10)
+        let alpha = Alpha.init(opacity: 10)
         
         return ImageRectangle(id: id, size: size, point: point, backgroundImage: image, alpha: alpha)
     }
     
-    static func createColorRenctangle(maxX: Double, maxY: Double, minWidth: Double, minHeight: Double, maxWidth: Double, maxHeight: Double)-> ColorRectangle{
+    static func createColorRenctangle(maxX: Double, maxY: Double, minWidth: Double, minHeight: Double, maxWidth: Double, maxHeight: Double)-> RectangleApplicable & RandomColorApplicable{
         let id = createRandomStringId()
         let point = createRandomPoint(maxX: maxX, maxY: maxY)
         let size = createRandomRectangleSize(minWidth: minWidth, minHeight: minHeight, maxWidth: maxWidth, maxHeight: maxHeight)
@@ -21,23 +21,23 @@ class RectangleFactory{
         return ColorRectangle(id: id, size: size, point: point, backgroundColor: color, alpha: alpha)
     }
     
-    private static func createRandomStringId()-> Rectangle.Id{
-        return Rectangle.Id()
+    private static func createRandomStringId()->Id{
+        return Id()
     }
     
-    private static func createRandomPoint(maxX: Double, maxY: Double)-> Rectangle.Point{
-        return Rectangle.Point(x: Double.random(in: 1...maxX), y: Double.random(in: 1...maxY))
+    private static func createRandomPoint(maxX: Double, maxY: Double)-> Point{
+        return Point(x: Double.random(in: 1...maxX), y: Double.random(in: 1...maxY))
     }
     
-    private static func createRandomRectangleSize(minWidth: Double, minHeight: Double, maxWidth: Double, maxHeight: Double)-> Rectangle.Size{
-        return Rectangle.Size(width: Double.random(in: minWidth...maxWidth), height: Double.random(in: minHeight...maxHeight))
+    private static func createRandomRectangleSize(minWidth: Double, minHeight: Double, maxWidth: Double, maxHeight: Double)-> Size{
+        return Size(width: Double.random(in: minWidth...maxWidth), height: Double.random(in: minHeight...maxHeight))
     }
     
-    static func createRandomColor()-> ColorRectangle.Color{
-        return ColorRectangle.Color(r: Double.random(in: 0...255), g: Double.random(in: 0...255), b: Double.random(in: 0...255))
+    static func createRandomColor()-> Color{
+        return Color(r: Double.random(in: 0...255), g: Double.random(in: 0...255), b: Double.random(in: 0...255))
     }
     
-    private static func createRandomAlpha()-> Rectangle.Alpha{
-        return Rectangle.Alpha(opacity: Int.random(in: 0...9))
+    private static func createRandomAlpha()-> Alpha{
+        return Alpha(opacity: Int.random(in: 0...9))
     }
 }
