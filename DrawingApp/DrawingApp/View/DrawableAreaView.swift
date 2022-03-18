a5c0bbae9302843451af6c51a591c6b45666b535
sol <hskim6521@naver.com>
2022-03-18 16:17:46 +0900

    Refactor: DrawableAreaView를 따로 분리
    DrawableAreaView에서 발생하는 터치, 드래그 이벤트를 Delegate 패턴을 이용해 ViewController로 전달하도록 흐름 수정
    다른 뷰들의 입력은 ViewController가 DrawableAreaView로 전달해 DrawableAreaView의 서브뷰들을 업데이트하도록 흐름 수정
    DrawableAreaView를 따로 분리함으로 인해, 선택된 뷰와 복사된 temporaryView를 ViewController가 알 필요 없으므로 해당 프로퍼티 제거
    DrawableAreaView가 선택된 뷰를 알고 있기 때문에 불필요하게 matchedView를 찾기 위해 타입 캐스팅 하던 부분 제거

---
+++ DrawingApp/DrawingApp/View/DrawableAreaView.swift
@@ -0,0 +1,119 @@
+//
+//  DrawableAreaView.swift
+//  DrawingApp
+//
+//  Created by 김한솔 on 2022/03/18.
+//
+
+import UIKit
+
+protocol DrawableAreaViewDelegate: AnyObject {
+    func drawableAreaViewDidReceiveTap(_ drawableAreaView: DrawableAreaView)
+    func drawableAreaViewDidBeginPan(_ drawableAreaView: DrawableAreaView)
+    func drawableAreaViewDidChangePan(_ drawableAreaView: DrawableAreaView)
+    func drawableAreaViewDidEndPan(_ drawableAreaView: DrawableAreaView)
+}
+
+class DrawableAreaView: UIView {
+    
+    weak var delegate: DrawableAreaViewDelegate?
+    private(set) var touchedPoint: Point?
+    private(set) var selectedView: RectangleViewable?
+    private(set) var movingTemporaryView: RectangleViewable?
+    
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.clipsToBounds = true
+        addGestures()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.clipsToBounds = true
+        addGestures()
+    }
+    
+    func addGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapAction(_:)))
+        self.addGestureRecognizer(tapGesture)
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panAction(_:)))
+        panGesture.minimumNumberOfTouches = 2
+        panGesture.maximumNumberOfTouches = 2
+        self.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func tapAction(_ sender: UITapGestureRecognizer) {
+        let touchedLocation = sender.location(in: self)
+        self.touchedPoint = Point(x: touchedLocation.x, y: touchedLocation.y)
+        
+        delegate?.drawableAreaViewDidReceiveTap(self)
+    }
+    
+    @objc func panAction(_ sender: UIPanGestureRecognizer) {
+        let tanslation = sender.translation(in: self)
+        
+        switch sender.state {
+            case .began:
+                let touchedLocation = sender.location(in: self)
+                self.touchedPoint = Point(x: touchedLocation.x, y: touchedLocation.y)
+                
+                delegate?.drawableAreaViewDidBeginPan(self)
+                
+            case .changed:
+                guard let movingTemporaryView = movingTemporaryView else {return}
+                movingTemporaryView.move(distance: tanslation)
+                sender.setTranslation(.zero, in: self)
+                
+                delegate?.drawableAreaViewDidChangePan(self)
+                
+            case .ended:
+                guard let movingTemporaryView = movingTemporaryView as? UIView else {return}
+                let convertedNewPoint = Point(x: movingTemporaryView.frame.origin.x,
+                                              y: movingTemporaryView.frame.origin.y)
+
+                delegate?.drawableAreaViewDidEndPan(self)
+                movingTemporaryView.removeFromSuperview()
+                self.movingTemporaryView = nil
+            default: return
+        }
+    }
+    
+    func makeTemporaryView() {
+        guard let selectedView = selectedView,
+              let copiedView = selectedView.copy() as? UIView & RectangleViewable else {return}
+        
+        copiedView.changeAlphaValue(to: 0.5)
+        self.movingTemporaryView = copiedView
+        
+        addSubview(copiedView)
+    }
+    
+    func add(subView: UIView & RectangleViewable) {
+        self.addSubview(subView)
+    }
+    
+    func showSelectedView(_ selectedView: RectangleViewable?) {
+        self.selectedView?.hideBoundary()
+        
+        self.selectedView = selectedView
+        selectedView?.showBoundary()
+    }
+    
+    func updateSelectedView(backgroundColor: UIColor) {
+        self.selectedView?.changeBackgroundColor(to: backgroundColor)
+    }
+    
+    func updateSelectedView(alpha: CGFloat) {
+        self.selectedView?.changeAlphaValue(to: alpha)
+    }
+    
+    func updateSelectedView(point: CGPoint) {
+        self.selectedView?.move(to: point)
+    }
+    
+    func updateSelectedView(width: CGFloat, height: CGFloat) {
+        self.selectedView?.resize(to: (width, height))
+    }
+    
+}
