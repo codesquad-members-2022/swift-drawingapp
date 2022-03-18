7e4c8b662ef18d7422be09fe7902c32d1e7ec55e
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-18 10:21:39 +0900

    Refactor: ColorRectangle, ImageRectangle 속성 private (set) 으로 일괄 변경

--- DrawingApp/DrawingApp/Model/ColorRectangle.swift
+++ DrawingApp/DrawingApp/Model/ColorRectangle.swift
@@ -2,10 +2,10 @@ import Foundation
 
 class ColorRectangle: RectangleApplicable, RandomColorApplicable, CustomStringConvertible{
     
-    var id: Id
-    var size: Size
-    var point: Point
-    var alpha: Alpha
+    private (set) var id: Id
+    private (set) var size: Size
+    private (set) var point: Point
+    private (set) var alpha: Alpha
     private (set) var backgroundColor: Color
     var description: String{
         return "(\(id)), \(size), \(point), \(backgroundColor), \(alpha)"
@@ -17,6 +17,15 @@ class ColorRectangle: RectangleApplicable, RandomColorApplicable, CustomStringCo
         self.size = size
         self.point = point
         self.alpha = alpha
+    }
+    
+    func updateAlpha(opacity: Int) {
+        self.alpha = Alpha(opacity: opacity)
+    }
+    
+    func updatePoint(x: Double, y: Double) {
+        self.point.x = x
+        self.point.y = y
     }
     
     func updateBackgroundColor(newColor: Color){
