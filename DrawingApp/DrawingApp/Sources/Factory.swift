0e85494d547ffda07fca1a7e661e710bf4f34c7d
Jihee hwang <hwangjihee94@gmail.com>
2022-03-18 11:35:08 +0900

    [ Update ] Panel View

--- DrawingApp/DrawingApp/Sources/Factory.swift
+++ DrawingApp/DrawingApp/Sources/Factory.swift
@@ -14,7 +14,7 @@ class Factory {
 
         let size = Size(width: 150, height: 120)
         let point = Point(x: round(Double.random(in: 0...700)),
-                          y: round(Double.random(in: 0...720)))
+                          y: round(Double.random(in: 0...700)))
         let color = Color(r: round(Double.random(in: 0...255)),
                           g: round(Double.random(in: 0...255)),
                           b: round(Double.random(in: 0...255)))
