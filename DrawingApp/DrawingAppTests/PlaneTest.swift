1a734349bc4dfb8fd448fc0df1cd6bbb8434a39d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-18 12:58:16 +0900

    Refactor: 테스트 메소드 수정

--- DrawingApp/DrawingAppTests/PlaneTest.swift
+++ DrawingApp/DrawingAppTests/PlaneTest.swift
@@ -17,7 +17,7 @@ class PlaneTest: XCTestCase {
         let rectangle = Rectangle(id: Id(), size: Size(width: 200, height: 200), point: Point(x: 100, y: 100), alpha: Alpha(opacity: Int.random(in: 0...9)))
         plane.addRectangle(rectangle)
         
-        XCTAssertNotNil(plane[150,150])
-        XCTAssertNil(plane[400,400])
+        XCTAssertNotNil(plane.findMatchingRectangleModel(x:150,y:150))
+        XCTAssertNil(plane.findMatchingRectangleModel(x:400,y:400))
     }
 }
