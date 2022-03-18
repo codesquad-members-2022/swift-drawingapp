24fdbc5e8dd4499f52c1f511f9a45b88ac9291a8
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-18 11:49:09 +0900

    Refactor: ViewController의 rectangleDictionary 속성 키를 Rectangle 자체로 사용하도록 수정
    - Rectangle 클래스 Hashable 채택
    - ColorRectangle, ImageRectangle은 RectangleApplicable이 아닌 Rectangle을 상속받도록 수정

--- DrawingApp/DrawingApp/Model/ImageRectangle.swift
+++ DrawingApp/DrawingApp/Model/ImageRectangle.swift
@@ -1,31 +1,15 @@
 import Foundation
 
-class ImageRectangle: RectangleApplicable, CustomImageApplicable, CustomStringConvertible{
-    
-    private (set) var id: Id
-    private (set) var size: Size
-    private (set) var point: Point
-    private (set) var alpha: Alpha
+class ImageRectangle: Rectangle, CustomImageApplicable{
+
     private (set) var backgroundImage: Data
-    var description: String{
+    override var description: String{
         return "(\(id)), \(size), \(point), \(backgroundImage.hashValue), \(alpha)"
     }
      
     init(id: Id, size: Size, point: Point, backgroundImage: Data, alpha: Alpha){
         self.backgroundImage = backgroundImage
-        self.id = id
-        self.size = size
-        self.point = point
-        self.alpha = alpha
-    }
-    
-    func updateAlpha(opacity: Int) {
-        self.alpha = Alpha(opacity: opacity)
-    }
-    
-    func updatePoint(x: Double, y: Double) {
-        self.point.x = x
-        self.point.y = y
+        super.init(id: id, size: size, point: point, alpha: alpha)
     }
      
 }
