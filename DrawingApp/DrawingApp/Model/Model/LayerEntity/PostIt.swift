1282aebae1c3009691c564bfdbb3d5c2795cc30a
BumgeunSong <isp1195@gmail.com>
2022-03-28 14:49:23 +0900

    Add: PostIt entity

---
+++ DrawingApp/DrawingApp/Model/Model/LayerEntity/PostIt.swift
@@ -0,0 +1,41 @@
+//
+//  PostIt.swift
+//  DrawingApp
+//
+//  Created by Bumgeun Song on 2022/03/28.
+//
+
+import Foundation
+
+class PostIt: Layer {
+    private(set) var text: String
+    private(set) var backgroundColor: Color
+    
+    init(title: String, id: ID, origin: Point, size: Size, text: String, color: Color) {
+        self.text = text
+        self.backgroundColor = color
+        super.init(title: title, id: id, origin: origin, size: size)
+    }
+}
+
+extension PostIt: TextMutable {
+    func set(to text: String) {
+        self.text = text
+    }
+}
+
+extension PostIt: ColorMutable {
+    var color: Color {
+        self.backgroundColor
+    }
+    
+    func set(to color: Color) {
+        self.backgroundColor = color
+    }
+}
+
+extension PostIt: CustomStringConvertible {
+    var description: String {
+        return "(\(id)), \(origin), \(size), \(backgroundColor), \(text)"
+    }
+}
