decd21a5d8d1bad10ff3b52992ec1e4924beb6f7
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-28 22:44:41 +0900

    [Refactor] BasicShapeView, RectangleView 리팩토링 -  생성자 구현 및 상속 활용 -  Color, Alpha를 View가 직접 UIColor, CGFloat 값으로 변환해서 사용
    (cherry picked from commit dc6342f7f55102fd54f49b1d66f31841c48d0e45)

--- DrawingApp/DrawingApp/Views/RectangleView.swift
+++ DrawingApp/DrawingApp/Views/RectangleView.swift
@@ -9,17 +9,28 @@ import UIKit
 
 class RectangleView: BasicShapeView {
     
+    init(point: Point, size: Size, backgroundColor: Color, alpha: Alpha) {
+        super.init(point: point, size: size)
+        self.changeBackgroundColor(by: backgroundColor)
+        self.changeAlpha(to: alpha)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
 
 extension RectangleView: ViewColorChangable {
-    func changeBackgroundColor(by newColor: UIColor) {
-        backgroundColor = newColor
+    func changeBackgroundColor(by newColor: Color) {
+        backgroundColor = UIColor(red: newColor.red / 255.0,
+                                  green: newColor.green / 255.0,
+                                  blue: newColor.blue / 255.0,
+                                  alpha: 1.0)
     }
 }
 
 extension RectangleView: ViewAlphaChangable {
-    func changeAlpha(to alphaLevel: CGFloat) {
-        alpha = alphaLevel
+    func changeAlpha(to alphaLevel: Alpha) {
+        alpha = alphaLevel.value
     }
 }
-
