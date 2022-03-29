d8c1e60f973cf2c064d5261e0fb5de0f52d38eb8
sanghee <anshee97@naver.com>
2022-03-29 20:41:21 +0900

    Refactor: Rectangle 터치 시의 delegate를 Notification 사용하도록 리팩토링

--- DrawingApp/DrawingApp/PlaneDelegate.swift
+++ DrawingApp/DrawingApp/PlaneDelegate.swift
@@ -8,9 +8,6 @@
 import Foundation
 
 protocol PlaneDelegate {
-    /// Plane에서 터치된 좌표에 사각형이 있음을 알림
-    func planeDidTouchedRectangle(_ rectangle: Rectangle)
-    
     /// Plane에서 터치된 좌표에 아무것도 없음을 알림
     func planeDidTouchedEmptySpace()
     
