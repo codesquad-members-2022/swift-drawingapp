96d1dfbbc89e1d1a482684be2d5968bb0e52fcdb
Jason <ggj00125@gmail.com>
2022-03-29 15:39:33 +0900

    [Add] Alpha, Identifier, Point, Size 클래스 객체 생성

---
+++ DrawingApp/DrawingApp/Source/Model/Size.swift
@@ -0,0 +1,24 @@
+//
+//  Size.swift
+//  DrawingApp
+//
+//  Created by Jason on 2022/03/29.
+//
+
+import Foundation
+
+struct Size {
+    private(set) var width: Double
+    private(set) var height: Double
+    
+    init(width:Double, height:Double) {
+        self.width = width
+        self.height = height
+    }
+}
+
+extension Size: CustomStringConvertible {
+    var description: String {
+        return "Size: W = \(self.width), H = \(self.height)"
+    }
+}
