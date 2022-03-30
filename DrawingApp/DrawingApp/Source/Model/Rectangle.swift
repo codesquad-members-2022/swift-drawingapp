e41bac9eb42236e54ee79da01f5ae59ba90b8d0a
Jason <ggj00125@gmail.com>
2022-03-30 23:05:33 +0900

    [Feat] Background Color 작성 및 Rectangle 파일 생성

---
+++ DrawingApp/DrawingApp/Source/Model/Rectangle.swift
@@ -0,0 +1,16 @@
+//
+//  Rectangle.swift
+//  DrawingApp
+//
+//  Created by Jason on 2022/03/30.
+//
+
+import Foundation
+
+class Rectangle {
+//    private(set) var id: Identifier
+//    private(set) var point: Point
+//    private(set) var size: Size
+//    private(set) var alpha: Alpha
+    
+}
