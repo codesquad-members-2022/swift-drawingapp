5bba20086d7b7322c6109f7917145c26524b6906
Jason <ggj00125@gmail.com>
2022-03-30 23:11:11 +0900

    [Feat] Rectangle Object Attribute 생성

--- DrawingApp/DrawingApp/Source/Model/Rectangle.swift
+++ DrawingApp/DrawingApp/Source/Model/Rectangle.swift
@@ -8,9 +8,18 @@
 import Foundation
 
 class Rectangle {
-//    private(set) var id: Identifier
-//    private(set) var point: Point
-//    private(set) var size: Size
-//    private(set) var alpha: Alpha
+    private(set) var id: Identifier
+    private(set) var point: Point
+    private(set) var size: Size
+    private(set) var alpha: Alpha
+    private(set) var backgroundColor: BackgroundColor
     
+    init(id: Identifier, size: Size, point: Point, backGroundColor: BackgroundColor, alpha: Alpha)
+        {
+            self.id = id
+            self.size = Size(width: 150, height: 120)
+            self.point = point
+            self.backgroundColor = backGroundColor
+            self.alpha = alpha
+        }
 }
