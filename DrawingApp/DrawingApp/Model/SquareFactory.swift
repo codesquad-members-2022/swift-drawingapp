40c9467e1928396b7639f37da3694e6943c80082
OceanShape <oceanshape700@gmail.com>
2022-05-27 02:57:10 +0900

    Feat: RGB 자료형 변경
    - 정상적인 범위에 해당하는 값(0~255)를 강제하기 위해, 자료형을 UInt8로 변경
    
    (cherry picked from commit c7b6566b8a5f755c01f0742c99742292f5bdca9d)

--- DrawingApp/DrawingApp/Model/SquareFactory.swift
+++ DrawingApp/DrawingApp/Model/SquareFactory.swift
@@ -3,8 +3,8 @@ import Foundation
 class SquareFactory {
     let idElement: String = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-    func createSquare(size: Size, point: Point, r: Int, g: Int, b: Int, a: Int) -> Square {
-        return Square(id: getRandomId(), size: size, point: point, r: r, g: g, b: b, alpha: a)
+    func createSquare(size: Size, point: Point, R: UInt8, G: UInt8, B: UInt8, alpha: Int) -> Square {
+        return Square(id: getRandomId(), size: size, point: point, R: UInt8(R), G: UInt8(G), B: UInt8(B), alpha: alpha)
     }
     
     func getRandomId() -> String {
--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -10,7 +10,7 @@ class ViewController: UIViewController {
         var squareAry : [Square] = []
         
         for i in 1..<5 {
-            squareAry.append(factory.createSquare(size: Size(width: Double(i * 100), height: Double(i * 200)), point: Point(X: Double(i * 300), Y: Double(i * 400)), r: i + 10, g: i + 20, b: i + 30, a: 2 * i + 1))
+            squareAry.append(factory.createSquare(size: Size(width: Double(i * 100), height: Double(i * 200)), point: Point(X: Double(i * 300), Y: Double(i * 400)), R: UInt8(i + 10), G: UInt8(i + 20), B: UInt8(i + 30), alpha: 2 * i + 1))
         }
         
         for i in 0..<4 {
