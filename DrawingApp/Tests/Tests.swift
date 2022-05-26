a2ec852ea224e5a837939d4d46e35768cb19a613
OceanShape <oceanshape700@gmail.com>
2022-05-27 02:54:43 +0900

    Test: 샘플 테스트파일 추가
    (cherry picked from commit 8f2cc6516d07b88a74ab072d1341d01e7816414b)

---
+++ DrawingApp/Tests/Tests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import Plane
+@testable import Square
+
+class Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testPlane() {
+        // given
+        let plane = Plane()
+        let square1 = Square()
+        
+        // when
+        // addSquare() & totalSquareCount
+        
+        
+        // subscript(index)
+        // subscript(position)
+        
+        
+        // then
+    }
+
+}
