c7b6566b8a5f755c01f0742c99742292f5bdca9d
OceanShape <oceanshape700@gmail.com>
2022-05-27 02:57:10 +0900

    Feat: RGB 자료형 변경
    - 정상적인 범위에 해당하는 값(0~255)를 강제하기 위해, 자료형을 UInt8로 변경

--- DrawingApp/DrawingApp/Model/Square.swift
+++ DrawingApp/DrawingApp/Model/Square.swift
@@ -4,37 +4,26 @@ class Square : CustomStringConvertible {
     private var id: String
     private var size: Size
     private var point: Point
-    private var R: Int = 0
-    private var G: Int = 0
-    private var B: Int = 0
-    private var A: Int = 0
+    private var R: UInt8
+    private var G: UInt8
+    private var B: UInt8
+    private var alpha: Int
 
-    init(id: String, size: Size, point: Point, r: Int, g: Int, b: Int, alpha: Int) {
+    init(id: String, size: Size, point: Point, R: UInt8, G: UInt8, B: UInt8, alpha: Int) {
         self.id = id
         self.size = size
         self.point = point
 
-        self.R = setRGBDegree(deg: r)
-        self.G = setRGBDegree(deg: g)
-        self.B = setRGBDegree(deg: b)
+        self.R = R
+        self.G = G
+        self.B = B
         switch alpha {
         case ...0 :
-            self.A = 0
+            self.alpha = 0
         case 10...:
-            self.A = 10
+            self.alpha = 10
         default:
-            self.A = alpha
-        }
-    }
-    
-    func setRGBDegree(deg: Int) -> Int {
-        switch deg {
-        case ...0 :
-            return 0
-        case 256...:
-            return 256
-        default:
-            return deg
+            self.alpha = alpha
         }
     }
     
@@ -50,6 +39,6 @@ class Square : CustomStringConvertible {
     }
     
     var description: String {
-        return "(\(id)), X:\(point.X),Y:\(point.Y), W\(size.Width), H\(size.Height), R:\(R), G:\(G), B:\(B), Alpha: \(A)"
+        return "(\(id)), X:\(point.X),Y:\(point.Y), W\(size.Width), H\(size.Height), R:\(R), G:\(G), B:\(B), alpha: \(alpha)"
     }
 }
