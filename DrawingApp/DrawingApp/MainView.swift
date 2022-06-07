a952e4ef49089d556c27df7b0e17c130d1c39094
OceanShape <oceanshape700@gmail.com>
2022-06-07 23:53:30 +0900

    Refactor: ViewController의 view를 MainView로 변경

---
+++ DrawingApp/DrawingApp/MainView.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MainView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
