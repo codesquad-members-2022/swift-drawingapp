6d8638fe2ee44fa1f6707f5f003bcedef7fd0485
OceanShape <oceanshape700@gmail.com>
2022-06-20 01:12:13 +0900

    Refactor: DrawingSection/StatusSection 작성

---
+++ DrawingApp/DrawingApp/DrawingView.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class DrawingSection: UIView {
+
+    private let drawingView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addSubview(drawingView)
+        
+        NSLayoutConstraint.activate([
+            self.drawingView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.drawingView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.drawingView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            self.drawingView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
